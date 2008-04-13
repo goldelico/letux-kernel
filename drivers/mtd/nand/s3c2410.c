1d89da736ed33d3f7c398fb9f8dfddecb7c7c7a9
mokopatches <mokopatches@openmoko.org>
2008-04-13 07:23:48 +0100

    s3c2440-nand-disable-hwecc.patch Disable the hardware ECC checking on S3C2440 based platforms (HXD8, SMDK2440, GTA02) for the time being, since our u-boot doesn't yet support it for 2k page size NAND

--- drivers/mtd/nand/s3c2410.c
+++ drivers/mtd/nand/s3c2410.c
@@ -658,7 +658,7 @@ static void s3c2410_nand_init_chip(struct s3c2410_nand_info *info,
 	nmtd->mtd.owner    = THIS_MODULE;
 	nmtd->set	   = set;
 
-	if (hardware_ecc) {
+	if (info->cpu_type == TYPE_S3C2410 && hardware_ecc) {
 		chip->ecc.calculate = s3c2410_nand_calculate_ecc;
 		chip->ecc.correct   = s3c2410_nand_correct_data;
 		chip->ecc.mode	    = NAND_ECC_HW;
