70f2dc966b4e73538b9b3366fcabb809203875cf
Andy Green <andy@openmoko.com>
2008-11-19 17:11:15 +0000

    tracking-fix-android-gpio-include.patch
    Signed-off-by: Andy Green <andy@openmoko.com>

--- drivers/android/timed_gpio.c
+++ drivers/android/timed_gpio.c
@@ -18,7 +18,7 @@
 #include <linux/platform_device.h>
 #include <linux/hrtimer.h>
 #include <linux/err.h>
-#include <asm/arch/gpio.h>
+#include <mach/gpio.h>
 
 #include <linux/android_timed_gpio.h>
 
