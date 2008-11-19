dde5086ba728e1d4c03128ad054e1f7e8b309366
Matt Hsu <matt_hsu@openmoko.org>
2008-11-19 17:10:53 +0000

    gta03-pca9632.patch
     - pca9632 is a LED driver which will be adopted in gta03.
    
    Signed-off-by: Matt Hsu <matt_hsu@openmoko.org>

--- include/linux/i2c-id.h
+++ include/linux/i2c-id.h
@@ -85,6 +85,7 @@
 #define I2C_DRIVERID_OV7670 1048	/* Omnivision 7670 camera */
 #define I2C_DRIVERID_PCF50606 1049
 #define I2C_DRIVERID_PCF50633 1051
+#define I2C_DRIVERID_PCA9632  1052
 
 /*
  * ---- Adapter types ----------------------------------------------------
