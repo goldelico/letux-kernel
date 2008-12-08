31383993decd60a7cc783f402ad83ee8580008d3
merge <null@invalid>
2008-12-08 16:57:14 +0000

    MERGE-via-pending-tracking-hist-s3c24xx-needs-gpio_track pending-tracking-hist top was s3c24xx-needs-gpio_track / da546f30820208681442d98d29aadd2e6a909a09 ... parent commitmessage: From: Werner Almesberger <werner@openmoko.org> S3C24xx needs GPIO_TRACK
    s3c_gpiolib_getchip for 24xx assumes that, as far as addressing is
    concerned, all ports have 32 pins. However, there are two flaws in
    this logic:
    
    1) gpiolib only assigns pin number for real pins (with the
       exception of gaps added with CONFIG_S3C_GPIO_SPACE), and
    
    2) not all ports have the same number of pins.
    
    This patch makes 24xx use the more generic platform s3c_gpiolib_getchip,
    like we do on 6410.
    
    Signed-off-by: Werner Almesberger <werner@openmoko.org>

--- arch/arm/mach-s3c2410/include/mach/gpio-core.h
+++ arch/arm/mach-s3c2410/include/mach/gpio-core.h
@@ -15,20 +15,7 @@
 #ifndef __ASM_ARCH_GPIO_CORE_H
 #define __ASM_ARCH_GPIO_CORE_H __FILE__
 
+/* currently we just include the platform support */
 #include <plat/gpio-core.h>
-#include <mach/regs-gpio.h>
-
-extern struct s3c_gpio_chip s3c24xx_gpios[];
-
-static inline struct s3c_gpio_chip *s3c_gpiolib_getchip(unsigned int pin)
-{
-	struct s3c_gpio_chip *chip;
-
-	if (pin > S3C2410_GPH10)
-		return NULL;
-
-	chip = &s3c24xx_gpios[pin/32];
-	return (S3C2410_GPIO_OFFSET(pin) > chip->chip.ngpio) ? chip : NULL;
-}
 
 #endif /* __ASM_ARCH_GPIO_CORE_H */
