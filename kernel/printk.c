ffe28618306dfeb7d7923539cc468ff2dbbdf1dc
Werner Almesberger <werner@openmoko.org>
2009-01-19 01:18:02 +0000

    Make modified printk.c build without CONFIG_PM
    Our suspend/resume debugging code in vprintk uses global_inside_suspend,
    which is only defined if CONFIG_PM is enabled.
    
    Fortunately, the fix is straightforward: if there's no CONFIG_PM, then
    there's nothing to debug here either :-)
    
    Signed-off-by: Werner Almesberger <werner@openmoko.org>

--- kernel/printk.c
+++ kernel/printk.c
@@ -676,7 +676,7 @@ asmlinkage int vprintk(const char *fmt, va_list args)
 	/* Emit the output into the temporary buffer */
 	printed_len += vscnprintf(printk_buf + printed_len,
 				  sizeof(printk_buf) - printed_len, fmt, args);
-#ifdef CONFIG_MACH_NEO1973_GTA02
+#if defined(CONFIG_MACH_NEO1973_GTA02) && defined(CONFIG_PM)
 	/* if you're debugging resume, the normal methods can change resume
 	 * ordering behaviours because their debugging output is synchronous
 	 * (ie, CONFIG_DEBUG_LL).  If your problem is an OOPS, this code
