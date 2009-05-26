e6e1e32b7ac0d39b89a08e07992548b78e810105
David Cohen <david.cohen@nokia.com>
2009-05-26 17:07:20 +0300

    omap3isp: af: enable AF interrupt and small cleanups
    - Enable AF interrupt during ISP initialization
     - Remove unused callback type CBK_H3A_AF_DONE
     - Remove uneeded callback set/unset code for AF
    
    Signed-off-by: David Cohen <david.cohen@nokia.com>

--- drivers/media/video/isp/isp.c
+++ drivers/media/video/isp/isp.c
@@ -361,7 +361,7 @@ static void isp_enable_interrupts(struct device *dev)
 		| IRQ0ENABLE_HS_VS_IRQ
 		| IRQ0ENABLE_CCDC_VD0_IRQ | IRQ0ENABLE_CCDC_VD1_IRQ
 		| IRQ0ENABLE_CSIB_IRQ
-		| IRQ0ENABLE_H3A_AWB_DONE_IRQ
+		| IRQ0ENABLE_H3A_AWB_DONE_IRQ | IRQ0ENABLE_H3A_AF_DONE_IRQ
 		| isp->module.interrupts;
 
 	if (CCDC_PREV_CAPTURE(isp))
@@ -414,15 +414,6 @@ int isp_set_callback(struct device *dev, enum isp_callback_type type,
 	spin_unlock_irqrestore(&isp->lock, irqflags);
 
 	switch (type) {
-	case CBK_H3A_AF_DONE:
-		isp->module.interrupts |= IRQ0ENABLE_H3A_AF_DONE_IRQ;
-		if (isp->module.running != ISP_RUNNING)
-			break;
-		isp_reg_writel(dev, IRQ0ENABLE_H3A_AF_DONE_IRQ,
-			       OMAP3_ISP_IOMEM_MAIN, ISP_IRQ0STATUS);
-		isp_reg_or(dev, OMAP3_ISP_IOMEM_MAIN, ISP_IRQ0ENABLE,
-			   IRQ0ENABLE_H3A_AF_DONE_IRQ);
-		break;
 	case CBK_HIST_DONE:
 		isp->module.interrupts |= IRQ0ENABLE_HIST_DONE_IRQ;
 		if (isp->module.running != ISP_RUNNING)
@@ -471,13 +462,6 @@ int isp_unset_callback(struct device *dev, enum isp_callback_type type)
 	spin_unlock_irqrestore(&isp->lock, irqflags);
 
 	switch (type) {
-	case CBK_H3A_AF_DONE:
-		isp->module.interrupts &= ~IRQ0ENABLE_H3A_AF_DONE_IRQ;
-		if (isp->module.running != ISP_RUNNING)
-			break;
-		isp_reg_and(dev, OMAP3_ISP_IOMEM_MAIN, ISP_IRQ0ENABLE,
-			    ~IRQ0ENABLE_H3A_AF_DONE_IRQ);
-		break;
 	case CBK_HIST_DONE:
 		isp->module.interrupts &= ~IRQ0ENABLE_HIST_DONE_IRQ;
 		if (isp->module.running != ISP_RUNNING)
