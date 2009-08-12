5b443479fc285b912ab966b1016713695c480631
Hiroshi DOYU <Hiroshi.DOYU@nokia.com>
2009-08-12 15:06:32 +0300

    OMAP: iommu: fix wrong argument in flush_cache_vmap()
    The second argument should be the end address, not the
    length. Actually there will not be any effect on the behavior of this
    driver since flush_cache_vmap() calls flush_cache_all() in the end.
    
    Signed-off-by: Hiroshi DOYU <Hiroshi.DOYU@nokia.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/iovmm.c
+++ arch/arm/plat-omap/iovmm.c
@@ -194,7 +194,7 @@ static void *vmap_sg(const struct sg_table *sgt)
 		va += bytes;
 	}
 
-	flush_cache_vmap(new->addr, total);
+	flush_cache_vmap(new->addr, new->addr + total);
 	return new->addr;
 
 err_out:
