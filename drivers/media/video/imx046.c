b55a2c698c2740a73f5ceb332c852f689981bc1c
Pallabi Parveen <pparveen@ti.com>
2009-08-17 14:23:05 -0500

    omap3isp: 720p support in camera
    Configure the sensor for 720p and support 720p from sensor to CCDC
    to PREV module.
    
    Signed-off-by: Pallabi Parveen <pparveen@ti.com>

--- drivers/media/video/imx046.c
+++ drivers/media/video/imx046.c
@@ -190,6 +190,40 @@ static struct imx046_sensor_settings sensor_settings[] = {
 		},
 	},
 
+	/* 720p_MP */
+	{
+		.clk = {
+			.pre_pll_div = 1,
+			.pll_mult = 18,
+			.post_pll_div = 1,
+			.vt_pix_clk_div = 10,
+			.vt_sys_clk_div = 1,
+		},
+		.mipi = {
+			.data_lanes = 1,
+			.ths_prepare = 2,
+			.ths_zero = 5,
+			.ths_settle_lower = 8,
+			.ths_settle_upper = 28,
+		},
+		.frame = {
+			.frame_len_lines_min = 1250,
+			.line_len_pck = 3456,
+			.x_addr_start = 348,
+			.x_addr_end = 2931,
+			.y_addr_start = 504,
+			.y_addr_end = 1959,
+			.x_output_size = 1292,
+			.y_output_size = 728,
+			.x_even_inc = 1,
+			.x_odd_inc = 3,
+			.y_even_inc = 1,
+			.y_odd_inc = 3,
+			.v_mode_add = 0,
+			.h_mode_add = 0,
+			.h_add_ave = 134,
+		},
+	},
 	/* TWO_MP */
 	{
 		.clk = {
