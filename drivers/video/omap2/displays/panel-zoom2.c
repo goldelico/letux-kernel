b0ff98f48c94cba89c7277729b72dcaa6cda4491
Subramani Venkatesh <x0108988@ti.com>
2009-09-02 19:02:30 -0400

    Omap zoom2-panel default recommended bpp
    Intilialise default recommended bpp to 16, this value enables proper
    memory allocation for VRFB and VRFB rotation.
    
    Signed-off-by: Subramani Venkatesh <subramani.venkatesh@windriver.com>

--- drivers/video/omap2/displays/panel-zoom2.c
+++ drivers/video/omap2/displays/panel-zoom2.c
@@ -64,6 +64,7 @@ static int zoom2_panel_probe(struct omap_dss_device *dssdev)
 	dssdev->panel.config = OMAP_DSS_LCD_TFT | OMAP_DSS_LCD_IVS |
 				OMAP_DSS_LCD_IHS;
 	dssdev->panel.timings = zoom2_panel_timings;
+	dssdev->panel.recommended_bpp = 16;
 
 	return 0;
 }
