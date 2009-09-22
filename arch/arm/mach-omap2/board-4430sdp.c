f84b66b1bd5783e41d68d4081ddd436552661ef4
Nishant Kamat <nskamat@ti.com>
2009-09-23 02:31:25 +0530

    ARM: OMAP4: Add platform data for TWL6030 GPADC
    This patch adds the irq line to be used for the TWL6030 GPADC on OMAP4.
    
    Signed-off-by: Nishant Kamat <nskamat@ti.com>
    Signed-off-by: Balaji T K <balajitk@ti.com>

--- arch/arm/mach-omap2/board-4430sdp.c
+++ arch/arm/mach-omap2/board-4430sdp.c
@@ -428,6 +428,10 @@ static struct regulator_init_data sdp4430_vusb = {
 	},
 };
 
+static struct twl4030_madc_platform_data sdp4430_gpadc_data = {
+	.irq_line	= 1,
+};
+
 static struct twl4030_platform_data sdp4430_twldata = {
 	.irq_base	= TWL6030_IRQ_BASE,
 	.irq_end	= TWL6030_IRQ_END,
@@ -443,6 +447,7 @@ static struct twl4030_platform_data sdp4430_twldata = {
 	.vaux1		= &sdp4430_vaux1,
 	.vaux2		= &sdp4430_vaux2,
 	.vaux3		= &sdp4430_vaux3,
+	.madc           = &sdp4430_gpadc_data,
 };
 
 static struct i2c_board_info __initdata sdp4430_i2c_boardinfo[] = {
