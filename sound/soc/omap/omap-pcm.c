471966d075e3918572356c493e68d9ed68521826
Jorge Eduardo Candelaria <x0107209@ti.com>
2009-10-13 16:49:50 -0500

    ASoC: OMAP4: HDMI: Add support for HDMI interface
    HDMI interface makes use of ALSA framework for audio playback.
    
    HDMI platform driver enables ALSA to configure and start HDMI Wrapper
    when it is needed, and also start the audio transfer to the interface
    output.
    
    DAI has on stream (playback) with the following specifications:
    * Support for 2 and 8 channels.
    * Rate: 48000 Hz (will support 44100 in the future).
    * Sample format: 32 bits little endian (will support 16 bits also)
    
    Signed-off-by: Jorge Eduardo Candelaria <x0107209@ti.com>
    Signed-off-by: Margarita Olaya Cabrera <magi.olaya@ti.com>
    Signed-off-by: Jorge Eduardo Candelaria <jorge.candelaria@ti.com>

--- sound/soc/omap/omap-pcm.c
+++ sound/soc/omap/omap-pcm.c
@@ -281,9 +281,11 @@ static int omap_pcm_open(struct snd_pcm_substream *substream)
 	if (ret < 0)
 		goto out;
 
-	/* ensure that buffer size is a multiple of 24 */
+	/* ABE needs a step of 24 * 4 data bits, and HDMI 32 * 4
+	* Ensure buffer size satisfies both constraints.
+	*/
 	ret = snd_pcm_hw_constraint_step(runtime, 0,
-					 SNDRV_PCM_HW_PARAM_BUFFER_BYTES, 96);
+					 SNDRV_PCM_HW_PARAM_BUFFER_BYTES, 384);
 	if (ret < 0)
 		goto out;
 
