31b5c3d859bae8d11f85d729d15967628a16d968
Mikkel Christensen <mlc@ti.com>
2009-11-02 14:37:26 -0600

    DSS2: RFBI suspend/resume functionality
    Adding missing suspend and resume functions.
    
    Signed-off-by: Mikkel Christensen <mlc@ti.com>
    (cherry picked from commit 4a8ebd4ac8a5beec76f71c0663fd040f143a9dd2)

--- drivers/video/omap2/dss/rfbi.c
+++ drivers/video/omap2/dss/rfbi.c
@@ -124,6 +124,7 @@ static struct {
 	struct kfifo      *cmd_fifo;
 	spinlock_t        cmd_lock;
 	struct completion cmd_done;
+	struct completion update_done;
 	atomic_t          cmd_fifo_full;
 	atomic_t          cmd_pending;
 #ifdef MEASURE_PERF
@@ -418,6 +419,8 @@ static void framedone_callback(void *data, u32 mask)
 	rfbi.framedone_callback = NULL;
 
 	/*callback(rfbi.framedone_callback_data);*/
+
+	complete(&rfbi.update_done);
 
 	atomic_set(&rfbi.cmd_pending, 0);
 
@@ -1164,6 +1167,7 @@ int rfbi_init(void)
 		return -ENOMEM;
 
 	init_completion(&rfbi.cmd_done);
+	init_completion(&rfbi.update_done);
 	atomic_set(&rfbi.cmd_fifo_full, 0);
 	atomic_set(&rfbi.cmd_pending, 0);
 
@@ -1207,6 +1211,9 @@ static int rfbi_display_update(struct omap_dss_device *dssdev,
 			u16 x, u16 y, u16 w, u16 h)
 {
 	int rfbi_module;
+
+	if (dssdev->state != OMAP_DSS_DISPLAY_ACTIVE)
+		return 0;
 
 	if (w == 0 || h == 0)
 		return 0;
@@ -1254,6 +1261,8 @@ static int rfbi_display_enable_te(struct omap_dss_device *dssdev, bool enable)
 static int rfbi_display_enable(struct omap_dss_device *dssdev)
 {
 	int r;
+
+	dssdev->state = OMAP_DSS_DISPLAY_ACTIVE;
 
 	r = omap_dss_start_device(dssdev);
 	if (r) {
@@ -1306,6 +1315,51 @@ static void rfbi_display_disable(struct omap_dss_device *dssdev)
 	omap_dss_stop_device(dssdev);
 }
 
+static int rfbi_display_suspend(struct omap_dss_device *dssdev)
+{
+	DSSDBG("rfbi_display_suspend\n");
+
+	if (dssdev->state != OMAP_DSS_DISPLAY_ACTIVE)
+		return -EINVAL;
+
+	dssdev->state = OMAP_DSS_DISPLAY_SUSPENDED;
+
+	wait_for_completion(&rfbi.update_done);
+
+	rfbi_enable_clocks(1);
+
+	/* Bypass mode must be enabled to allow the RFBI module to idle */
+	REG_FLD_MOD(RFBI_CONTROL, 1, 1, 1);
+
+	rfbi_display_disable(dssdev);
+	rfbi_exit();
+
+	rfbi_enable_clocks(0);
+
+	return 0;
+}
+
+static int rfbi_display_resume(struct omap_dss_device *dssdev)
+{
+
+	DSSDBG("rfbi_display_resume\n");
+
+	if (dssdev->state != OMAP_DSS_DISPLAY_SUSPENDED)
+		return -EINVAL;
+
+	rfbi_enable_clocks(1);
+
+	/* Disable Bypass mode again */
+	REG_FLD_MOD(RFBI_CONTROL, 0, 1, 1);
+
+	rfbi_init();
+	rfbi_display_enable(dssdev);
+
+	rfbi_enable_clocks(0);
+
+	return 0;
+}
+
 int rfbi_init_display(struct omap_dss_device *dssdev)
 {
 	dssdev->enable = rfbi_display_enable;
@@ -1315,6 +1369,8 @@ int rfbi_init_display(struct omap_dss_device *dssdev)
 	dssdev->enable_te = rfbi_display_enable_te;
 	dssdev->get_update_mode = rfbi_display_get_update_mode;
 	dssdev->set_update_mode = rfbi_display_set_update_mode;
+	dssdev->suspend = rfbi_display_suspend;
+	dssdev->resume = rfbi_display_resume;
 
 	rfbi.dssdev[dssdev->phy.rfbi.channel] = dssdev;
 
