fc681d7d37cb92b8f30dd0bd047c6c217f6f2333
Moiz Sonasath <m-sonasath@ti.com>
2009-11-11 15:57:25 -0600

    omap: 3630: disable internal pull-ups
    This patch disables the newly introduced internal pull-up feature in
    OMAP3630, to use only the external HW resitor >= 470 Ohm for the
    assured functionality in HS mode.
    
    While testing the I2C in High Speed mode, it was discovered that
    without a proper pull-up resitor, there is data corruption during
    multi-byte transfers. RTC(time_set) test case was used for testing.
    
    From the analysis done, it was concluded that ideally we need a pull-up
    of 1.6 K Ohm (recomended) or atleast 470 Ohm or greater for assured
    performance in HS mode.
    
    Signed-off-by: Moiz Sonasath <m-sonasath@ti.com>

--- arch/arm/mach-omap2/board-zoom2.c
+++ arch/arm/mach-omap2/board-zoom2.c
@@ -1033,6 +1033,30 @@ static struct i2c_board_info __initdata zoom2_i2c_bus3_info[] = {
 
 static int __init omap_i2c_init(void)
 {
+
+/* Disable OMAP 3630 internal pull-ups for I2Ci */
+	if (cpu_is_omap3630()) {
+
+		u32 prog_io;
+
+		prog_io = omap_ctrl_readl(OMAP343X_CONTROL_PROG_IO1);
+		/* Program (bit 19)=1 to disable internal pull-up on I2C1 */
+		prog_io |= OMAP3630_PRG_I2C1_PULLUPRESX;
+		/* Program (bit 0)=1 to disable internal pull-up on I2C2 */
+		prog_io |= OMAP3630_PRG_I2C2_PULLUPRESX;
+		omap_ctrl_writel(prog_io, OMAP343X_CONTROL_PROG_IO1);
+
+		prog_io = omap_ctrl_readl(OMAP36XX_CONTROL_PROG_IO2);
+		/* Program (bit 7)=1 to disable internal pull-up on I2C3 */
+		prog_io |= OMAP3630_PRG_I2C3_PULLUPRESX;
+		omap_ctrl_writel(prog_io, OMAP36XX_CONTROL_PROG_IO2);
+
+		prog_io = omap_ctrl_readl(OMAP36XX_CONTROL_PROG_IO_WKUP1);
+		/* Program (bit 5)=1 to disable internal pull-up on I2C4(SR) */
+		prog_io |= OMAP3630_PRG_SR_PULLUPRESX;
+		omap_ctrl_writel(prog_io, OMAP36XX_CONTROL_PROG_IO_WKUP1);
+	}
+
 	omap_register_i2c_bus(1, 100, zoom2_i2c_bus1_info,
 			ARRAY_SIZE(zoom2_i2c_bus1_info));
 	omap_register_i2c_bus(2, 100, zoom2_i2c_bus2_info,
