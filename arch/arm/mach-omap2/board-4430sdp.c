be57de399f31ba0a1d12535edd6675ad3a8b884d
Balaji T K <balajitk@ti.com>
2009-11-25 22:15:06 +0530

    ARM: OMAP4: Add platform data for TWL6030 BCI
    This patch adds sdp4430_bci_data to be used for the
    	TWL6030 Battery Charger Interface on OMAP4.
    
    Signed-off-by: Nishant Kamat <nskamat@ti.com>
    Signed-off-by: Balaji T K <balajitk@ti.com>

--- arch/arm/mach-omap2/board-4430sdp.c
+++ arch/arm/mach-omap2/board-4430sdp.c
@@ -432,6 +432,9 @@ static struct twl4030_madc_platform_data sdp4430_gpadc_data = {
 	.irq_line	= 1,
 };
 
+static struct twl4030_bci_platform_data sdp4430_bci_data = {
+};
+
 static struct twl4030_platform_data sdp4430_twldata = {
 	.irq_base	= TWL6030_IRQ_BASE,
 	.irq_end	= TWL6030_IRQ_END,
@@ -448,6 +451,7 @@ static struct twl4030_platform_data sdp4430_twldata = {
 	.vaux2		= &sdp4430_vaux2,
 	.vaux3		= &sdp4430_vaux3,
 	.madc           = &sdp4430_gpadc_data,
+	.bci            = &sdp4430_bci_data,
 };
 
 static struct i2c_board_info __initdata sdp4430_i2c_boardinfo[] = {
