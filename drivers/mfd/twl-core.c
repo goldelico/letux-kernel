f3c5c3a2fd9368c38d27389ed2623bab837f913d
Balaji T K <balajitk@ti.com>
2009-11-25 22:15:06 +0530

    MFD: TWL6030: Add twl6030_bci as child to twl
    Add twl6030_bci as child to twl
    
    Signed-off-by: Nishant Kamat <nskamat@ti.com>
    Signed-off-by: Balaji T K <balajitk@ti.com>

--- drivers/mfd/twl-core.c
+++ drivers/mfd/twl-core.c
@@ -59,7 +59,9 @@
 #define DRIVER_NAME			"twl"
 
 #if defined(CONFIG_TWL4030_BCI_BATTERY) || \
-	defined(CONFIG_TWL4030_BCI_BATTERY_MODULE)
+	defined(CONFIG_TWL4030_BCI_BATTERY_MODULE) || \
+	defined(CONFIG_TWL6030_BCI_BATTERY) || \
+	defined(CONFIG_TWL6030_BCI_BATTERY_MODULE)
 #define twl_has_bci()		true
 #else
 #define twl_has_bci()		false
@@ -602,6 +604,15 @@ add_children(struct twl4030_platform_data *pdata, unsigned long features)
 		if (IS_ERR(child))
 			return PTR_ERR(child);
 	}
+	if (twl_has_bci() && pdata->bci &&
+	    (features & TWL6030_CLASS)) {
+		child = add_child(1, "twl6030_bci",
+				pdata->bci, sizeof(*pdata->bci),
+				false,
+				pdata->irq_base + CHARGER_INTR_OFFSET,
+				pdata->irq_base + CHARGERFAULT_INTR_OFFSET);
+	}
+
 
 	if (twl_has_gpio() && pdata->gpio) {
 		child = add_child(SUB_CHIP_ID1, "twl4030_gpio",
