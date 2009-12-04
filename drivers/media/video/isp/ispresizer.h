d00d3e5e1d5353da1bb5bdffcebd8d8d2402a167
Dominic Curran <dcurran@ti.com>
2009-12-04 15:13:08 -0600

    Revert "omap3isp: resz: Fix cropping for MEM vs PREV input"
    This reverts commit 94182e5a44630dd0f5cf4b98a5662980888b98dc.
    
    Causing a iommu problem. Cause not currently known.
    Required more investigation.

--- drivers/media/video/isp/ispresizer.h
+++ drivers/media/video/isp/ispresizer.h
@@ -134,8 +134,7 @@ struct isp_res_device {
 	u16 h_resz;
 	u16 v_resz;
 	u8 algo;
-	dma_addr_t in_buf_addr;
-	dma_addr_t in_buf_addr_off;
+	dma_addr_t tmp_buf;
 	struct isprsz_coef coeflist;
 	struct mutex ispres_mutex; /* For checking/modifying res_inuse */
 	struct isprsz_yenh defaultyenh;
@@ -171,7 +170,7 @@ int ispresizer_s_pipeline(struct isp_res_device *isp_res,
 
 int ispresizer_config_inlineoffset(struct isp_res_device *isp_res, u32 offset);
 
-int ispresizer_set_inaddr(struct isp_res_device *isp_res, u32 addr, u32 offset);
+int ispresizer_set_inaddr(struct isp_res_device *isp_res, u32 addr);
 
 int ispresizer_config_outlineoffset(struct isp_res_device *isp_res, u32 offset);
 
