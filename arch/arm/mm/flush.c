86f8333e50cd136247d6d605e4586d038fc854aa
Catalin Marinas <catalin.marinas@arm.com>
2009-12-18 12:45:11 +0000

    ARMv7: Use lazy cache flushing if hardware broadcasts cache operations
    ARMv7 processors like Cortex-A9 broadcast the cache maintenance
    operations in hardware. The patch adds the CPU ID checks for such
    feature and allows the flush_dcache_page/update_mmu_cache pair to work
    in lazy flushing mode similar to the UP case.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm/mm/flush.c
+++ arch/arm/mm/flush.c
@@ -15,6 +15,7 @@
 #include <asm/cachetype.h>
 #include <asm/system.h>
 #include <asm/tlbflush.h>
+#include <asm/smp_plat.h>
 
 #include "mm.h"
 
@@ -206,12 +207,10 @@ void flush_dcache_page(struct page *page)
 
 	mapping = page_mapping(page);
 
-#ifndef CONFIG_SMP
-	if (!PageHighMem(page) && mapping && !mapping_mapped(mapping))
+	if (!cache_ops_need_broadcast() &&
+	    !PageHighMem(page) && mapping && !mapping_mapped(mapping))
 		set_bit(PG_dcache_dirty, &page->flags);
-	else
-#endif
-	{
+	else {
 		__flush_dcache_page(mapping, page);
 		if (mapping && cache_is_vivt())
 			__flush_dcache_aliases(mapping, page);
