b83dafb821024c372e2253b6b17f40dc75ff8cb3
Pavan Savoy <pavan_savoy@ti.com>
2010-01-05 22:20:28 +0530

    drivers: misc: sources for FM header file
    Texas Instruments BT, FM and GPS combo chips/drivers
    make use of a single TTY to communicate with the chip.
    This is the FM common header file across ST and FM protocol
    driver.
    
    Signed-off-by: Pavan Savoy <pavan_savoy@ti.com>

---
+++ drivers/misc/ti-st/fm.h
@@ -0,0 +1,7 @@
+struct fm_event_hdr {
+	unsigned char plen;
+} __attribute__ ((packed));
+
+#define FM_MAX_FRAME_SIZE 0xFF	/* TODO: */
+#define FM_EVENT_HDR_SIZE 1	/* size of fm_event_hdr */
+#define ST_FM_CH8_PKT 0x8
