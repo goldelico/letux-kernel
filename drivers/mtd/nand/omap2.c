977071f0ab79006e6e55f15329387513e3d58078
Madhusudhan Chikkature <madhu.cr@ti.com>
2010-01-08 15:52:07 -0500

    OMAP: NAND: Making hw ecc compatible with romcode ecc
    This patch overrides nand ecc layout and bad block descriptor
    (for 8-bit device) to support hw ecc in romcode layout.
    So as to have in sync with ecc layout throughout; i.e. x-laod,
    u-boot and kernel.
    This enables to use single tool for flashing x-load, u-boot,
    kernel, FS images to nand device.
    
    Signed-off-by: Vimal Singh <vimalsingh@ti.com>
    Signed-off-by: Madhusudhan Chikkature <madhu.cr@ti.com>

--- drivers/mtd/nand/omap2.c
+++ drivers/mtd/nand/omap2.c
@@ -149,6 +149,39 @@ struct omap_nand_info {
 	int				dma_ch;
 };
 
+static struct nand_ecclayout nand_x8_hw_romcode_oob_64 = {
+	.eccbytes = 12,
+	.eccpos = {
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+	},
+	.oobfree = {
+		{.offset = 13,
+			.length = 51}
+		}
+};
+
+/* Define some generic bad / good block scan pattern which are used
+ * while scanning a device for factory marked good / bad blocks
+ */
+static uint8_t scan_ff_pattern[] = { 0xff };
+static struct nand_bbt_descr bb_descrip_flashbased = {
+	.options = NAND_BBT_SCANEMPTY | NAND_BBT_SCANALLPAGES,
+	.offs = 0,
+	.len = 1,
+	.pattern = scan_ff_pattern,
+};
+
+static struct nand_ecclayout nand_x16_hw_romcode_oob_64 = {
+	.eccbytes = 12,
+	.eccpos = {
+		2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
+	},
+	.oobfree = {
+			{.offset = 14,
+			.length = 50}
+	}
+};
+
 /*
  * omap_nand_wp - This function enable or disable the Write Protect feature on
  * NAND device
@@ -996,6 +1029,12 @@ static int __devinit omap_nand_probe(struct platform_device *pdev)
 	if (pdata->ecc_opt == 0x1) {
 		info->nand.ecc.bytes            = 3;
 		info->nand.ecc.size             = 512;
+		if (info->nand.options & NAND_BUSWIDTH_16) {
+			info->nand.ecc.layout   = &nand_x16_hw_romcode_oob_64;
+		} else {
+			info->nand.ecc.layout   = &nand_x8_hw_romcode_oob_64;
+			info->nand.badblock_pattern = &bb_descrip_flashbased;
+		}
 		info->nand.ecc.calculate        = omap_calculate_ecc;
 		info->nand.ecc.hwctl            = omap_enable_hwecc;
 		info->nand.ecc.correct          = omap_correct_data;
