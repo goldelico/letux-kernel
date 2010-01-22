dc7d559f20c36d20418f92879409101ad2f20ff6
Maulik Mankad <x0082077@ti.com>
2010-01-22 11:19:37 +0530

    USB : Add empty functions in otg.h
    Add empty functions for usb_nop_xceiv_register()
    and usb_nop_xceiv_unregister() in otg.h so that
    these functions can be called even when
    CONFIG_NOP_USB_XCEIV is not enabled.
    
    It allows to remove ifdef's from board file.
    
    Signed-off-by: Maulik Mankad <x0082077@ti.com>
    Cc: Tony Lindgren <tony@atomide.com>
    Cc: Felipe Balbi <felipe.balbi@nokia.com>
    Cc: Greg Kroah-Hartman <gregkh@suse.de>
    Cc: Olof Johansson <olof@lixom.net>
    Cc: Sergei Shtylyov <sshtylyov@ru.mvista.com>
    Cc: Gupta, Ajay Kumar <ajay.gupta@ti.com>

--- include/linux/usb/otg.h
+++ include/linux/usb/otg.h
@@ -81,9 +81,19 @@ struct otg_transceiver {
 /* for board-specific init logic */
 extern int otg_set_transceiver(struct otg_transceiver *);
 
+#if defined(CONFIG_NOP_USB_XCEIV)
 /* sometimes transceivers are accessed only through e.g. ULPI */
 extern void usb_nop_xceiv_register(void);
 extern void usb_nop_xceiv_unregister(void);
+#else
+static inline void usb_nop_xceiv_register(void)
+{
+}
+
+static inline void usb_nop_xceiv_unregister(void)
+{
+}
+#endif
 
 
 /* for usb host and peripheral controller drivers */
