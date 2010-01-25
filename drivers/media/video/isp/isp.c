45c5309f5479be22dd1dc7fb847c3609782e94ea
Penda Naveen <pnaveen@ti.com>
2010-01-25 16:40:06 -0600

    disable mclk in suspended state and enable when it resumes
    This patch will ensure mclk is cut in suspended state and enabled
    during resume.
    
    Signed-off-by: Penda Naveen pnaveen@ti.com

--- drivers/media/video/isp/isp.c
+++ drivers/media/video/isp/isp.c
@@ -2697,6 +2697,7 @@ static int isp_suspend(struct platform_device *pdev, pm_message_t state)
 		isp_reset(&pdev->dev);
 
 	isp_disable_clocks(&pdev->dev);
+	isp_disable_mclk(isp);
 
 out:
 	DPRINTK_ISPCTRL("isp_suspend: done\n");
@@ -2721,6 +2722,9 @@ static int isp_resume(struct platform_device *pdev)
 		goto out;
 
 	ret_err = isp_enable_clocks(&pdev->dev);
+	if (ret_err)
+		goto out;
+	ret_err = isp_enable_mclk(&pdev->dev);
 	if (ret_err)
 		goto out;
 	isp_restore_ctx(&pdev->dev);
