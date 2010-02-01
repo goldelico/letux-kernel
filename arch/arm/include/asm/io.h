ef96bba506c2c710c39cb6c749331f71d9a3308c
Lajos Molnar <molnar@ti.com>
2010-02-01 19:07:05 +0530

    MM: Add support for ioremapping multiple strided areas
    Added new method __arm_multi_strided_ioremap to remap multiple
    strided physical areas into one contiguous virtual area.
    
    Signed-off: Lajos Molnar <molnar@ti.com>

--- arch/arm/include/asm/io.h
+++ arch/arm/include/asm/io.h
@@ -72,6 +72,9 @@ extern void __raw_readsl(const void __iomem *addr, void *data, int longlen);
  */
 extern void __iomem * __arm_ioremap_pfn(unsigned long, unsigned long, size_t, unsigned int);
 extern void __iomem * __arm_ioremap(unsigned long, size_t, unsigned int);
+extern void __iomem * __arm_multi_strided_ioremap(int, unsigned long *,
+	size_t *,unsigned long *, unsigned long *, unsigned int);
+
 extern void __iounmap(volatile void __iomem *addr);
 
 /*
