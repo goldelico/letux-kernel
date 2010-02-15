dc651732d5d24f9b80ce14456812aaf658b7c63f
Fernando Guzman Lugo <x0095840@ti.com>
2010-02-15 12:32:19 -0600

    DSPBRIDGE: fix regression introduced by other patch
    This patch fixes regression introduce by this patch:
    DSPBRIDGE: Remove chnl_sm.h and tiomap_sm.c
    
    Signed-off-by: Fernando Guzman Lugo <x0095840@ti.com>

--- drivers/dsp/bridge/wmd/tiomap_io.c
+++ drivers/dsp/bridge/wmd/tiomap_io.c
@@ -415,62 +415,48 @@ DSP_STATUS WriteExtDspData(struct WMD_DEV_CONTEXT *pDevContext,
 DSP_STATUS sm_interrupt_dsp(struct WMD_DEV_CONTEXT *pDevContext,
 				u16 wMbVal)
 {
-#ifdef CONFIG_BRIDGE_DVFS
-	struct dspbridge_platform_data *pdata =
-		omap_dspbridge_dev->dev.platform_data;
-	u32 opplevel = 0;
-#endif
-	struct CFG_HOSTRES resources;
 	DSP_STATUS status = DSP_SOK;
 	u32 temp;
 
 	if (!pDevContext->mbox)
 		return DSP_SOK;
 
-	status = CFG_GetHostResources((struct CFG_DEVNODE *)
-			DRV_GetFirstDevExtension(), &resources);
-	if (DSP_FAILED(status))
-		return DSP_EFAIL;
+#ifdef CONFIG_BRIDGE_DVFS
+	if (pDevContext->dwBrdState == BRD_DSP_HIBERNATION ||
+	    pDevContext->dwBrdState == BRD_HIBERNATION) {
+		if (!in_atomic())
+			tiomap3430_bump_dsp_opp_level();
+	}
+#endif
 
 	if (pDevContext->dwBrdState == BRD_DSP_HIBERNATION ||
 	    pDevContext->dwBrdState == BRD_HIBERNATION) {
-#ifdef CONFIG_BRIDGE_DVFS
-		if (pdata->dsp_get_opp)
-			opplevel = (*pdata->dsp_get_opp)();
-		if (opplevel == VDD1_OPP1) {
-			if (pdata->dsp_set_min_opp)
-				(*pdata->dsp_set_min_opp)(VDD1_OPP2);
-		}
-#endif
 		/* Restart the peripheral clocks */
 		DSP_PeripheralClocks_Enable(pDevContext, NULL);
-
-		/*
-		 * 2:0 AUTO_IVA2_DPLL - Enabling IVA2 DPLL auto control
-		 *     in CM_AUTOIDLE_PLL_IVA2 register
-		 */
-		*(REG_UWORD32 *)(resources.dwCmBase + 0x34) = 0x1;
-
-		/*
-		 * 7:4 IVA2_DPLL_FREQSEL - IVA2 internal frq set to
-		 *     0.75 MHz - 1.0 MHz
-		 * 2:0 EN_IVA2_DPLL - Enable IVA2 DPLL in lock mode
-		 */
-		temp = *(REG_UWORD32 *)(resources.dwCmBase + 0x4);
-		temp = (temp & 0xFFFFFF08) | 0x37;
-		*(REG_UWORD32 *)(resources.dwCmBase + 0x4) = temp;
 
 		/* Restore mailbox settings */
+		/* Enabling Dpll in lock mode*/
+		temp = (u32) *((REG_UWORD32 *)
+				((u32) (pDevContext->cmbase) + 0x34));
+		temp = (temp & 0xFFFFFFFE) | 0x1;
+		*((REG_UWORD32 *) ((u32) (pDevContext->cmbase) + 0x34)) =
+			(u32) temp;
+		temp = (u32) *((REG_UWORD32 *)
+				((u32) (pDevContext->cmbase) + 0x4));
+		temp = (temp & 0xFFFFFC8) | 0x37;
+
+		*((REG_UWORD32 *) ((u32) (pDevContext->cmbase) + 0x4)) =
+			(u32) temp;
 		omap_mbox_restore_ctx(pDevContext->mbox);
 
-		/* Access MMU SYS CONFIG register to generate a short wakeup */
-		temp = *(REG_UWORD32 *)(resources.dwDmmuBase + 0x10);
+		/*  Access MMU SYS CONFIG register to generate a short wakeup */
+		temp = (u32) *((REG_UWORD32 *) ((u32)
+					(pDevContext->dwDSPMmuBase) + 0x10));
 
 		pDevContext->dwBrdState = BRD_RUNNING;
-	} else if (pDevContext->dwBrdState == BRD_RETENTION) {
+	} else if (pDevContext->dwBrdState == BRD_RETENTION)
 		/* Restart the peripheral clocks */
 		DSP_PeripheralClocks_Enable(pDevContext, NULL);
-	}
 
 	status = omap_mbox_msg_send(pDevContext->mbox, wMbVal, NULL);
 
@@ -478,8 +464,7 @@ DSP_STATUS sm_interrupt_dsp(struct WMD_DEV_CONTEXT *pDevContext,
 		pr_err("omap_mbox_msg_send Fail and status = %d\n", status);
 		status = DSP_EFAIL;
 	}
-
 	DBG_Trace(DBG_LEVEL3, "writing %x to Mailbox\n", wMbVal);
+
 	return DSP_SOK;
 }
-
