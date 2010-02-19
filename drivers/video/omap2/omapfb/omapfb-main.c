25cc07ba1eeb7865f2b14178e0c4c532f23233e9
Lajos Molnar <molnar@ti.com>
2010-02-19 08:31:55 -0600

    Revert "DSS2: suspend and resume handlers in fb driver"
    This reverts commit 83a3b45dd80653e3c66d962ff0ed6b334522bc86.

--- drivers/video/omap2/omapfb/omapfb-main.c
+++ drivers/video/omap2/omapfb/omapfb-main.c
@@ -1708,10 +1708,14 @@ void suspend(struct early_suspend *h)
 	struct suspend_info *info = container_of(h, struct suspend_info,
 						early_suspend);
 	struct fb_info *fbi = info->fbi;
+	struct omapfb_info *ofbi = FB2OFB(fbi);
+	struct omapfb2_device *fbdev = ofbi->fbdev;
 	struct omap_dss_device *display = fb2display(fbi);
 
 	if (display->suspend)
 		display->suspend(display);
+
+	omapfb_vrfb_suspend_all(fbdev);
 }
 
 void resume(struct early_suspend *h)
@@ -1719,7 +1723,11 @@ void resume(struct early_suspend *h)
 	struct suspend_info *info = container_of(h, struct suspend_info,
 						early_suspend);
 	struct fb_info *fbi = info->fbi;
+	struct omapfb_info *ofbi = FB2OFB(fbi);
+	struct omapfb2_device *fbdev = ofbi->fbdev;
 	struct omap_dss_device *display = fb2display(fbi);
+
+	omapfb_vrfb_resume_all(fbdev);
 
 	if (display->resume)
 		display->resume(display);
