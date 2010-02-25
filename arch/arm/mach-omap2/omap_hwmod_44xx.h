bb8877f7387c92ef896346d7028540f076498676
Benoit Cousson <b-cousson@ti.com>
2010-02-25 15:04:55 +0100

    OMAP4: HWMOD: Add missing sysc for SPINLOCK
    Signed-off-by: Benoit Cousson <b-cousson@ti.com>
    (cherry picked from commit e6405566cf670ff40e446e81a7557beae1aeb6b3)

--- arch/arm/mach-omap2/omap_hwmod_44xx.h
+++ arch/arm/mach-omap2/omap_hwmod_44xx.h
@@ -4853,6 +4853,16 @@ static struct omap_hwmod_ocp_if *omap44xx_spinlock_slaves[] = {
 	&omap44xx_l4_cfg__spinlock,
 };
 
+static struct omap_hwmod_sysconfig omap44xx_spinlock_sysc = {
+	.rev_offs	= 0x0000,
+	.sysc_offs	= 0x0010,
+	.sysc_flags	= (SYSC_HAS_ENAWAKEUP | SYSC_HAS_SIDLEMODE |
+			   SYSC_HAS_CLOCKACTIVITY | SYSC_HAS_SOFTRESET |
+			   SYSC_HAS_AUTOIDLE | SYSS_MISSING),
+	.idlemodes	= (SIDLE_FORCE | SIDLE_NO | SIDLE_SMART),
+	.sysc_fields	= &omap_hwmod_sysc_type1,
+};
+
 static struct omap_hwmod omap44xx_spinlock_hwmod = {
 	.name		= "spinlock",
 	.prcm = {
@@ -4861,6 +4871,7 @@ static struct omap_hwmod omap44xx_spinlock_hwmod = {
 			.device_reg = OMAP4430_CM_L4CFG_HW_SEM_CLKCTRL,
 		},
 	},
+	.sysconfig	= &omap44xx_spinlock_sysc,
 	.slaves		= omap44xx_spinlock_slaves,
 	.slaves_cnt	= ARRAY_SIZE(omap44xx_spinlock_slaves),
 	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_OMAP4430),
