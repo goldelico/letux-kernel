3c1a0d91156c11f602036efd60b091517798a9ae
Benoit Cousson <b-cousson@ti.com>
2010-03-01 15:13:24 +0100

    OMAP4: CM & HWMOD: Implement the omap4_cm_wait_module_ready function
    The return of this function is checked in order to avoid
    accessing the sysconfig register is the module is not in the
    correct state.
    In that case the _setup is exited without trying to reset
    using sysconfig.
    For the moment a warning is printed. A proper management of fclk
    and module reset will have to be done in order to init correctly
    the problematic IPs listed below.
    
      <4>omap_hwmod: ivahd: cannot be enabled (3)
      <4>omap_hwmod: iss: cannot be enabled (3)
      <4>omap_hwmod: tesla: cannot be enabled (3)
      <4>omap_hwmod: sdma: cannot be enabled (3)
      <4>omap_hwmod: sl2: cannot be enabled (3)
      <4>omap_hwmod: sad2d: cannot be enabled (3)
      <4>omap_hwmod: ducati: cannot be enabled (3)
    
    Signed-off-by: Benoit Cousson <b-cousson@ti.com>
    (cherry picked from commit 09a96a296d5ffaef5d374c09574526a40f5511ed)

--- arch/arm/mach-omap2/omap_hwmod.c
+++ arch/arm/mach-omap2/omap_hwmod.c
@@ -839,11 +839,8 @@ static int _wait_target_ready(struct omap_hwmod *oh)
 		ret = omap2_cm_wait_module_ready(oh->prcm.omap2.module_offs,
 						 oh->prcm.omap2.idlest_reg_id,
 						 oh->prcm.omap2.idlest_idle_bit);
-#if 0
 	} else if (cpu_is_omap44xx()) {
-		ret = omap4_cm_wait_module_ready(oh->prcm.omap4.module_offs,
-						 oh->prcm.omap4.device_offs);
-#endif
+		ret = omap4_cm_wait_module_ready(oh->prcm.omap4.device_reg);
 	} else {
 		BUG();
 	};
@@ -934,15 +931,20 @@ static int _enable(struct omap_hwmod *oh)
 #endif
 	_enable_clocks(oh);
 
-	if (oh->sysconfig) {
-		if (!(oh->_int_flags & _HWMOD_SYSCONFIG_LOADED))
-			_update_sysc_cache(oh);
-		_sysc_enable(oh);
-	}
-
 	r = _wait_target_ready(oh);
-	if (!r)
+	if (!r) {
 		oh->_state = _HWMOD_STATE_ENABLED;
+
+		/* Access the sysconfig only if the target is ready */
+		if (oh->sysconfig) {
+			if (!(oh->_int_flags & _HWMOD_SYSCONFIG_LOADED))
+				_update_sysc_cache(oh);
+			_sysc_enable(oh);
+		}
+	}
+	else {
+		pr_debug("omap_hwmod: %s: _wait_target_ready: %d\n", oh->name, r);
+	}
 
 	return r;
 }
@@ -1032,7 +1034,7 @@ static int _shutdown(struct omap_hwmod *oh)
 static int _setup(struct omap_hwmod *oh)
 {
 	struct omap_hwmod_ocp_if *os;
-	int i;
+	int i, r;
 
 	if (!oh)
 		return -EINVAL;
@@ -1056,7 +1058,11 @@ static int _setup(struct omap_hwmod *oh)
 
 	oh->_state = _HWMOD_STATE_INITIALIZED;
 
-	_enable(oh);
+	r = _enable(oh);
+	if (r) {
+		pr_warning("omap_hwmod: %s: cannot be enabled (%d)\n", oh->name, oh->_state);
+		return 0;
+	}
 
 	if (!(oh->flags & HWMOD_INIT_NO_RESET)) {
 		/*
