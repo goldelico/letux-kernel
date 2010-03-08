c4d57f4311259f0be6f23546a058679e6764ca4c
Hari Kanigeri <h-kanigeri2@ti.com>
2010-03-08 17:55:21 -0600

    ARM:iommu support for OMAP4
    This patch provides the iommu support for OMAP4 co-processors.
    
    Signed-off-by: Hari Kanigeri <h-kanigeri2@ti.com>

--- arch/arm/plat-omap/iommu.c
+++ arch/arm/plat-omap/iommu.c
@@ -862,10 +862,12 @@ static int __devinit omap_iommu_probe(struct platform_device *pdev)
 	if (!obj)
 		return -ENOMEM;
 
-	obj->clk = clk_get(&pdev->dev, pdata->clk_name);
-	if (IS_ERR(obj->clk))
-		goto err_clk;
-
+	/* FIX ME: OMAP4 PM framework not ready */
+	if (!cpu_is_omap44xx()) {
+		obj->clk = clk_get(&pdev->dev, pdata->clk_name);
+		if (IS_ERR(obj->clk))
+			goto err_clk;
+	}
 	obj->nr_tlb_entries = pdata->nr_tlb_entries;
 	obj->name = pdata->name;
 	obj->dev = &pdev->dev;
