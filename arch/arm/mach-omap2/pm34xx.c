b6b02f7eefac2970becc1dc1f1ed6b8719da057a
Girish S G <girishsg@ti.com>
2010-03-10 17:12:07 -0600

    OMAP3:PM: enable/disable SR only in RET/OFF
    Disable Smartreflex before pwrdm enters RET
    
    Smartreflex for the corresponding powerdomain (MPU/CORE) must be disabled
    before the domain enters retention, otherwise the device may hang. This is
    caused by overlapping smartreflex / auto retention command on the voltage
    channel resulting in incorrect voltage.
    
    Rebased patch from l-o.
    
    Signed-off-by: Girish S G <girishsg@ti.com>
    Signed-off-by: Tero Kristo <tero.kristo@nokia.com>

--- arch/arm/mach-omap2/pm34xx.c
+++ arch/arm/mach-omap2/pm34xx.c
@@ -475,12 +475,14 @@ void omap_sram_idle(void)
 	} else
 		omap_uart_prepare_idle(2, per_next_state);
 
+	/* Disable smartreflex before entering WFI */
+	if (mpu_next_state <= PWRDM_POWER_RET)
+		disable_smartreflex(SR1);
+	if (core_next_state <= PWRDM_POWER_RET)
+		disable_smartreflex(SR2);
 
 	/* CORE */
 	if (core_next_state < PWRDM_POWER_ON) {
-		/* Disable smartreflex before entering WFI */
-		disable_smartreflex(SR1);
-		disable_smartreflex(SR2);
 		omap_uart_prepare_idle(0, core_next_state & core_logic_state);
 		omap_uart_prepare_idle(1, core_next_state & core_logic_state);
 		if (core_next_state == PWRDM_POWER_OFF) {
@@ -609,15 +611,21 @@ void omap_sram_idle(void)
 			if (prm_setup->setup_times_off != NULL)
 				prm_program_setup_times(prm_setup->setup_times);
 		}
-		/* Enable smartreflex after WFI */
-		enable_smartreflex(SR1);
-		enable_smartreflex(SR2);
 	} else {
 		omap_uart_resume_idle(0);
 		omap_uart_resume_idle(1);
 	}
 	/* Re-enable interrupt controller autoidle */
 	omap3_intc_autoidle(1);
+
+	/*
+	 * Enable smartreflex after WFI. Only needed if we entered
+	 * retention or off
+	 */
+	if (mpu_next_state <= PWRDM_POWER_RET)
+		enable_smartreflex(SR1);
+	if (core_next_state <= PWRDM_POWER_RET)
+		enable_smartreflex(SR2);
 
 	/* PER */
 	if (per_next_state < PWRDM_POWER_ON) {
