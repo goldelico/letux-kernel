5a6142393eaf4827eec14b6f9272bd000a1ab21d
Hari Nagalla <hnagalla@ti.com>
2010-03-16 15:50:19 -0600

    DSS2: Check GO-bit before handling ISR.
    This patch prevents that a VSYNC ISR can be handled
    if GO-bit for LCD has been set, this is a workaround
    in order to avoid the tearing that was observed with
    Android FS when camera preview was active and the
    camera menu was dropped up or down.
    
    Signed-off-by: Hari Nagalla <hnagalla@ti.com>
    Signed-off-by: Axel Castaneda Gonzalez <x0055901@ti.com>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -3194,6 +3194,10 @@ void dispc_irq_handler(void)
 		if (isr_data->mask & irqstatus) {
 			isr_data->isr(isr_data->arg, irqstatus);
 			handledirqs |= isr_data->mask;
+
+			if (isr_data->mask & irqstatus & DISPC_IRQ_VSYNC)
+				if (dispc_go_busy(OMAP_DSS_CHANNEL_LCD))
+					break;
 		}
 	}
 
