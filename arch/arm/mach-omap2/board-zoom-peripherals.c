3704de2aad7751bb1b4936d9121e89eb2bcac44e
Axel Haslam <axelhaslam@ti.com>
2010-04-19 06:08:43 -0500

    ZOOM3: Add display device to board file.
    This adds the Display device dev structures
    and calls panel init.
    
    Signed-off-by: Axel Haslam <axelhaslam@ti.com>

--- arch/arm/mach-omap2/board-zoom-peripherals.c
+++ arch/arm/mach-omap2/board-zoom-peripherals.c
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (C) 2009 Texas Instruments Inc.
  *
@@ -32,6 +33,10 @@
 #include "mux.h"
 
 #define OMAP_SYNAPTICS_GPIO	163
+
+extern struct regulator_init_data zoom_vdac;
+extern struct regulator_init_data zoom2_vdsi;
+extern void zoom_lcd_tv_panel_init(void);
 
 /* Zoom2 has Qwerty keyboard*/
 static int board_keymap[] = {
@@ -246,9 +251,11 @@ static struct twl4030_platform_data zoom_twldata = {
 	.gpio		= &zoom_gpio_data,
 	.keypad		= &zoom_kp_twl4030_data,
 	.codec		= &zoom_codec_data,
-	.vmmc1          = &zoom_vmmc1,
-	.vmmc2          = &zoom_vmmc2,
-	.vsim           = &zoom_vsim,
+	.vmmc1		= &zoom_vmmc1,
+	.vmmc2		= &zoom_vmmc2,
+	.vsim		= &zoom_vsim,
+	.vdac		= &zoom_vdac,
+	.vpll2		= &zoom2_vdsi,
 
 };
 
@@ -347,5 +354,6 @@ void __init zoom_peripherals_init(void)
 	omap_i2c_init();
 	synaptics_dev_init();
 	omap_serial_init();
+	zoom_lcd_tv_panel_init();
 	usb_musb_init(&musb_board_data);
 }
