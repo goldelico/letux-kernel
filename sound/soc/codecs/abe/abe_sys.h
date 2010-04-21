47f9c9e583b9ff36d5405b9baa96ff5e32a7d4e4
Leed Aguilar <leed.aguilar@ti.com>
2010-04-20 19:44:53 -0500

    ASoC: ABE: Remove Audio HAL 05.15
    This patch removes the Audio HAL 05.15 to prepare the
    path for the new Audio HAL 06.20.
    
    Signed-off-by: Leed Aguilar <leed.aguilar@ti.com>

--- sound/soc/codecs/abe/abe_sys.h
+++
@@ -1,9 +0,0 @@
-/*
- * ==========================================================================
- *               Texas Instruments OMAP(TM) Platform Firmware
- * (c) Copyright 2009, Texas Instruments Incorporated.  All Rights Reserved.
- *
- *  Use of this firmware is controlled by the terms and conditions found
- *  in the license agreement under which this firmware has been supplied.
- * ==========================================================================
- */
