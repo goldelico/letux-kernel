8c811a82a2c7d084079a9a92cabd2249e484d7aa
Moiz Sonasath <m-sonasath@ti.com>
2010-04-23 11:57:12 -0500

    OMAP3: Move the frequency defines to a standard .h file
    Move the MPU, DSP and L3 speed defines to a more general
    omap34xx.h file from the omap3-opp.h.
    
    REVISIT for OMAP4 later
    
    Signed-off-by: Moiz Sonasath <m-sonasath@ti.com>

--- arch/arm/mach-omap2/omap3-opp.h
+++ arch/arm/mach-omap2/omap3-opp.h
@@ -3,24 +3,6 @@
 
 #include <plat/omap-pm.h>
 
-/* MPU speeds */
-#define S600M   600000000
-#define S550M   550000000
-#define S500M   500000000
-#define S250M   250000000
-#define S125M   125000000
-
-/* DSP speeds */
-#define S430M   430000000
-#define S400M   400000000
-#define S360M   360000000
-#define S180M   180000000
-#define S90M    90000000
-
-/* L3 speeds */
-#define S83M    83000000
-#define S166M   166000000
-
 static struct omap_opp omap3_mpu_rate_table[] = {
 	{0, 0, 0},
 	/*OPP1*/
