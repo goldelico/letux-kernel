1757944fa053b0a0a30ab64d04bf187d67c817f9
Mike Turquette <mturquette@ti.com>
2010-04-30 15:34:01 -0500

    OMAP3630: PM: enable DVFS for OMAP 3630 silicon
    Populates OPP tables for 3630 silicon, up to 1GHz on the ARM, enables
    OPPs 1 through 5 (OPP50 through OPP1G) in the Shared Resource Framework
    and fixes a merge issue in SmartReflex code, which is needed for voltage
    scaling.
    
    Change-Id: Ib8c6c0d0c068b50214a9ace725747c9edae969c7
    Signed-off-by: Mike Turquette <mturquette@ti.com>

--- arch/arm/mach-omap2/omap3-opp.h
+++ arch/arm/mach-omap2/omap3-opp.h
@@ -4,22 +4,68 @@
 #include <plat/omap-pm.h>
 
 /* MPU speeds */
+#define S1000M	1000000000
+#define S800M	800000000
 #define S600M   600000000
 #define S550M   550000000
 #define S500M   500000000
+#define S300M	300000000
 #define S250M   250000000
 #define S125M   125000000
 
 /* DSP speeds */
+#define S800M	800000000
+#define S660M	660000000
+#define S520M	520000000
 #define S430M   430000000
 #define S400M   400000000
 #define S360M   360000000
+#define S260M	260000000
 #define S180M   180000000
 #define S90M    90000000
 
 /* L3 speeds */
-#define S83M    83000000
+#define S200M	200000000
 #define S166M   166000000
+#define S100M	100000000
+#define S83M    83000000
+
+static struct omap_opp omap3630_mpu_rate_table[] = {
+	{0, 0, 0},
+	/*OPP1 (OPP50) - 0.93mV*/
+	{S300M, VDD1_OPP1, 0x1b},
+	/*OPP2 (OPP100) - 1.1V*/
+	{S600M, VDD1_OPP2, 0x28},
+	/*OPP3 (OPP130) - 1.26V*/
+	{S800M, VDD1_OPP3, 0x35},
+	/*OPP4 (OPP-1G) - 1.35V*/
+	{S800M, VDD1_OPP4, 0x3C},
+	/*OPP5 (OPP-1G) - 1.35V*/
+	{S1000M, VDD1_OPP5, 0x3C},
+
+};
+
+static struct omap_opp omap3630_l3_rate_table[] = {
+	{0, 0, 0},
+	/*OPP1 (OPP50) - 0.93V*/
+	{S100M, VDD2_OPP1, 0x1b},
+	/*OPP2 (OPP100) - 1.1375V*/
+	{S200M, VDD2_OPP2, 0x2b},
+};
+
+static struct omap_opp omap3630_dsp_rate_table[] = {
+	{0, 0, 0},
+	/*OPP1 (OPP50) - 0.93V*/
+	{S260M, VDD1_OPP1, 0x1b},
+	/*OPP2 (OPP100) - 1.1V*/
+	{S520M, VDD1_OPP2, 0x28},
+	/*OPP3 (OPP130) - 1.26V*/
+	{S660M, VDD1_OPP3, 0x35},
+	/*OPP4 (OPP-1G) - 1.35V*/
+	{S800M, VDD1_OPP4, 0x3C},
+	/*OPP5 (OPP-1G) - 1.35V*/
+	{S400M, VDD1_OPP5, 0x3C},
+};
 
 static struct omap_opp omap3_mpu_rate_table[] = {
 	{0, 0, 0},
