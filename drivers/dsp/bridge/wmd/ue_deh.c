8d412f4b0743ab83b9b25729de7945ac5a383dc5
Shivananda Hebbar <x0hebbar@ti.com>
2010-05-12 16:05:00 -0500

    DSPBRIDGE: Use GPT7 to handle MMU Fault
    This patch allows MPU use the GPT7 to signal DSP an MMU Fault
    and start the stack dump.
    
    Signed-off-by: Armando Uribe De Leon <x0095078@ti.com>

--- drivers/dsp/bridge/wmd/ue_deh.c
+++ drivers/dsp/bridge/wmd/ue_deh.c
@@ -56,7 +56,7 @@
 #include <mach/dmtimer.h>
 
 /* GP Timer number to trigger interrupt for MMU-fault ISR on DSP */
-#define GPTIMER_FOR_DSP_MMU_FAULT	8
+#define GPTIMER_FOR_DSP_MMU_FAULT	7
 /* Bit mask to enable overflow interrupt */
 #define GPTIMER_IRQ_OVERFLOW		2
 /* Max time to check for GP Timer IRQ */
