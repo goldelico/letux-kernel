2c6ba8cbed85c60ec52c408dfa04b805cd35e605
Girish S G <girishsg@ti.com>
2010-06-30 15:07:24 -0500

    OMAP3: PM: Add support for checking if pwrdm can idle.
    pwrdm_can_idle(pwrdm) will check if the specified powerdomain can enter
    idle. This is done by checking the current fclk enable bits.
    
    This call can be used e.g. inside cpuidle to decide which power states
    core and mpu should enter during idle, as there are certain dependencies
    between wakeup capabilities and reset logic.
    
    This patch has been ported from L-O, original patch can be found here:
    http://patchwork.kernel.org/patch/73137/
    
    Change-Id: I30024e0fc2d8cdb7753d57740279bd88a5e6b975
    Signed-off-by: Girish S G <girishsg@ti.com>

--- arch/arm/mach-omap2/powerdomains34xx.h
+++ arch/arm/mach-omap2/powerdomains34xx.h
@@ -180,6 +180,7 @@ static struct powerdomain iva2_pwrdm = {
 		[2] = PWRSTS_OFF_ON,
 		[3] = PWRDM_POWER_ON,
 	},
+	.fclk_reg_amt	  = 1,
 };
 
 static struct powerdomain mpu_34xx_pwrdm = {
@@ -237,6 +238,12 @@ static struct powerdomain core_34xx_es3_1_pwrdm = {
 		[0] = PWRSTS_OFF_RET_ON, /* MEM1ONSTATE */
 		[1] = PWRSTS_OFF_RET_ON, /* MEM2ONSTATE */
 	},
+	.fclk_reg_amt	  = 2,
+	.fclk_masks	  = {
+		[0] = OMAP3430_EN_UART2 | OMAP3430_EN_UART1,
+		[1] = 0,
+	},
+
 };
 
 /* Another case of bit name collisions between several registers: EN_DSS */
@@ -256,6 +263,7 @@ static struct powerdomain dss_pwrdm = {
 	.pwrsts_mem_on	  = {
 		[0] = PWRDM_POWER_ON,  /* MEMONSTATE */
 	},
+	.fclk_reg_amt	  = 1,
 };
 
 /*
@@ -279,6 +287,7 @@ static struct powerdomain sgx_pwrdm = {
 	.pwrsts_mem_on	  = {
 		[0] = PWRDM_POWER_ON,  /* MEMONSTATE */
 	},
+	.fclk_reg_amt	  = 1,
 };
 
 static struct powerdomain cam_pwrdm = {
@@ -296,6 +305,7 @@ static struct powerdomain cam_pwrdm = {
 	.pwrsts_mem_on	  = {
 		[0] = PWRDM_POWER_ON,  /* MEMONSTATE */
 	},
+	.fclk_reg_amt	  = 1,
 };
 
 static struct powerdomain per_pwrdm = {
@@ -313,6 +323,10 @@ static struct powerdomain per_pwrdm = {
 	},
 	.pwrsts_mem_on	  = {
 		[0] = PWRDM_POWER_ON,  /* MEMONSTATE */
+	},
+	.fclk_reg_amt	  = 1,
+	.fclk_masks	  = {
+		[0] = OMAP3430_EN_UART3,
 	},
 };
 
@@ -354,6 +368,7 @@ static struct powerdomain usbhost_pwrdm = {
 	.pwrsts_mem_on	  = {
 		[0] = PWRDM_POWER_ON,  /* MEMONSTATE */
 	},
+	.fclk_reg_amt	  = 1,
 };
 
 static struct powerdomain dpll1_pwrdm = {
