c521828b94300b0afd0c8dc5dfb5c2010612e2dd
David Sin <davidsin@ti.com>
2010-07-01 11:11:49 -0500

    TILER: Move dma_alloc_coherent call to tiler init
    Instead of allocating and freeing PAT page array
    memory each time, allocate 128k upfront and reuse the
    memory.  This will avoid the possibilty of not being
    able to obtain the memory after driver initialization.
    
    Change-Id: Ia2a15e7fe0d10efa3ecf47511d892834ced19a16
    Signed-off-by: David Sin <davidsin@ti.com>

--- drivers/media/video/tiler/tiler.c
+++ drivers/media/video/tiler/tiler.c
@@ -119,6 +119,8 @@ static u32 id;
 static struct mutex mtx;
 static struct tcm *tcm[TILER_FORMATS];
 static struct tmm *tmm[TILER_FORMATS];
+static u32 *dmac_va;
+static dma_addr_t dmac_pa;
 
 #define TCM(fmt)        tcm[(fmt) - TILFMT_8BIT]
 #define TCM_SS(ssptr)   TCM(TILER_GET_ACC_MODE(ssptr))
@@ -828,16 +830,8 @@ static s32 tiler_mmap(struct file *filp, struct vm_area_struct *vma)
 static s32 refill_pat(struct tmm *tmm, struct tcm_area *area, u32 *ptr)
 {
 	s32 res = 0;
-	s32 size = tcm_sizeof(*area) * sizeof(*ptr);
-	u32 *page;
-	dma_addr_t page_pa;
 	struct pat_area p_area = {0};
 	struct tcm_area slice, area_s;
-
-	/* must be a 16-byte aligned physical address */
-	page = dma_alloc_coherent(NULL, size, &page_pa, GFP_ATOMIC);
-	if (!page)
-		return -ENOMEM;
 
 	tcm_for_each_slice(slice, *area, area_s) {
 		p_area.x0 = slice.p0.x;
@@ -845,16 +839,14 @@ static s32 refill_pat(struct tmm *tmm, struct tcm_area *area, u32 *ptr)
 		p_area.x1 = slice.p1.x;
 		p_area.y1 = slice.p1.y;
 
-		memcpy(page, ptr, sizeof(*ptr) * tcm_sizeof(slice));
+		memcpy(dmac_va, ptr, sizeof(*ptr) * tcm_sizeof(slice));
 		ptr += tcm_sizeof(slice);
 
-		if (tmm_map(tmm, p_area, page_pa)) {
+		if (tmm_map(tmm, p_area, dmac_pa)) {
 			res = -EFAULT;
 			break;
 		}
 	}
-
-	dma_free_coherent(NULL, size, page, page_pa);
 
 	return res;
 }
@@ -1422,6 +1414,9 @@ static void __exit tiler_exit(void)
 
 	mutex_unlock(&mtx);
 
+	dma_free_coherent(NULL, TILER_WIDTH * TILER_HEIGHT * sizeof(*dmac_va),
+							dmac_va, dmac_pa);
+
 	/* close containers only once */
 	for (i = TILFMT_8BIT; i <= TILFMT_MAX; i++) {
 		/* remove identical containers (tmm is unique per tcm) */
@@ -1483,6 +1478,15 @@ static s32 __init tiler_init(void)
 	struct tcm_pt div_pt;
 	struct tcm *sita = NULL;
 	struct tmm *tmm_pat = NULL;
+
+	/**
+	  * Array of physical pages for PAT programming, which must be a 16-byte
+	  * aligned physical address
+	*/
+	dmac_va = dma_alloc_coherent(NULL, TILER_WIDTH * TILER_HEIGHT *
+					sizeof(*dmac_va), &dmac_pa, GFP_ATOMIC);
+	if (!dmac_va)
+		return -ENOMEM;
 
 	/* Allocate tiler container manager (we share 1 on OMAP4) */
 	div_pt.x = TILER_WIDTH;   /* hardcoded default */
@@ -1550,6 +1554,8 @@ error:
 		kfree(tiler_device);
 		tcm_deinit(sita);
 		tmm_deinit(tmm_pat);
+		dma_free_coherent(NULL, TILER_WIDTH * TILER_HEIGHT *
+					sizeof(*dmac_va), dmac_va, dmac_pa);
 	}
 
 	return r;
