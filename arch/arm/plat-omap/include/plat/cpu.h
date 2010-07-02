3011cf1ae8aaa9a647b9cf8a3d09e2ee34931277
Vikram Pandita <vikram.pandita@ti.com>
2010-07-02 08:46:13 -0500

    omap4: Update id.c and cpu.h for es2.0
    ES2.0 4430 detection code:
    Should print: "OMAP4430 ES2.0" on bootup on es2 chip
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Signed-off-by: Vikram Pandita <vikram.pandita@ti.com>

--- arch/arm/plat-omap/include/plat/cpu.h
+++ arch/arm/plat-omap/include/plat/cpu.h
@@ -455,6 +455,7 @@ IS_OMAP_TYPE(3517, 0x3517)
 
 #define OMAP443X_CLASS		0x44300044
 #define OMAP4430_REV_ES1_0	0x44300044
+#define OMAP4430_REV_ES2_0	0x44301044
 
 /*
  * omap_chip bits
@@ -478,9 +479,13 @@ IS_OMAP_TYPE(3517, 0x3517)
 #define CHIP_IS_OMAP3430ES3_0		(1 << 5)
 #define CHIP_IS_OMAP3430ES3_1		(1 << 6)
 #define CHIP_IS_OMAP3630ES1		(1 << 7)
-#define CHIP_IS_OMAP4430		(1 << 8)
+#define CHIP_IS_OMAP4430ES1		(1 << 8)
+#define CHIP_IS_OMAP4430ES2		(1 << 9)
 
 #define CHIP_IS_OMAP24XX		(CHIP_IS_OMAP2420 | CHIP_IS_OMAP2430)
+
+#define CHIP_IS_OMAP4430		(CHIP_IS_OMAP4430ES1 | \
+						CHIP_IS_OMAP4430ES2)
 
 /*
  * "GE" here represents "greater than or equal to" in terms of ES
