76a4f149d26f6cd93402c860bbd599d27771349e
Mike Turquette <mturquette@ti.com>
2010-07-12 14:35:55 -0500

    OMAP3: PM: VDD1 OPP table change
    Change the OPP table as per latest h/w recommentdation values
    OPP1 to OPP3 will remain the same.
    
    	ARM (MHz)	DSP (MHz)
    OPP4	800		800
    OPP5	1000		400
    
    By _default_ the system will run at OPP4.
    To support OPP5 in the system more patch will follow on top of
    this commit, which requires changes in DSP as well.
    
    Signed-off-by: Girish S G <girishsg@ti.com>
    (cherry picked from commit c15b536d3ed14aab8a0a961d2cda19d55dc98753)
    
    Conflicts:
    
    	arch/arm/mach-omap2/omap3-opp.h
    	arch/arm/plat-omap/omap-pm-srf.c
    
    Change-Id: I459f32888cb92a42226dffe49bf9aed2047db700
    Signed-off-by: Mike Turquette <mturquette@ti.com>

--- arch/arm/mach-omap2/omap3-opp.h
+++ arch/arm/mach-omap2/omap3-opp.h
@@ -39,8 +39,11 @@ static struct omap_opp omap3630_mpu_rate_table[] = {
 	{S600M, VDD1_OPP2, 0x28},
 	/*OPP3 (OPP130) - 1.26V*/
 	{S800M, VDD1_OPP3, 0x35},
-	/*OPP4 (OPP1G) - 1.35V*/
-	{S1000M, VDD1_OPP4, 0x3c},
+	/*OPP4 (OPP-1G) - 1.35V*/
+	{S800M, VDD1_OPP4, 0x3C},
+	/*OPP5 (OPP-1G) - 1.35V*/
+	{S1000M, VDD1_OPP5, 0x3C},
+
 };
 
 static struct omap_opp omap3630_l3_rate_table[] = {
@@ -59,8 +62,10 @@ static struct omap_opp omap3630_dsp_rate_table[] = {
 	{S520M, VDD1_OPP2, 0x28},
 	/*OPP3 (OPP130) - 1.26V*/
 	{S660M, VDD1_OPP3, 0x35},
-	/*OPP4 (OPP1G) - 1.35V*/
-	{S800M, VDD1_OPP4, 0x3c},
+	/*OPP4 (OPP-1G) - 1.35V*/
+	{S800M, VDD1_OPP4, 0x3C},
+	/*OPP5 (OPP-1G) - 1.35V*/
+	{S400M, VDD1_OPP5, 0x3C},
 };
 
 static struct omap_opp omap3_mpu_rate_table[] = {
