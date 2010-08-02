c3035cca8a587a2cf6e74ebc11a09a1a66f1a99d
Hari Kanigeri <h-kanigeri2@ti.com>
2010-08-02 11:45:49 -0500

    Revert "SYSLINK: iommu temp hack set twl to false by default"
    This reverts commit 630b121d90d7a186b2cf6717ef19a61ddad015da.
    
    Change-Id: Ia1b75bcc5ec491644532b5fd306974f6c639895e
    Signed-off-by: Hari Kanigeri <h-kanigeri2@ti.com>

--- arch/arm/plat-omap/iommu.c
+++ arch/arm/plat-omap/iommu.c
@@ -933,10 +933,6 @@ static int omap_iommu_open(struct inode *inode, struct file *filp)
 
 	filp->private_data = obj;
 	iommu_get(obj->name);
-	/* Hack for now: This is required for ES1.0 until TWL
-	 * fix is available.
-	 */
-	iommu_set_twl(obj, false);
 
 	return ret;
 }
