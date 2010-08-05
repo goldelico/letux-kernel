df7314927d06c227a62b18e8241c0451f71c6e7d
Angela Stegmaier <angelabaker@ti.com>
2010-08-04 20:37:48 -0500

    omap:iommu-disable the MMU interrupts in fault ISR
    Instead of disabling the MMU device, just disable the MMU interrupts
    when the MMU fault occurs. This change is required because the user
    applications might be releasing the MMU resources after the MMU fault
    recovery and this requires MMU to be in enabled state. MMU will be
    disabled after all the open handles to IOMMU are closed.
    
    Change-Id: Iec83e9f4ab37050a54065d625ef0d4552c7cd24a
    Signed-off-by: Hari Kanigeri <h-kanigeri2@ti.com>
    Signed-off-by: Angela Stegmaier <angelabaker@ti.com>

--- arch/arm/mach-omap2/iommu2.c
+++ arch/arm/mach-omap2/iommu2.c
@@ -164,9 +164,8 @@ static u32 omap2_iommu_fault_isr(struct iommu *obj, u32 *ra)
 	}
 	printk("\n");
 
-	iommu_write_reg(obj, stat, MMU_IRQSTATUS);
-	/* Disable MMU to stop continuous generation of MMU faults */
-	omap2_iommu_disable(obj);
+	/* Disable further interrupts */
+	iommu_write_reg(obj, 0, MMU_IRQENABLE);
 	return stat;
 }
 
