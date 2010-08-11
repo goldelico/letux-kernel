f79198c1b8615190ccfbf777bbf3f4665ec37889
Girish S G <girishsg@ti.com>
2010-08-11 11:35:16 -0500

    OMAP3:PM:BUG scale VDD2 OPP to Max when VDD1 runs above OPP3
    VDD2 should be bummped to max when ARM runs above OPP3.
    
    Signed-off-by: Girish S G <girishsg@ti.com>

--- arch/arm/mach-omap2/resource34xx.c
+++ arch/arm/mach-omap2/resource34xx.c
@@ -401,10 +401,11 @@ int set_opp(struct shared_resource *resp, u32 target_level)
 		/*
 		 * For VDD1 OPP3 and above, make sure the interconnect
 		 * is at 100Mhz or above.
-		 * throughput in KiB/s for 100 Mhz = 100 * 1000 * 4.
+		 * throughput in KiB/s for 200 Mhz = 200 * 1000 * 4.
 		 */
 		if (target_level >= 3)
-			resource_request("vdd2_opp", &vdd2_dev, 400000);
+			resource_request("vdd2_opp", &vdd2_dev,
+				4 * l3_opps[omap_pm_get_max_vdd2_opp()].rate/1000);
 
 	} else if (resp == vdd2_resp) {
 		tput = target_level;
