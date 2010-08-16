347e6064ed2509507ae67cd66b7b2a76aecaa25b
Shivananda Hebbar <x0hebbar@ti.com>
2010-08-16 17:55:30 -0500

    remoteproc- Add Tesla support to remoteproc44xx
    Added Tesla start and stop functionality to remoteproc44xx.
    
    [Original Patch]
    Signed-off-by: Angela Stegmaier <angelabaker@ti.com>
    
    Change-Id: I42eccc809e9e75e7d89c7a7de9e09468b84f71a0
    [Ported-to-Android]
    Signed-off-by: Shivananda Hebbar <x0hebbar@ti.com>

--- arch/arm/mach-omap2/remoteproc44xx.c
+++ arch/arm/mach-omap2/remoteproc44xx.c
@@ -32,6 +32,15 @@
 #include "cm.h"
 #include "prm.h"
 
+#define RM_TESLA_RST1ST				0x1
+#define RM_TESLA_REL_RST1_MASK			0x0
+#define RM_TESLA_AST_RST1_MASK			0x1
+
+#define TESLA_CLK_MOD_MODE_HW_AUTO		0x1
+#define TESLA_CLKTRCTRL_SW_WKUP			0x2
+#define TESLA_CLKTRCTRL_SW_SLEEP		0x1
+#define TESLA_CLKACTIVITY_MPU_TESLA_CLK		0x100
+
 #define RM_M3_RST1ST				0x1
 #define RM_M3_RST2ST				0x2
 #define RM_M3_RST3ST				0x4
@@ -50,6 +59,12 @@
 #define OMAP4_RM_DUCATI_RSTCTRL_OFFSET		0x0210
 #define OMAP4_RM_DUCATI_RSTST_OFFSET		0x0214
 #define OMAP4430_CM2_CORE_MOD_1			0x4700
+
+#define OMAP4_CM_TESLA_CLKSTCTRL_OFFSET	0x0000
+#define OMAP4_CM1_ABE_TIMER5_CLKCTRL_OFFSET	0x0068
+#define OMAP4_RM_TESLA_RSTCTRL_OFFSET		0x0010
+#define OMAP4_RM_TESLA_RSTST_OFFSET		0x0014
+#define OMAP4_CM_TESLA_TESLA_CLKCTRL_OFFSET	0x0020
 
 static inline int proc44x_sysm3_start(struct omap_rproc *rproc)
 {
@@ -114,6 +129,52 @@ static inline int proc44x_appm3_start(struct omap_rproc *rproc)
 	return 0;
 }
 
+static inline int proc44x_tesla_start(struct omap_rproc *rproc)
+{
+	u32 reg;
+	int counter = 10;
+	struct device *dev = rproc->dev;
+
+	/* Module is managed automatically by HW */
+	cm_write_mod_reg(TESLA_CLK_MOD_MODE_HW_AUTO, OMAP4430_CM1_TESLA_MOD,
+				OMAP4_CM_TESLA_TESLA_CLKCTRL_OFFSET);
+
+	/* Enable the M3 clock */
+	cm_write_mod_reg(TESLA_CLKTRCTRL_SW_WKUP, OMAP4430_CM1_TESLA_MOD,
+			OMAP4_CM_TESLA_CLKSTCTRL_OFFSET);
+	do {
+		reg = cm_read_mod_reg(OMAP4430_CM1_TESLA_MOD,
+				OMAP4_CM_TESLA_CLKSTCTRL_OFFSET);
+		if (reg & TESLA_CLKACTIVITY_MPU_TESLA_CLK) {
+			dev_info(dev, "TESLA clock enabled:"
+				"OMAP4430_CM_TESLA_CLKSTCTRL = 0x%x\n", reg);
+			break;
+		}
+
+		msleep(1);
+	} while (--counter);
+
+	if (counter == 0) {
+		dev_info(dev, "FAILED TO ENABLE TESLA CLOCK !%x\n", reg);
+		return -EFAULT;
+	}
+	/* De-assert RST1, and clear the Reset status */
+	dev_info(dev, "De-assert RST1\n");
+	prm_write_mod_reg(RM_TESLA_REL_RST1_MASK, OMAP4430_PRM_TESLA_MOD,
+				OMAP4_RM_TESLA_RSTCTRL_OFFSET);
+
+while (!(prm_read_mod_reg(OMAP4430_PRM_TESLA_MOD,
+		 OMAP4_RM_TESLA_RSTST_OFFSET) & RM_TESLA_RST1ST))
+		;
+
+	dev_info(dev, "RST1 released!");
+
+	prm_write_mod_reg(RM_TESLA_RST1ST, OMAP4430_PRM_TESLA_MOD,
+				OMAP4_RM_TESLA_RSTST_OFFSET);
+
+	return 0;
+}
+
 static inline int proc44x_sysm3_stop(struct omap_rproc *rproc)
 {
 	struct device *dev = rproc->dev;
@@ -143,6 +204,19 @@ static inline int proc44x_appm3_stop(struct omap_rproc *rproc)
 	return 0;
 }
 
+static inline int proc44x_tesla_stop(struct omap_rproc *rproc)
+{
+	struct device *dev = rproc->dev;
+	u32 reg;
+	reg = prm_read_mod_reg(OMAP4430_PRM_CORE_MOD,
+			OMAP4_RM_TESLA_RSTCTRL_OFFSET);
+
+	dev_info(dev, "assert RST1 reg = 0x%x\n", reg);
+	prm_write_mod_reg((reg | RM_TESLA_AST_RST1_MASK), OMAP4430_PRM_CORE_MOD,
+				OMAP4_RM_TESLA_RSTCTRL_OFFSET);
+	return 0;
+}
+
 static inline int omap4_rproc_get_state(struct omap_rproc *rproc)
 {
 	return rproc->state;
@@ -161,8 +235,9 @@ static struct omap_rproc_ops omap4_ducati1_ops = {
 };
 
 static struct omap_rproc_ops omap4_tesla_ops = {
-	.start = NULL,
-	.stop = NULL,
+	.start = proc44x_tesla_start,
+	.stop = proc44x_tesla_stop,
+	.get_state = omap4_rproc_get_state,
 };
 
 static struct omap_rproc_platform_data omap4_rproc_data[] = {
