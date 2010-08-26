16cf6a8799b367b1b45cc3fce93a3542a81e4376
Axel Haslam <axelhaslam@ti.com>
2010-08-26 11:58:14 -0500

    KW: OMAP3: serial_core: add null pointer check
    This patch adds some null pointer checks.
    
    Change-Id: I99eb027b132b2a4341812e50130f6accb25c856b
    Signed-off-by: Axel Haslam <axelhaslam@ti.com>

--- drivers/serial/serial_core.c
+++ drivers/serial/serial_core.c
@@ -2016,7 +2016,7 @@ int uart_suspend_port(struct uart_driver *drv, struct uart_port *uport)
 	}
 
 	tty_dev = device_find_child(uport->dev, &match, serial_match_port);
-	if (device_may_wakeup(tty_dev)) {
+	if (tty_dev && device_may_wakeup(tty_dev)) {
 		enable_irq_wake(uport->irq);
 		put_device(tty_dev);
 		mutex_unlock(&port->mutex);
@@ -2100,7 +2100,7 @@ int uart_resume_port(struct uart_driver *drv, struct uart_port *uport)
 	}
 
 	tty_dev = device_find_child(uport->dev, &match, serial_match_port);
-	if (!uport->suspended && device_may_wakeup(tty_dev)) {
+	if (tty_dev && !uport->suspended && device_may_wakeup(tty_dev)) {
 		disable_irq_wake(uport->irq);
 		mutex_unlock(&port->mutex);
 		return 0;
