4338b51578c7840bcadf0a809a59815a29e339ac
Dan Murphy <dmurphy@ti.com>
2010-09-09 08:20:51 -0500

    input/misc: HMC5843 add schedule work queues and enable attribute
    1. Added schedule work queues to the driver instead of input polled devices.
    2. Added an enable attribute so that the reporting can be enabled and disabled for the scheduled
    work queues.
    3. Modified the rate interface to accept Android times and match to the rate array in the
    driver.
    
    Change-Id: Ia997ba7ca6160bfc6662ed3f40df137f8ccdb4f9
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- drivers/input/misc/hmc5843.c
+++ drivers/input/misc/hmc5843.c
@@ -18,11 +18,14 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#define HMC5843_USE_WORK_QUEUES	1
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/jiffies.h>
 #include <linux/err.h>
 #include <linux/i2c.h>
+#include <linux/input.h>
 #include <linux/input-polldev.h>
 
 #define	HMC5843_CFG_A_REG			0
@@ -53,12 +56,16 @@
 struct hmc5843 {
 	struct i2c_client	*client;
 	struct mutex		lock;
+	struct input_dev *input_dev;
+	struct delayed_work input_work;
 	struct input_polled_dev	*ipdev;
 	int			bias;
 	int			gain;
 	int			rate;
 	int			mode;
 	int			index;
+
+	int 			req_poll_rate;
 };
 
 /* interval between samples for the different rates, in msecs */
@@ -66,6 +73,9 @@ static const unsigned int hmc5843_sample_interval[] = {
 	1000 * 2,	1000,		1000 / 2,	1000 / 5,
 	1000 / 10,	1000 / 20,	1000 / 50,
 };
+
+static uint32_t mag_debug;
+module_param_named(hmc5843_debug, mag_debug, uint, 0664);
 
 /*
  * From the datasheet:
@@ -208,13 +218,38 @@ static ssize_t hmc5843_store_rate(struct device *dev,
 	struct hmc5843 *hmc5843 = i2c_get_clientdata(client);
 	unsigned long val;
 	int status = count;
+	int i;
 
-	if ((strict_strtol(buf, 10, &val) < 0) || (val > 6))
+	if (strict_strtol(buf, 10, &val))
 		return -EINVAL;
 	mutex_lock(&hmc5843->lock);
 	if (hmc5843->rate != val) {
 		hmc5843->rate = val;
-		hmc5843->ipdev->poll_interval = hmc5843_sample_interval[val];
+#ifdef HMC5843_USE_WORK_QUEUES
+		if (val == 0) {
+			/* Set to the fastest speed */
+			i = (ARRAY_SIZE(hmc5843_sample_interval) - 1);
+		} else {
+			for (i = 0; i < ARRAY_SIZE(hmc5843_sample_interval); i++) {
+				if (hmc5843_sample_interval[i] == val)
+					break;
+				/* The array is from slowest to fastest */
+				if ((hmc5843_sample_interval[i] < val) &&
+				(hmc5843_sample_interval[i + 1] > val))
+					break;
+			}
+		}
+		if (mag_debug)
+			dev_info(&hmc5843->client->dev, "%s:Rate picked is %d\n",
+				__func__, hmc5843_sample_interval[i]);
+
+		hmc5843->req_poll_rate = hmc5843_sample_interval[i];
+#else
+		if ((val < 0) || (val < 6))
+			hmc5843->ipdev->poll_interval = hmc5843_sample_interval[val];
+		else
+			return -EINVAL;
+#endif
 		status = hmc5843_write_register(hmc5843, HMC5843_CFG_A_REG);
 	}
 	mutex_unlock(&hmc5843->lock);
@@ -292,11 +327,40 @@ static ssize_t hmc5843_store_mode(struct device *dev,
 static DEVICE_ATTR(mode, S_IWUSR | S_IRUGO, hmc5843_show_mode,
 		hmc5843_store_mode);
 
+static ssize_t hmc5843_store_enable(struct device *dev,
+				     struct device_attribute *attr,
+				     const char *buf, size_t count)
+{
+	struct i2c_client *client = to_i2c_client(dev);
+	struct hmc5843 *hmc5843 = i2c_get_clientdata(client);
+	unsigned long val;
+	int error;
+
+#ifdef HMC5843_USE_WORK_QUEUES
+	error = strict_strtoul(buf, 0, &val);
+	if (error)
+		return error;
+
+	if (val < 0 || val > 1)
+		return -EINVAL;
+
+	if (val == 0)
+		cancel_delayed_work_sync(&hmc5843->input_work);
+	else
+		schedule_delayed_work(&hmc5843->input_work, 0);
+#endif
+
+	return 1;
+}
+static DEVICE_ATTR(enable, S_IWUSR | S_IRUGO, NULL,
+		hmc5843_store_enable);
+
 static struct attribute *hmc5843_attributes[] = {
 	&dev_attr_bias.attr,
 	&dev_attr_rate.attr,
 	&dev_attr_gain.attr,
 	&dev_attr_mode.attr,
+	&dev_attr_enable.attr,
 	NULL,
 };
 
@@ -304,7 +368,73 @@ static const struct attribute_group hmc5843_attr_group = {
 	.attrs = hmc5843_attributes,
 };
 
-/* use polled input device */
+#ifdef HMC5843_USE_WORK_QUEUES
+static void hmc5843_read_report_data(struct hmc5843 *data)
+{
+	int x = 0;
+	int y = 0;
+	int z = 0;
+
+	mutex_lock(&data->lock);
+	if (!hmc5843_read_xyz(data, &x, &y, &z)) {
+		if (mag_debug)
+			dev_info(&data->client->dev,
+				"%s:Reporting x= %d, y= %d, z= %d\n",
+				__func__, x, y, z);
+		input_report_abs(data->input_dev, ABS_HAT0X, x);
+		input_report_abs(data->input_dev, ABS_HAT0Y, y);
+		input_report_abs(data->input_dev, ABS_BRAKE, z);
+		input_sync(data->input_dev);
+	}
+	mutex_unlock(&data->lock);
+}
+
+static void hmc5843_input_work_func(struct work_struct *work)
+{
+	struct hmc5843 *hmc = container_of((struct delayed_work *)work,
+						  struct hmc5843,
+						  input_work);
+
+	hmc5843_read_report_data(hmc);
+	schedule_delayed_work(&hmc->input_work,
+		msecs_to_jiffies(hmc->req_poll_rate));
+}
+
+static int __devinit hmc5843_input_init(struct hmc5843 *hmc5843)
+{
+	int ret;
+
+	hmc5843->input_dev = input_allocate_device();
+	if (hmc5843->input_dev == NULL) {
+		dev_err(&hmc5843->client->dev,
+			"Failed to allocate input device\n");
+		return -ENOMEM;
+	}
+
+	hmc5843->input_dev->name = "hmc5843";
+	hmc5843->input_dev->id.bustype = BUS_I2C;
+
+	 __set_bit(EV_ABS, hmc5843->input_dev->evbit);
+
+	input_set_abs_params(hmc5843->input_dev, ABS_HAT0X, -2047, 2047, 0, 0);
+	input_set_abs_params(hmc5843->input_dev, ABS_HAT0Y, -2047, 2047, 0, 0);
+	input_set_abs_params(hmc5843->input_dev, ABS_BRAKE, -2047, 2047, 0, 0);
+
+	ret = input_register_device(hmc5843->input_dev);
+	if (ret) {
+		dev_err(&hmc5843->client->dev,
+			"Unable to register input device\n");
+		input_free_device(hmc5843->input_dev);
+		return ret;
+	}
+	return 0;
+}
+static void hmc5843_unregister_input_device(struct hmc5843 *hmc5843)
+{
+	input_unregister_device(hmc5843->input_dev);
+}
+
+#else /* use polled input device */
 
 static void hmc5843_poll(struct input_polled_dev *ipdev)
 {
@@ -356,6 +486,12 @@ static int __devinit hmc5843_input_init(struct hmc5843 *hmc5843)
 exit:
 	return status;
 }
+
+static void hmc5843_unregister_input_device(struct hmc5843 *hmc5843)
+{
+	input_unregister_polled_device(hmc5843->ipdev);
+}
+#endif
 
 static int __devinit hmc5843_device_init(struct hmc5843 *hmc5843)
 {
@@ -414,6 +550,8 @@ static int __devinit hmc5843_i2c_probe(struct i2c_client *client,
 	hmc5843->client = client;
 	i2c_set_clientdata(client, hmc5843);
 
+	hmc5843->req_poll_rate = 200;
+
 	status = hmc5843_device_init(hmc5843);
 	if (status)
 		goto fail0;
@@ -427,9 +565,10 @@ static int __devinit hmc5843_i2c_probe(struct i2c_client *client,
 		dev_dbg(&client->dev, "could not create sysfs files\n");
 		goto fail1;
 	}
+	INIT_DELAYED_WORK(&hmc5843->input_work, hmc5843_input_work_func);
 	return 0;
 fail1:
-	input_unregister_polled_device(hmc5843->ipdev);
+	hmc5843_unregister_input_device(hmc5843);
 fail0:
 	kfree(hmc5843);
 	return status;
@@ -440,7 +579,7 @@ static int __devexit hmc5843_i2c_remove(struct i2c_client *client)
 	struct hmc5843 *hmc5843 = i2c_get_clientdata(client);
 
 	sysfs_remove_group(&client->dev.kobj, &hmc5843_attr_group);
-	input_unregister_polled_device(hmc5843->ipdev);
+	hmc5843_unregister_input_device(hmc5843);
 	i2c_set_clientdata(client, NULL);
 	kfree(hmc5843);
 	return 0;
