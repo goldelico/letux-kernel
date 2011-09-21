c0bb74c4df01383e02b405908edcd96ac7bb897f
sricharan <r.sricharan@ti.com>
2011-09-21 16:08:46 +0530

    ARM: OMAP: hwmod: Fix the addr spaces, irq, dma count APIs.
    The address spaces, irqs and dma reqs count API returns the
    number of corresponding entries in a hwmod including a additional
    null value or a -1 terminator in the structure introduced
    recently. More information here:
    
            212738a4 (omap_hwmod: use a terminator record with omap_hwmod_mpu_irqs arrays)
            78183f3f (omap_hwmod: use a null structure record to terminate omap_hwmod_addr_space arrays)
            bc614958 (omap_hwmod: use a terminator record with omap_hwmod_dma_info arrays)
    
    The issue with irqs and dma info was originally reported by Benoit Cousson.
    
    The devices which have multiple hwmods and use device_build_ss are
    broken with this, as their resources are populated with a
    extra null value, subsequently the probe fails. So fix the API not to
    include the array terminator in the count.
    
    Reported-by: Benoit Cousson <b-cousson@ti.com>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Signed-off-by: R Sricharan <r.sricharan@ti.com>
    Cc: Benoit Cousson <b-cousson@ti.com>
    Cc: Paul Walmsley <paul@pwsan.com>
    Cc: Kevin Hilman <khilman@ti.com>

--- arch/arm/mach-omap2/omap_hwmod.c
+++ arch/arm/mach-omap2/omap_hwmod.c
@@ -785,7 +785,7 @@ static int _count_mpu_irqs(struct omap_hwmod *oh)
 		ohii = &oh->mpu_irqs[i++];
 	} while (ohii->irq != -1);
 
-	return i;
+	return i-1;
 }
 
 /**
@@ -808,7 +808,7 @@ static int _count_sdma_reqs(struct omap_hwmod *oh)
 		ohdi = &oh->sdma_reqs[i++];
 	} while (ohdi->dma_req != -1);
 
-	return i;
+	return i-1;
 }
 
 /**
@@ -831,7 +831,7 @@ static int _count_ocp_if_addr_spaces(struct omap_hwmod_ocp_if *os)
 		mem = &os->addr[i++];
 	} while (mem->pa_start != mem->pa_end);
 
-	return i;
+	return i-1;
 }
 
 /**
