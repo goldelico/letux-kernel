a232b2cde612538520239bf33eaa6a4c0dbbe88a
Mike Turquette <mturquette@ti.com>
2011-10-17 15:40:08 -0700

    omap: abb: add PRM_IRQSTATUS tranxdone handlers
    Some PRM_IRQSTATUS registers contain a bit to signal that an ABB LDO
    transition has completed.  These tranxdone bits exist for every instance
    of an ABB LDO; thus these tranxdone bits are supported on 36xx variants
    for the MPU voltage domain, and on 44xx variants for the MPU and IVA
    voltage domains.
    
    This patch introduces some data structures and helper functions for
    checking on and clearing the TRANXDONE bits.  They are a copy-paste of
    existing VP functions of similar nature and will be used in forthcoming
    patches that control the ABB programming sequence during a DVFS
    transition.
    
    In the v2 patch series for ABB the VP tranxdone handlers were made
    generic so that only one set of data and handlers existed for both VP
    and ABB.  That was NAK'd so this patch re-implements this via
    duplication of code/data.  Original post can be see at:
    http://article.gmane.org/gmane.linux.ports.arm.omap/63609
    
    Signed-off-by: Mike Turquette <mturquette@ti.com>

--- arch/arm/mach-omap2/prm2xxx_3xxx.c
+++ arch/arm/mach-omap2/prm2xxx_3xxx.c
@@ -212,3 +212,39 @@ u32 omap3_prm_vcvp_rmw(u32 mask, u32 bits, u8 offset)
 {
 	return omap2_prm_rmw_mod_reg_bits(mask, bits, OMAP3430_GR_MOD, offset);
 }
+
+/* PRM ABB */
+
+/*
+ * struct omap36xx_abb - OMAP3 ABB register access description
+ * @tranxdone_status: ABB_xxx_DONE_ST bitmask in PRM_IRQSTATUS reg
+ */
+struct omap36xx_abb {
+	u32 tranxdone_status;
+};
+
+static struct omap36xx_abb omap36xx_abb[] = {
+	[OMAP3_VP_VDD_MPU_ID] = {
+		.tranxdone_status = OMAP3630_ABB_LDO_TRANXDONE_ST_MASK,
+	},
+};
+
+#define MAX_ABB_ID ARRAY_SIZE(omap36xx_abb);
+
+u32 omap3_prm_abb_check_txdone(u8 abb_id)
+{
+	struct omap36xx_abb *abb = &omap36xx_abb[abb_id];
+	u32 irqstatus;
+
+	irqstatus = omap2_prm_read_mod_reg(OCP_MOD,
+					   OMAP3_PRM_IRQSTATUS_MPU_OFFSET);
+	return irqstatus & abb->tranxdone_status;
+}
+
+void omap3_prm_abb_clear_txdone(u8 abb_id)
+{
+	struct omap36xx_abb *abb = &omap36xx_abb[abb_id];
+
+	omap2_prm_write_mod_reg(abb->tranxdone_status,
+				OCP_MOD, OMAP3_PRM_IRQSTATUS_MPU_OFFSET);
+}
