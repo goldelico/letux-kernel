a4b65661babfc8f000934564a55568159bba23ca
Tero Kristo <t-kristo@ti.com>
2011-11-18 09:54:45 +0200

    omap: pm: wait for domain wakeup if changing state of idle domain
    If we are switching the state of an idle powerdomain, we must wait for
    the wakeup to complete before attempting to switch to the new state,
    otherwise the powerdomain may not be ready for the switch and will fail.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- arch/arm/mach-omap2/pm.c
+++ arch/arm/mach-omap2/pm.c
@@ -176,6 +176,7 @@ int omap_set_pwrdm_state(struct powerdomain *pwrdm, u32 state)
 		} else {
 			hwsup = clkdm_in_hwsup(pwrdm->pwrdm_clkdms[0]);
 			clkdm_wakeup(pwrdm->pwrdm_clkdms[0]);
+			pwrdm_wait_transition(pwrdm);
 			sleep_switch = FORCEWAKEUP_SWITCH;
 		}
 	}
