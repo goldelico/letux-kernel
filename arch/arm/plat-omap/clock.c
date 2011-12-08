4955b29202806788725c00f19aaf83175a5ac0dd
Todd Poynor <toddpoynor@google.com>
2011-12-08 14:59:13 +0530

    OMAP: PM: Lock clocks list while generating summary
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- arch/arm/plat-omap/clock.c
+++ arch/arm/plat-omap/clock.c
@@ -488,6 +488,7 @@ static int clk_dbg_show_summary(struct seq_file *s, void *unused)
 	struct clk *c;
 	struct clk *pa;
 
+	mutex_lock(&clocks_mutex);
 	seq_printf(s, "%-30s %-30s %-10s %s\n",
 		"clock-name", "parent-name", "rate", "use-count");
 
@@ -496,6 +497,7 @@ static int clk_dbg_show_summary(struct seq_file *s, void *unused)
 		seq_printf(s, "%-30s %-30s %-10lu %d\n",
 			c->name, pa ? pa->name : "none", c->rate, c->usecount);
 	}
+	mutex_unlock(&clocks_mutex);
 
 	return 0;
 }
