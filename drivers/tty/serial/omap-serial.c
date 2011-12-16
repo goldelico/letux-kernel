6437d6c82cb09a43c23cd9aaca93d8551b527dee
Govindraj.R <govindraj.raja@ti.com>
2011-12-16 14:37:57 +0530

    OAMP2+: UART: Make module wakeup configurable from board file
    Different boards use different uart lines to connect client devices.
    Based on lines used module level wakeup configuration can be set
    or enabled from board file, same is context restored if context lost
    while waking up from low power state. By default all wakeups are enabled
    as done prior to this patch. However one can configure wakeups based on
    the board requirements.
    
    Signed-off-by: Govindraj.R <govindraj.raja@ti.com>

--- drivers/tty/serial/omap-serial.c
+++ drivers/tty/serial/omap-serial.c
@@ -561,7 +561,7 @@ static int serial_omap_startup(struct uart_port *port)
 	serial_out(up, UART_IER, up->ier);
 
 	/* Enable module level wake up */
-	serial_out(up, UART_OMAP_WER, OMAP_UART_WER_MOD_WKUP);
+	serial_out(up, UART_OMAP_WER, up->wer);
 
 	pm_runtime_mark_last_busy(&up->pdev->dev);
 	pm_runtime_put_autosuspend(&up->pdev->dev);
@@ -1394,6 +1394,7 @@ static int serial_omap_probe(struct platform_device *pdev)
 	up->port.uartclk = omap_up_info->uartclk;
 	up->uart_dma.uart_base = mem->start;
 	up->errata = omap_up_info->errata;
+	up->wer = omap_up_info->wer;
 
 	if (omap_up_info->dma_enabled) {
 		up->uart_dma.uart_dma_tx = dma_tx->start;
@@ -1518,6 +1519,7 @@ static void serial_omap_restore_context(struct uart_omap_port *up)
 		serial_omap_mdr1_errataset(up, up->mdr1);
 	else
 		serial_out(up, UART_OMAP_MDR1, up->mdr1);
+	serial_out(up, UART_OMAP_WER, up->wer);
 }
 
 #ifdef CONFIG_PM_RUNTIME
