00ab6f877fac7df4208c88dafd03ef9776f182ef
Vishwanath BS <vishwanath.bs@ti.com>
2012-01-03 12:03:38 +0530

    OMAP OPP: Remove iva and dsp opp entires in pm standalone build
    In PM standalone build, remove iva and dsp opps since they cause
    unnecessary warnings in boot up. Also commenting out dsp hwmos
    for PM standalone build.
    
    Signed-off-by: Vishwanath BS <vishwanath.bs@ti.com>

--- arch/arm/mach-omap2/pm.c
+++ arch/arm/mach-omap2/pm.c
@@ -96,8 +96,10 @@ static void omap2_init_processor_devices(void)
 
 	if (cpu_is_omap44xx() || cpu_is_omap54xx()) {
 		_init_omap_device("l3_main_1", &l3_dev);
+#ifndef CONFIG_OMAP_PM_STANDALONE
 		_init_omap_device("dsp", &dsp_dev);
 		_init_omap_device("iva", &iva_dev);
+#endif
 	} else {
 		_init_omap_device("l3_main", &l3_dev);
 	}
@@ -429,7 +431,9 @@ static void __init omap5_init_voltages(void)
 
 	omap2_set_init_voltage("mpu", "virt_dpll_mpu_ck", mpu_dev);
 	omap2_set_init_voltage("core", "virt_l3_ck", l3_dev);
+#ifndef CONFIG_OMAP_PM_STANDALONE
 	omap2_set_init_voltage("mm", "dpll_iva_h12x2_ck", iva_dev);
+#endif
 }
 
 static int __init omap2_common_pm_init(void)
