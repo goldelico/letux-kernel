9e4cfddd336ca31382a18cbb1d833bcace54665a
Rajendra Nayak <rnayak@ti.com>
2012-01-04 10:37:15 +0100

    ARM: omap: hwmod: Fix unwanted warns at boot
    Complain only if clkdm association is missing from
    both clock node as well as hwmod node. On OMAP4+, the
    clkdm association is moved to hwmod from the clock.
    
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>

--- arch/arm/mach-omap2/omap_hwmod.c
+++ arch/arm/mach-omap2/omap_hwmod.c
@@ -519,11 +519,9 @@ static int _init_main_clk(struct omap_hwmod *oh)
 			   oh->name, oh->main_clk);
 		return -EINVAL;
 	}
-#ifndef CONFIG_MACH_OMAP_5430ZEBU
 	if (!oh->_clk->clkdm && !oh->clkdm_name)
 		pr_warning("omap_hwmod: %s: missing clockdomain for %s.\n",
 			   oh->main_clk, oh->_clk->name);
-#endif
 	return ret;
 }
 
