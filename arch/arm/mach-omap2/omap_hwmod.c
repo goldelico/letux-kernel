5f7782a1a776af91b6787be870f5eb91af9f5b8e
Rajendra Nayak <rnayak@ti.com>
2012-01-07 13:46:21 +0100

    ARM: OMAP5: hwmod: Disable modules for omap5 post hwmod setup
    Disable modules for omap5 post hwmod setup
    
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>

--- arch/arm/mach-omap2/omap_hwmod.c
+++ arch/arm/mach-omap2/omap_hwmod.c
@@ -714,7 +714,7 @@ static void _enable_module(struct omap_hwmod *oh)
  */
 static int _omap4_wait_target_disable(struct omap_hwmod *oh)
 {
-	if (!cpu_is_omap44xx())
+	if (!cpu_is_omap44xx() && !cpu_is_omap54xx())
 		return 0;
 
 	if (!oh)
@@ -744,7 +744,7 @@ static int _omap4_disable_module(struct omap_hwmod *oh)
 	int v;
 
 	/* The module mode does not exist prior OMAP4 */
-	if (!cpu_is_omap44xx())
+	if (!cpu_is_omap44xx() && !cpu_is_omap54xx())
 		return -EINVAL;
 
 	if (!oh->clkdm || !oh->prcm.omap4.modulemode)
