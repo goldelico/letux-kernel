a86d234feb992f935b6cddf65ac57f66b560b33c
Santosh Shilimkar <santosh.shilimkar@ti.com>
2012-01-20 12:12:53 +0100

    OMAP4460: Workaround for ROM bug because of CA9 r2pX gic control register change
    GIC distributor control register has changed between CortexA9 r1pX and r2pX.
    The Control Register secure banked version is now composed of 2 bits:
         bit 0 == Secure Enable
         bit 1 == Non-Secure Enable
    The Non-Secure banked register has not changed. Since the ROM Code is
    based on the r1pX GIC, the CPU1 GIC restoration will cause a problem
    to CPU0 Non-Secure SW.
    The workaround must be:
         1) Before doing the CPU1 wakeup, CPU0 must disable
            the GIC distributor
         2) CPU1 must re-enable the GIC distributor on
            it's wakeup path.
    
    With this procedure, the GIC configuration done between the
    CPU0 wakeup and CPU1 wakeup will not be lost but during this
    short windows, the CPU0 will not receive interrupts
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Signed-off-by: Vishwanath BS <vishwanath.bs@ti.com>

--- arch/arm/mach-omap2/include/mach/omap4-common.h
+++ arch/arm/mach-omap2/include/mach/omap4-common.h
@@ -55,8 +55,9 @@ extern int omap_mpuss_init(void);
 extern int omap_enter_lowpower(unsigned int cpu, unsigned int power_state);
 extern int omap_hotplug_cpu(unsigned int cpu, unsigned int power_state);
 extern u32 omap_mpuss_read_prev_context_state(void);
-extern void mpuss_timer_setup(unsigned long freq);
 extern void omap_mpuss_timer_init(void);
+extern void gic_dist_enable(void);
+extern void gic_dist_disable(void);
 #else
 static inline int omap_enter_lowpower(unsigned int cpu,
 					unsigned int power_state)
