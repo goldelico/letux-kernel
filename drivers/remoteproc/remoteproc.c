2e5585951c1bfe7273c71c107a79e05398fad343
Subramaniam C.A <subramaniam.ca@ti.com>
2012-01-30 10:18:26 -0600

    remoteproc: create version debugfs after image is loaded
    This patch ensures that the image version debugfs entry is
    created only after the image is loaded. Version info is
    not available before the image is loaded.
    
    If the debugfs entry is created before, then it results in
    a kernel panic.
    
    Change-Id: I69f6ad33acbc5908a8ffdad96389c5602ada6336
    Signed-off-by: Subramaniam C.A <subramaniam.ca@ti.com>

--- drivers/remoteproc/remoteproc.c
+++ drivers/remoteproc/remoteproc.c
@@ -1136,6 +1136,9 @@ static void rproc_loader_defered(struct rproc *rproc)
 	memcpy(rproc->header, image->header, image->header_len);
 	rproc->header_len = image->header_len;
 
+	debugfs_create_file("version", 0444, rproc->dbg_dir, rproc,
+							&rproc_version_ops);
+
 	/* Ensure we recognize this BIOS version: */
 	if (image->version != RPROC_BIOS_VERSION) {
 		dev_err(dev, "Expected BIOS version: %d!\n",
@@ -1719,8 +1722,6 @@ int rproc_register(struct device *dev, const char *name,
 	debugfs_create_file("name", 0444, rproc->dbg_dir, rproc,
 							&rproc_name_ops);
 
-	debugfs_create_file("version", 0444, rproc->dbg_dir, rproc,
-							&rproc_version_ops);
 out:
 	return 0;
 }
