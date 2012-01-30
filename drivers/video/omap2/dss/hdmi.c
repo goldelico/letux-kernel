ccafbd8f8206b7ed657fbe9bb6eded0d1afb12e6
Mythri P K <mythripk@ti.com>
2012-01-30 14:24:14 +0530

    OMAPDSS: HDMI: Changes in OMAP5 PLL calcula
    M2 divider should be used in OMAP5 HDMI PLL calculation.
    Also the value must be 2 to get frequencies below 50Mz.
    
    Signed-off-by: Mythri P K <mythripk@ti.com>
    Signed-off-by: Archit Taneja <archit@ti.com>

--- drivers/video/omap2/dss/hdmi.c
+++ drivers/video/omap2/dss/hdmi.c
@@ -290,24 +290,32 @@ static void hdmi_compute_pll(struct omap_dss_device *dssdev, int phy,
 
 	refclk = clkin / pi->regn;
 
+	if (dssdev->clocks.hdmi.regm2 == 0) {
+		if (cpu_is_omap44xx()) {
+			pi->regm2 = HDMI_DEFAULT_REGM2;
+		} else if (cpu_is_omap54xx()) {
+			if (phy <= 50000)
+				pi->regm2 = 2;
+			else
+				pi->regm2 = 1;
+		}
+	} else {
+		pi->regm2 = dssdev->clocks.hdmi.regm2;
+	}
+
 	/*
 	 * multiplier is pixel_clk/ref_clk
 	 * Multiplying by 100 to avoid fractional part removal
 	 */
-	pi->regm = (phy * 100 / (refclk)) / 100;
 
-	if (dssdev->clocks.hdmi.regm2 == 0)
-		pi->regm2 = HDMI_DEFAULT_REGM2;
-	else
-		pi->regm2 = dssdev->clocks.hdmi.regm2;
-
+	pi->regm = (phy * 100 * pi->regm2 / refclk) / 100;
 	/*
 	 * fractional multiplier is remainder of the difference between
 	 * multiplier and actual phy(required pixel clock thus should be
 	 * multiplied by 2^18(262144) divided by the reference clock
 	 */
-	mf = (phy - pi->regm * refclk) * 262144;
-	pi->regmf = mf / (refclk);
+	mf = (phy - pi->regm / pi->regm2 * refclk) * 262144;
+	pi->regmf = pi->regm2 * mf / refclk;
 
 	/*
 	 * Dcofreq should be set to 1 if required pixel clock
