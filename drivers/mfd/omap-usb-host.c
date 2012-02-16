fce5bdda416b2b30b269a87f31dfd3542c3554f1
Govindraj.R <govindraj.raja@ti.com>
2012-02-16 20:20:10 +0530

    usb: uhh: enable/disable wakeup capabilities around uhh clock gating
    Use hwmod framework to enable/disable module/pad wakeup capabilities.
    Move tll clock gating inside uhh_lock so all clock handling's are done
    with host lock held.
    
    Signed-off-by: Govindraj.R <govindraj.raja@ti.com>

--- drivers/mfd/omap-usb-host.c
+++ drivers/mfd/omap-usb-host.c
@@ -25,6 +25,7 @@
 #include <linux/dma-mapping.h>
 #include <linux/spinlock.h>
 #include <plat/usb.h>
+#include <plat/omap_device.h>
 #include <linux/pm_runtime.h>
 
 #define USBHS_DRIVER_NAME	"usbhs_omap"
@@ -669,6 +670,8 @@ static int __devexit usbhs_omap_remove(struct platform_device *pdev)
 
 static int usbhs_runtime_resume(struct device *dev)
 {
+	struct platform_device *pdev = to_platform_device(dev);
+	struct omap_device *od = to_omap_device(pdev);
 	struct usbhs_hcd_omap		*omap = dev_get_drvdata(dev);
 	struct usbhs_omap_platform_data	*pdata = &omap->platdata;
 	unsigned long			flags;
@@ -680,8 +683,8 @@ static int usbhs_runtime_resume(struct device *dev)
 		return  -ENODEV;
 	}
 
-	omap_tll_enable();
 	spin_lock_irqsave(&omap->lock, flags);
+	omap_tll_enable();
 
 	if (omap->ehci_logic_fck && !IS_ERR(omap->ehci_logic_fck))
 		clk_enable(omap->ehci_logic_fck);
@@ -716,12 +719,16 @@ static int usbhs_runtime_resume(struct device *dev)
 	clk_enable(omap->utmi_p1_fck);
 	clk_enable(omap->utmi_p2_fck);
 
+	omap_hwmod_disable_wakeup(od->hwmods[0]);
+
 	spin_unlock_irqrestore(&omap->lock, flags);
 	return 0;
 }
 
 static int usbhs_runtime_suspend(struct device *dev)
 {
+	struct platform_device *pdev = to_platform_device(dev);
+	struct omap_device *od = to_omap_device(pdev);
 	struct usbhs_hcd_omap		*omap = dev_get_drvdata(dev);
 	struct usbhs_omap_platform_data	*pdata = &omap->platdata;
 	unsigned long			flags;
@@ -735,6 +742,7 @@ static int usbhs_runtime_suspend(struct device *dev)
 
 	spin_lock_irqsave(&omap->lock, flags);
 
+	omap_hwmod_enable_wakeup(od->hwmods[0]);
 	if (is_ehci_tll_mode(pdata->port_mode[0]) ||
 			is_ehci_hsic_mode(pdata->port_mode[0]))
 		clk_disable(omap->usbhost_p1_fck);
@@ -767,8 +775,9 @@ static int usbhs_runtime_suspend(struct device *dev)
 	if (omap->ehci_logic_fck && !IS_ERR(omap->ehci_logic_fck))
 		clk_disable(omap->ehci_logic_fck);
 
-	spin_unlock_irqrestore(&omap->lock, flags);
 	omap_tll_disable();
+
+	spin_unlock_irqrestore(&omap->lock, flags);
 
 	return 0;
 }
