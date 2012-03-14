8f156ca3f0c5cac387de8424e664793cfdbf772f
Balaji T K <balajitk@ti.com>
2012-03-14 20:49:04 +0530

    mmc: set constraint to prevent mmc ip clock switch
    When MMC transaction is active, switching ip clock when OPP changes
    results in increasing or decreasing clock frequency to the card
    beyond the card supported frequency.
    Set constraint to prevent IP clock switch between 96MHz and 192MHz
    
    Signed-off-by: Balaji T K <balajitk@ti.com>

--- drivers/mmc/host/omap_hsmmc.c
+++ drivers/mmc/host/omap_hsmmc.c
@@ -35,6 +35,7 @@
 #include <linux/gpio.h>
 #include <linux/regulator/consumer.h>
 #include <linux/pm_runtime.h>
+#include <linux/pm_qos.h>
 #include <plat/dma.h>
 #include <mach/hardware.h>
 #include <plat/board.h>
@@ -167,6 +168,7 @@ struct omap_hsmmc_host {
 	struct	mmc_data	*data;
 	struct	clk		*fclk;
 	struct	clk		*dbclk;
+	struct pm_qos_request   pm_qos_request;
 	/*
 	 * vcc == configured supply
 	 * vcc_aux == optional
@@ -1750,6 +1752,9 @@ static void omap_hsmmc_set_ios(struct mmc_host *mmc, struct mmc_ios *ios)
 	struct omap_hsmmc_host *host = mmc_priv(mmc);
 	int do_send_init_stream = 0;
 
+	if ((mmc_slot(host).features & HSMMC_DVFS) && (ios->clock))
+		pm_qos_update_request(&host->pm_qos_request, 540 * 1000);
+
 	pm_runtime_get_sync(host->dev);
 
 	if (ios->power_mode != host->power_mode) {
@@ -2155,6 +2160,7 @@ static int __init omap_hsmmc_probe(struct platform_device *pdev)
 
 	mmc->caps |= MMC_CAP_DISABLE;
 
+	pm_qos_add_request(&host->pm_qos_request, PM_QOS_MEMORY_THROUGHPUT, 0);
 	pm_runtime_enable(host->dev);
 	pm_runtime_get_sync(host->dev);
 	pm_runtime_set_autosuspend_delay(host->dev, MMC_AUTOSUSPEND_DELAY);
@@ -2339,6 +2345,7 @@ static int omap_hsmmc_remove(struct platform_device *pdev)
 
 		pm_runtime_put_sync(host->dev);
 		pm_runtime_disable(host->dev);
+		pm_qos_remove_request(&host->pm_qos_request);
 		clk_put(host->fclk);
 		if (host->got_dbclk) {
 			clk_disable(host->dbclk);
@@ -2458,6 +2465,8 @@ static int omap_hsmmc_runtime_suspend(struct device *dev)
 	host = platform_get_drvdata(to_platform_device(dev));
 	omap_hsmmc_context_save(host);
 	dev_dbg(mmc_dev(host->mmc), "disabled\n");
+	if ((mmc_slot(host).features & HSMMC_DVFS))
+		pm_qos_update_request(&host->pm_qos_request, 0);
 
 	return 0;
 }
@@ -2467,6 +2476,10 @@ static int omap_hsmmc_runtime_resume(struct device *dev)
 	struct omap_hsmmc_host *host;
 
 	host = platform_get_drvdata(to_platform_device(dev));
+
+	if ((mmc_slot(host).features & HSMMC_DVFS))
+		pm_qos_update_request(&host->pm_qos_request, 540 * 1000);
+
 	omap_hsmmc_context_restore(host);
 	dev_dbg(mmc_dev(host->mmc), "enabled\n");
 
