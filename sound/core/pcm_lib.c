224c67efed4e2b660f21e59caf37debcf4aa4433
Misael Lopez Cruz <misael.lopez@ti.com>
2012-04-16 14:18:18 +0200

    ALSA: pcm: export 'snd_interval_step' symbol
    Export 'snd_interval_step' symbol so that ALSA drivers can use it
    to add hw rules.
    
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>

--- sound/core/pcm_lib.c
+++ sound/core/pcm_lib.c
@@ -1077,7 +1077,7 @@ int snd_interval_ranges(struct snd_interval *i, unsigned int count,
 }
 EXPORT_SYMBOL(snd_interval_ranges);
 
-static int snd_interval_step(struct snd_interval *i, unsigned int step)
+int snd_interval_step(struct snd_interval *i, unsigned int step)
 {
 	unsigned int n;
 	int changed = 0;
@@ -1099,6 +1099,7 @@ static int snd_interval_step(struct snd_interval *i, unsigned int step)
 	}
 	return changed;
 }
+EXPORT_SYMBOL(snd_interval_step);
 
 /* Info constraints helpers */
 
