476cc9f3a83b7ce1810423e93eb808b835809a1d
Andreas Kemnade <andreas@kemnade.info>
2012-07-23 10:42:56 +1000

    codec hack for GTA04A4
    hacked twl4030 codec to tristate the Voice PCM channel so that it
    works on the GTA04A4 board; needs to be solved better for a production
    kernel
    
    based on patch by "Dr. H. Nikolaus Schaller" <hns@goldelico.com>
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- sound/soc/codecs/twl4030.c
+++ sound/soc/codecs/twl4030.c
@@ -339,6 +339,14 @@ static void twl4030_init_chip(struct snd_soc_codec *codec)
 		 ((byte & TWL4030_CNCL_OFFSET_START) ==
 		  TWL4030_CNCL_OFFSET_START));
 
+	/* Make sure that the reg_cache has the same value as the HW */
+	twl4030_write_reg_cache(codec, TWL4030_REG_ANAMICL, byte);
+	twl_i2c_write_u8(TWL4030_MODULE_AUDIO_VOICE,
+                                        TWL4030_VIF_TRI_EN,
+                                        TWL4030_REG_VOICE_IF);
+        printk("TPS Voice IF set to tristate\n");
+
+
 	twl4030_codec_enable(codec, 0);
 }
 
@@ -2096,7 +2104,8 @@ static int twl4030_voice_set_dai_fmt(struct snd_soc_dai *codec_dai,
 static int twl4030_voice_set_tristate(struct snd_soc_dai *dai, int tristate)
 {
 	struct snd_soc_codec *codec = dai->codec;
-	u8 reg = twl4030_read(codec, TWL4030_REG_VOICE_IF);
+	u8 reg = twl4030_read_reg_cache(codec, TWL4030_REG_VOICE_IF);
+	printk("twl4030_voice_set_tristate codec=%p\n", codec);
 
 	if (tristate)
 		reg |= TWL4030_VIF_TRI_EN;
