0631927543e681f9dcbed5f371b09e1166f62446
NeilBrown <neilb@suse.de>
2012-08-04 22:09:47 +1000

    BMP085: Clean up and enable use of interrupt for completion.
    - discard init_platform_hw and exit_platform_hw as they aren't
      used
    - pass GPIO or IRQ to driver and have it initialise the GPIO and
      the IRQ
    - finish waiting early if interrupt fires
    - clean up GPIO and IRQ on exit.
    
    Signed-off-by: NeilBrown <neilb@suse.de>
    
    Conflicts:
    	arch/arm/configs/gta04_debug_defconfig
    	arch/arm/configs/gta04_defconfig
    	drivers/misc/bmp085.c

--- include/linux/i2c/bmp085.h
+++ include/linux/i2c/bmp085.h
@@ -3,11 +3,14 @@
 
 /* linux/i2c/bmp085.h */
 
+/* If GPIO is set, it is the End Of Conversion line which
+ * is High when conversion is finished.
+ * if it is <0, and irq > 0, then it is an interrupt with no
+ * GPIO.
+ */
 struct bmp085_platform_data {
+	int	gpio;
 	int	irq;
-
-        int     (*init_platform_hw)(void);
-        void    (*exit_platform_hw)(void);
 };
 
 #endif
