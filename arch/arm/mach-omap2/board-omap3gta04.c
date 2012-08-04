f0defa60f8b54ef3864867e1a76c64651dd7276c
NeilBrown <neilb@suse.de>
2012-08-05 07:52:58 +1000

    Fix some compiler warnings.
    Mostly about __initdata section mismatches.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- arch/arm/mach-omap2/board-omap3gta04.c
+++ arch/arm/mach-omap2/board-omap3gta04.c
@@ -1003,7 +1003,7 @@ struct tsc2007_platform_data __initdata tsc2007_info = {
 
 #define BMP085_EOC_IRQ_GPIO		113	/* BMP085 end of conversion GPIO */
 
-struct bmp085_platform_data bmp085_info = {
+struct bmp085_platform_data __initdata bmp085_info = {
 	.gpio = BMP085_EOC_IRQ_GPIO,
 };
 
