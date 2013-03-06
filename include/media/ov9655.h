2d04b2ba0b5f1d63d049b2188c29ffee6a82ce85
H. Nikolaus Schaller <hns@goldelico.com>
2013-03-06 15:38:06 +0100

    created fresh ov9655 driver that probes and controls power/clock correctly - but does not yet write any register through I2C

--- include/media/ov9655.h
+++ include/media/ov9655.h
@@ -1,3 +1,39 @@
+#ifndef __OV9655_H
+#define __OV9655_H
+
+struct v4l2_subdev;
+
+struct ov9655_platform_data {
+	/* from OV7670 - why do we ned that? */
+	int min_width;			/* Filter out smaller sizes */
+	int min_height;			/* Filter out smaller sizes */
+	int clock_speed;		/* External clock speed (MHz) */
+	bool use_smbus;			/* Use smbus I/O instead of I2C */
+	
+	/*
+	* struct mt9p031_platform_data - MT9P031 platform data
+	* @set_xclk: Clock frequency set callback
+	* @reset: Chip reset GPIO (set to -1 if not used)
+	* @ext_freq: Input clock frequency
+	* @target_freq: Pixel clock frequency
+	*/
+	int (*set_xclk)(struct v4l2_subdev *subdev, int hz);
+	int reset;	/* GPIO */
+	int ext_freq;
+	int target_freq;
+};
+
+#endif
+
+
+
+
+
+
+
+#ifdef OLD
+
+
 /*
  * drivers/media/video/ov9655.h
  *
@@ -72,3 +108,4 @@ struct ov9655_platform_data {
 
 #endif				/* ifndef _OV9655_H */
 
+#endif /* OLD */
