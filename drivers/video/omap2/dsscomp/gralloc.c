b2f041555e6b68c9b2fb5d08dcfc5e1e912f6d0e
Sergiy Kibrik <sergiy.kibrik@globallogic.com>
2013-04-03 15:19:23 -0500

    OMAPDSS: DSSCOMP: enable alpha blending at suspend
    set_dss_mgr_info() refuses to accept configurations without
    alpha blending enabled, thus in the very last composition
    queued in suspend handler we have to explicitly enable it.
    
    Change-Id: Ic663e9d7166e7386998cda3233791482adead0bd
    Signed-off-by: Arthur Philpott <arthur.philpott@ti.com>

--- drivers/video/omap2/dsscomp/gralloc.c
+++ drivers/video/omap2/dsscomp/gralloc.c
@@ -529,6 +529,7 @@ static void dsscomp_early_suspend(struct early_suspend *h)
 		if (dssdev && dssdev->state == OMAP_DSS_DISPLAY_ACTIVE) {
 			d.num_mgrs++;
 			d.mgrs[mgr_ix].ix = mgr_ix;
+			d.mgrs[mgr_ix].alpha_blending = true;
 		}
 	}
 
