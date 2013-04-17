2b54722ef62af8690af8e179e1f027270a8a2c90
Hemant Hariyani <hemanthariyani@ti.com>
2013-04-17 10:21:18 -0500

    Revert "gpu: HACK : add dummy declaration of VM reserved"
    This reverts commit a20fc2c7539217a8c5a8d70872be5497aef4119b.
    
    Change-Id: I3936c1c5b6e4c1f6b1eb3bb914c39428736edc42
    Signed-off-by: Hemant Hariyani <hemanthariyani@ti.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -107,7 +107,6 @@ extern unsigned int kobjsize(const void *objp);
 #define VM_HUGEPAGE	0x20000000	/* MADV_HUGEPAGE marked this vma */
 #define VM_NOHUGEPAGE	0x40000000	/* MADV_NOHUGEPAGE marked this vma */
 #define VM_MERGEABLE	0x80000000	/* KSM may merge identical pages */
-#define VM_RESERVED  	0x00080000      /* Count as reserved_vm like IO */
 
 #if defined(CONFIG_X86)
 # define VM_PAT		VM_ARCH_1	/* PAT reserves whole VMA at once (x86) */
