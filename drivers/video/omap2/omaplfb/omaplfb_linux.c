bf49a1b336ea0b3441704fb81c82bd4e5b729d20
Dandawate Saket <dsaket@ti.com>
2013-05-06 09:25:25 -0700

    OMAPLFB: Fix bad manager pointer
    Fix bad manager pointer in omaplfb
    
    Change-Id: I5c7c2fd513c2bbb36375165d7bf30a10ab7e9058
    Signed-off-by: Dandawate Saket <dsaket@ti.com>

--- drivers/video/omap2/omaplfb/omaplfb_linux.c
+++ drivers/video/omap2/omaplfb/omaplfb_linux.c
@@ -102,7 +102,7 @@ MODULE_SUPPORTED_DEVICE(DEVNAME);
 #if !defined(PVR_OMAPLFB_DRM_FB)
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,34))
 #define OMAP_DSS_DRIVER(drv, dev) struct omap_dss_driver *drv = (dev) != NULL ? (dev)->driver : NULL
-#define OMAP_DSS_MANAGER(man, dev) struct omap_overlay_manager *man = (dev) != NULL ? (dev)->output : NULL
+#define OMAP_DSS_MANAGER(man, dev) struct omap_overlay_manager *man = (dev) != NULL ? (dev)->output->manager : NULL
 #define	WAIT_FOR_VSYNC(man)	((man)->wait_for_vsync)
 #else
 #define OMAP_DSS_DRIVER(drv, dev) struct omap_dss_device *drv = (dev)
@@ -702,7 +702,7 @@ OMAPLFB_BOOL OMAPLFBWaitForVSync(OMAPLFB_DEVINFO *psDevInfo)
 	}
 
 	return OMAPLFB_TRUE;
-#else	
+#else
 	struct omap_dss_device *psDSSDev = fb2display(psDevInfo->psLINFBInfo);
 	OMAP_DSS_MANAGER(psDSSMan, psDSSDev);
 
