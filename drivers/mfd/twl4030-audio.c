41b6f0bf40869f08e6ea6500445bfab2e9a30864
Linus Torvalds <torvalds@linux-foundation.org>
2013-05-15 14:08:57 -0700

    Merge tag 'trace-fixes-v3.10-rc1' of git://git.kernel.org/pub/scm/linux/kernel/git/rostedt/linux-trace
    Pull tracing fixes from Steven Rostedt:
     "This includes a fix to a memory leak when adding filters to traces.
    
      Also, Masami Hiramatsu fixed up some minor bugs that were discovered
      by sparse."
    
    * tag 'trace-fixes-v3.10-rc1' of git://git.kernel.org/pub/scm/linux/kernel/git/rostedt/linux-trace:
      tracing/kprobes: Make print_*probe_event static
      tracing/kprobes: Fix a sparse warning for incorrect type in assignment
      tracing/kprobes: Use rcu_dereference_raw for tp->files
      tracing: Fix leaks of filter preds

--- drivers/mfd/twl4030-audio.c
+++ drivers/mfd/twl4030-audio.c
@@ -30,7 +30,7 @@
 #include <linux/platform_device.h>
 #include <linux/of.h>
 #include <linux/of_platform.h>
-#include <linux/mfd/twl.h>
+#include <linux/i2c/twl.h>
 #include <linux/mfd/core.h>
 #include <linux/mfd/twl4030-audio.h>
 
@@ -192,7 +192,7 @@ static bool twl4030_audio_has_vibra(struct twl4030_audio_data *pdata,
 static int twl4030_audio_probe(struct platform_device *pdev)
 {
 	struct twl4030_audio *audio;
-	struct twl4030_audio_data *pdata = dev_get_platdata(&pdev->dev);
+	struct twl4030_audio_data *pdata = pdev->dev.platform_data;
 	struct device_node *node = pdev->dev.of_node;
 	struct mfd_cell *cell = NULL;
 	int ret, childs = 0;
@@ -266,8 +266,10 @@ static int twl4030_audio_probe(struct platform_device *pdev)
 		ret = -ENODEV;
 	}
 
-	if (ret)
+	if (ret) {
+		platform_set_drvdata(pdev, NULL);
 		twl4030_audio_dev = NULL;
+	}
 
 	return ret;
 }
@@ -275,6 +277,7 @@ static int twl4030_audio_probe(struct platform_device *pdev)
 static int twl4030_audio_remove(struct platform_device *pdev)
 {
 	mfd_remove_devices(&pdev->dev);
+	platform_set_drvdata(pdev, NULL);
 	twl4030_audio_dev = NULL;
 
 	return 0;
@@ -288,6 +291,7 @@ MODULE_DEVICE_TABLE(of, twl4030_audio_of_match);
 
 static struct platform_driver twl4030_audio_driver = {
 	.driver		= {
+		.owner	= THIS_MODULE,
 		.name	= "twl4030-audio",
 		.of_match_table = twl4030_audio_of_match,
 	},
