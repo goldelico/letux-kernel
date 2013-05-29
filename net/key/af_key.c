b8ef5acf17759d0a6aa99f84fd5c958a808188c6
Paul Moore <pmoore@redhat.com>
2013-05-29 07:36:25 +0000

    xfrm: force a garbage collection after deleting a policy
    In some cases after deleting a policy from the SPD the policy would
    remain in the dst/flow/route cache for an extended period of time
    which caused problems for SELinux as its dynamic network access
    controls key off of the number of XFRM policy and state entries.
    This patch corrects this problem by forcing a XFRM garbage collection
    whenever a policy is sucessfully removed.
    
    [ cileija@ti.com: backport ]
    [ upstream commit e4c1721642bbd42d8142f4811cde0588c28db51d ]
    
    Change-Id: I7f51b8b10f9f660698d7abe24f033a283fd28849
    Reported-by: Ondrej Moris <omoris@redhat.com>
    Signed-off-by: Paul Moore <pmoore@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Carlos Leija <cileija@ti.com>

--- net/key/af_key.c
+++ net/key/af_key.c
@@ -2361,6 +2361,8 @@ static int pfkey_spddelete(struct sock *sk, struct sk_buff *skb, const struct sa
 
 out:
 	xfrm_pol_put(xp);
+	if (err == 0)
+		xfrm_garbage_collect(net);
 	return err;
 }
 
@@ -2610,6 +2612,8 @@ static int pfkey_spdget(struct sock *sk, struct sk_buff *skb, const struct sadb_
 
 out:
 	xfrm_pol_put(xp);
+	if (delete && err == 0)
+		xfrm_garbage_collect(net);
 	return err;
 }
 
