7ac053aa387d900173b4b0b3f627fdaac388e0cc
Marek Belisko <marek.belisko@open-nandra.com>
2013-07-17 23:24:19 +0200

    video: td028ttec1: Add possibility to pass spi gpio via platform data.
    Fixing issue 186: http://projects.goldelico.com/p/gta04-kernel/issues/186/
    
    Signed-off-by: Marek Belisko <marek.belisko@open-nandra.com>

---
+++ include/video/omap-panel-tpo-td028ttec1.h
@@ -0,0 +1,11 @@
+#ifndef __OMAP_PANEL_TD028TTEC1_H
+#define __OMAP_PANEL_TD028TTEC1_H
+
+struct panel_td028ttec1_data {
+	int gpio_cs;
+	int gpio_scl;
+	int gpio_din;
+	int gpio_dout;	
+};
+
+#endif /* __OMAP_PANEL_TD028TTEC1_H  */
