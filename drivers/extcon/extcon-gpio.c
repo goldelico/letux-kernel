6e3be14b78b86b884f628068c1627d62daf2cd95
H. Nikolaus Schaller <hns@goldelico.com>
2014-01-06 15:36:50 +0100

    fixed issue with extcon-gpio on 3.13 kernels (gpio must be requested before we can set the debounce time)
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/extcon/extcon-gpio.c
+++ drivers/extcon/extcon-gpio.c
@@ -145,14 +145,6 @@ static int gpio_extcon_probe(struct platform_device *pdev)
 	extcon_data->state_off = pdata->state_off;
 	if (pdata->state_on && pdata->state_off)
 		extcon_data->edev.print_state = extcon_gpio_print_state;
-	if (pdata->debounce) {
-		ret = gpio_set_debounce(extcon_data->gpio,
-					pdata->debounce * 1000);
-		if (ret < 0)
-			extcon_data->debounce_jiffies =
-				msecs_to_jiffies(pdata->debounce);
-	}
-
 	ret = extcon_dev_register(&extcon_data->edev);
 	if (ret < 0)
 		return ret;
@@ -162,6 +154,14 @@ static int gpio_extcon_probe(struct platform_device *pdev)
 	if (ret < 0)
 		goto err;
 
+	if (pdata->debounce) {
+		ret = gpio_set_debounce(extcon_data->gpio,
+								pdata->debounce * 1000);
+		if (ret < 0)
+			extcon_data->debounce_jiffies =
+			msecs_to_jiffies(pdata->debounce);
+	}
+	
 	INIT_DELAYED_WORK(&extcon_data->work, gpio_extcon_work);
 
 	extcon_data->irq = gpio_to_irq(extcon_data->gpio);
