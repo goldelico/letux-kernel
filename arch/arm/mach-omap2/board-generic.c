d53fb1732f0f34351b556a49d940b408cc1b0582
Rajendra Nayak <rnayak@ti.com>
2014-03-13 20:11:40 +0530

    ARM: OMAP2+: Add machine entry for dra72x devices
    The only difference from the dra74x devices is the missing .smp entry.
    
    Change-Id: Iabccf9118956d14f70c7eae809f1e265bacab3e2
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>
    [subramaniam.ca@ti.com: minor changes, ported to kernel 3.8]
    Signed-off-by: Subramaniam Chanderashekarapuram <subramaniam.ca@ti.com>

--- arch/arm/mach-omap2/board-generic.c
+++ arch/arm/mach-omap2/board-generic.c
@@ -258,12 +258,13 @@ MACHINE_END
 #endif
 
 #ifdef CONFIG_SOC_DRA7XX
-static const char *dra7xx_boards_compat[] __initconst = {
+static const char *dra74x_boards_compat[] __initconst = {
+	"ti,dra742",
 	"ti,dra7",
 	NULL,
 };
 
-DT_MACHINE_START(DRA7XX_DT, "Jacinto6 evm board")
+DT_MACHINE_START(DRA74X_DT, "Jacinto6 evm board")
 	.reserve	= dra7_reserve,
 	.smp		= smp_ops(omap4_smp_ops),
 	.map_io		= omap5_map_io,
@@ -273,7 +274,25 @@ DT_MACHINE_START(DRA7XX_DT, "Jacinto6 evm board")
 	.init_machine	= omap_generic_init,
 	.init_late	= dra7xx_init_late,
 	.timer		= &omap5_timer,
-	.dt_compat	= dra7xx_boards_compat,
+	.dt_compat	= dra74x_boards_compat,
+	.restart	= omap44xx_restart,
+MACHINE_END
+
+static const char *dra72x_boards_compat[] __initconst = {
+	"ti,dra722",
+	NULL,
+};
+
+DT_MACHINE_START(DRA72X_DT, "Jacinto6 evm board")
+	.reserve	= dra7_reserve,
+	.map_io		= omap5_map_io,
+	.init_early	= dra7xx_init_early,
+	.init_irq	= omap_gic_of_init,
+	.handle_irq	= gic_handle_irq,
+	.init_machine	= omap_generic_init,
+	.init_late	= dra7xx_init_late,
+	.timer		= &omap5_timer,
+	.dt_compat	= dra72x_boards_compat,
 	.restart	= omap44xx_restart,
 MACHINE_END
 #endif
