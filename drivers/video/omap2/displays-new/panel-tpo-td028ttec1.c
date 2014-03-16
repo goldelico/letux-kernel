22e96d4e74b473283df8c5a9db4cc051f1ac27ca
H. Nikolaus Schaller <hns@goldelico.com>
2014-03-16 15:50:33 +0100

    add a spi: alias to panel driver so that it is auto-loaded by DTB based kernel
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/video/omap2/displays-new/panel-tpo-td028ttec1.c
+++ drivers/video/omap2/displays-new/panel-tpo-td028ttec1.c
@@ -505,6 +505,8 @@ static struct spi_driver td028ttec1_spi_driver = {
 
 module_spi_driver(td028ttec1_spi_driver);
 
+MODULE_ALIAS("spi:td028ttec1");	/* this is "spi:" plus the second component of the "compatible=" entry */
+
 MODULE_AUTHOR("H. Nikolaus Schaller <hns@goldelico.com>");
 MODULE_DESCRIPTION("Toppoly TD028TTEC1 panel driver");
 MODULE_LICENSE("GPL");
