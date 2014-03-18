0aa59b0d94767ae65625be6afd9ff2cebe334021
Sundar Raman <sunds@ti.com>
2014-03-18 15:23:23 -0500

    v4l: ti-vps: vip: Register the video device only if sensor is probed
    The V4L devices should be registered only if the sensor is probed.
    Move the registration logic to async bound callback from sensor
    so that we are sure that the sensor is present before registering
    the device.
    
    Change-Id: Iaff635af794a42fe3a0c9ca7f9385314d4cd318e
    Signed-off-by: Sundar Raman <sunds@ti.com>

--- drivers/media/platform/ti-vps/vip.c
+++ drivers/media/platform/ti-vps/vip.c
@@ -1792,21 +1792,13 @@ static int alloc_stream(struct vip_port *port, int stream_id, int vfl_type)
 	*vfd = vip_videodev;
 	vfd->v4l2_dev = &dev->v4l2_dev;
 	vfd->queue = q;
+	vfd->num = -1;
 
 	vfd->lock = &dev->mutex;
 	video_set_drvdata(vfd, stream);
 
-	ret = video_register_device(vfd, vfl_type, -1);
-	if (ret) {
-		v4l2_err(&dev->v4l2_dev, "Failed to register video device\n");
-		goto do_free_stream;
-	}
-
-	snprintf(vfd->name, sizeof(vfd->name), "%s", vip_videodev.name);
 	stream->vfd = vfd;
 
-	v4l2_info(&dev->v4l2_dev, VIP_MODULE_NAME
-			" Device registered as /dev/video%d\n", vfd->num);
 	return 0;
 
 do_free_stream:
@@ -1965,6 +1957,8 @@ static int vip_async_bound(struct v4l2_async_notifier *notifier,
 {
 	struct vip_dev *dev = notifier_to_vip_dev(notifier);
 	unsigned int idx = asd - &dev->config->asd[0];
+	struct video_device *vfd;
+	int ret;
 
 	if (idx > dev->config->asd_sizes)
 		return -EINVAL;
@@ -1979,6 +1973,21 @@ static int vip_async_bound(struct v4l2_async_notifier *notifier,
 				asd->match.i2c.address);
 			return 0;
 		}
+	}
+
+	vfd = dev->ports[0]->cap_streams[0]->vfd;
+	if (((u16)-1) == vfd->num) {
+		ret = video_register_device(vfd, VFL_TYPE_GRABBER, -1);
+		if (ret) {
+			v4l2_err(&dev->v4l2_dev, "Failed to register video device\n");
+			return 0;
+		}
+
+		snprintf(vfd->name, sizeof(vfd->name), "%s", vip_videodev.name);
+
+		v4l2_info(&dev->v4l2_dev, VIP_MODULE_NAME
+				" Device registered as /dev/video%d\n",
+				vfd->num);
 	}
 
 	dev->sensor = subdev;
