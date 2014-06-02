81db639800a0ec9ec5257d526da62e4638e48d7a
H. Nikolaus Schaller <hns@goldelico.com>
2014-06-02 08:49:29 +0200

    w2cbw003: renamed structs and functions to correctly represent the w2cbw
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/misc/w2cbw003.c
+++ drivers/misc/w2cbw003.c
@@ -29,7 +29,7 @@
 #include <linux/platform_device.h>
 
 /* FIXME: rename to struct w2cbw */
-struct gpio_reg {
+struct w2cbw {
 	struct regulator	*reg;
 	int	set;	// gpio is "set"
 	/* there is no chip behind, but we need an address
@@ -42,10 +42,10 @@ struct gpio_reg {
 };
 
 /* FIXME: rename function to w2cbw_$$$ */
-static void gpio_reg_set_value(struct gpio_chip *gc,
+static void w2cbw_set_value(struct gpio_chip *gc,
 			       unsigned offset, int val)
 {
-	struct gpio_reg *greg = container_of(gc, struct gpio_reg, gpio);
+	struct w2cbw *greg = container_of(gc, struct w2cbw, gpio);
 	if (val) {
 		if (!greg->set && greg->reg)
 			if (regulator_enable(greg->reg) == 0)
@@ -57,21 +57,21 @@ static void gpio_reg_set_value(struct gpio_chip *gc,
 	}
 }
 
-static int gpio_reg_direction_output(struct gpio_chip *gc,
+static int w2cbw_direction_output(struct gpio_chip *gc,
 				     unsigned offset, int val)
 {
 	/* output => reg_enable, input => reg_disable */
-	gpio_reg_set_value(gc, offset, val);
+	w2cbw_set_value(gc, offset, val);
 	return 0;
 }
 
 
-static int gpio_reg_probe(struct platform_device *pdev)
+static int w2cbw_probe(struct platform_device *pdev)
 {
-	struct gpio_reg_data *pdata = dev_get_platdata(&pdev->dev);
-	struct gpio_reg *greg;
+	struct w2cbw_data *pdata = dev_get_platdata(&pdev->dev);
+	struct w2cbw *greg;
 	int err;
-	printk("gpio_reg_probe()\n");
+	printk("w2cbw_probe()\n");
 #ifdef CONFIG_OF
 	if (pdev->dev.of_node) {
 		struct device *dev = &pdev->dev;
@@ -87,7 +87,7 @@ static int gpio_reg_probe(struct platform_device *pdev)
 		greg->gpio.ngpio = 0;
 
 		greg->reg = regulator_get(&pdev->dev, "vdd");
-		printk("gpio_reg_probe() reg=%p\n", greg->reg);
+		printk("w2cbw_probe() reg=%p\n", greg->reg);
 
 		if (IS_ERR(greg->reg)) {
 			err = PTR_ERR(greg->reg);
@@ -106,12 +106,12 @@ static int gpio_reg_probe(struct platform_device *pdev)
 		greg->gpio.ngpio = 1;
 		greg->gpio.base = -1;
 		greg->gpio.owner = THIS_MODULE;
-		greg->gpio.direction_output = gpio_reg_direction_output;
-		greg->gpio.set = gpio_reg_set_value;
+		greg->gpio.direction_output = w2cbw_direction_output;
+		greg->gpio.set = w2cbw_set_value;
 		greg->gpio.can_sleep = 1;
         greg->gpio.of_node = of_node_get(pdev->dev.of_node);
 		err = gpiochip_add(&greg->gpio);
-		printk("gpio_reg_probe() gpiochip_add()=%d\n", err);
+		printk("w2cbw_probe() gpiochip_add()=%d\n", err);
 
 	out2:
 		if (err)
@@ -140,10 +140,10 @@ static int gpio_reg_probe(struct platform_device *pdev)
 	greg->gpio.label = "w2cbw003";
 	greg->gpio.names = greg->gpio_name;
 	greg->gpio.ngpio = 1;
-	greg->gpio.base = pdata->gpio;
+	greg->gpio.base = pdata->gpio_base;
 	greg->gpio.owner = THIS_MODULE;
-	greg->gpio.direction_output = gpio_reg_direction_output;
-	greg->gpio.set = gpio_reg_set_value;
+	greg->gpio.direction_output = w2cbw_direction_output;
+	greg->gpio.set = w2cbw_set_value;
 	greg->gpio.can_sleep = 1;
 	err = gpiochip_add(&greg->gpio);
 	if (err)
@@ -156,9 +156,9 @@ out:
 	return err;
 }
 
-static int gpio_reg_remove(struct platform_device *pdev)
+static int w2cbw_remove(struct platform_device *pdev)
 {
-	struct gpio_reg *greg = platform_get_drvdata(pdev);
+	struct w2cbw *greg = platform_get_drvdata(pdev);
 	int ret;
 
 	if (greg->reg) {
@@ -179,26 +179,26 @@ static const struct of_device_id w2cbw003_of_match[] = {
 MODULE_DEVICE_TABLE(of, w2cbw003_of_match);
 #endif
 
-static struct platform_driver gpio_reg_driver = {
+static struct platform_driver w2cbw_driver = {
 	.driver.name	= "w2cbw003",
 	.driver.owner	= THIS_MODULE,
 	.driver.of_match_table = of_match_ptr(w2cbw003_of_match),
-	.probe		= gpio_reg_probe,
-	.remove		= gpio_reg_remove,
+	.probe		= w2cbw_probe,
+	.remove		= w2cbw_remove,
 };
 
-static int __init gpio_reg_init(void)
+static int __init w2cbw_init(void)
 {
 	printk("gpio_w2cbw_init()\n");
-	return platform_driver_register(&gpio_reg_driver);
+	return platform_driver_register(&w2cbw_driver);
 }
-module_init(gpio_reg_init);
+module_init(w2cbw_init);
 
-static void __exit gpio_reg_exit(void)
+static void __exit w2cbw_exit(void)
 {
-	platform_driver_unregister(&gpio_reg_driver);
+	platform_driver_unregister(&w2cbw_driver);
 }
-module_exit(gpio_reg_exit);
+module_exit(w2cbw_exit);
 
 MODULE_ALIAS("w2cbw003");	/* should we have a simplified version of this as a separate driver specific for the w2cbw003? */
 
