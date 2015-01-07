7cad7684c8a57437bf8f1d13a0c5174d0b7f26bf
Praneeth Bajjuri <praneeth@ti.com>
2015-01-07 14:30:18 -0600

    ti_config_fragments: dra7xx: Add wrapper scripts for Android merge-config
    updating commit: f8c4fdf80c6b380e2f98eede8e67efeb0cdb3956
    
    Adds convinient wrapper scripts for merging linux config fragments.
    --
    Update:
    removing system_test.cfg
    adding dra7_only.cfg
    
    Change-Id: I23637f4a3ab535c1bf72f921afa41085af0581a1
    Signed-off-by: Praneeth Bajjuri <praneeth@ti.com>
    Signed-off-by: Hemant Hariyani <hemanthariyani@ti.com>

---
+++ ti_config_fragments/android_config.sh
@@ -0,0 +1,11 @@
+export ARCH=arm
+./scripts/kconfig/merge_config.sh \
+arch/arm/configs/omap2plus_defconfig \
+ti_config_fragments/dra7_only.cfg \
+ti_config_fragments/baseport.cfg \
+ti_config_fragments/power.cfg \
+ti_config_fragments/connectivity.cfg \
+ti_config_fragments/ipc.cfg \
+ti_config_fragments/audio_display.cfg \
+ti_config_fragments/wlan.cfg \
+ti_config_fragments/android_omap.cfg
