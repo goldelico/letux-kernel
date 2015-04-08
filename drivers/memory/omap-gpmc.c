de82b301b806064b5decf904882b0d1b2f031764
H. Nikolaus Schaller <hns@goldelico.com>
2015-04-08 16:49:09 +0200

    enable debugging for onenand and omap-gpmc driver
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/memory/omap-gpmc.c
+++ drivers/memory/omap-gpmc.c
@@ -12,6 +12,9 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
+
+#define DEBUG 1
+
 #include <linux/irq.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
