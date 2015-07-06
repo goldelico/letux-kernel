202cee14691081d0fc930c3b79e420275eea1cbf
H. Nikolaus Schaller <hns@goldelico.com>
2015-07-06 08:28:35 +0200

    intermediate merge of linus/master
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- scripts/sortextable.c
+++ scripts/sortextable.c
@@ -50,6 +50,10 @@
 #define EM_MICROBLAZE	189
 #endif
 
+#ifndef EM_ARCV2
+#define EM_ARCV2	195
+#endif
+
 static int fd_map;	/* File descriptor for file being modified. */
 static int mmap_failed; /* Boolean flag. */
 static void *ehdr_curr; /* current ElfXX_Ehdr *  for resource cleanup */
@@ -284,6 +288,7 @@ do_file(char const *const fname)
 		custom_sort = sort_relative_table;
 		break;
 	case EM_ARCOMPACT:
+	case EM_ARCV2:
 	case EM_ARM:
 	case EM_AARCH64:
 	case EM_MICROBLAZE:
