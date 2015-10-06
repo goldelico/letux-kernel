320465d0b9b3ed9f093f58927484b581bc23ae7a
H. Nikolaus Schaller <hns@goldelico.com>
2015-10-06 09:59:42 +0200

    revert any GTA04 specific changes of tsc2007.h and tsc2007.c so that they match mainline

--- include/linux/i2c/tsc2007.h
+++ include/linux/i2c/tsc2007.h
@@ -6,12 +6,6 @@
 struct tsc2007_platform_data {
 	u16	model;				/* 2007. */
 	u16	x_plate_ohms;	/* must be non-zero value */
-	bool swap_xy;	/* swap x and y axis */
-	u16	min_x;	/* min and max values to reported to user space */
-	u16	min_y;
-	u16	min_rt;
-	u16	max_x;
-	u16	max_y;
 	u16	max_rt; /* max. resistance above which samples are ignored */
 	unsigned long poll_period; /* time (in ms) between samples */
 	int	fuzzx; /* fuzz factor for X, Y and pressure axes */
