2431f4aaafdaac92c043495e23a54c97d669d5f1
H. Nikolaus Schaller <hns@goldelico.com>
2015-10-31 19:39:20 +0100

    debug: try to find out why the twl4030 phy is not initialized
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/phy/phy-twl4030-usb.c
+++ drivers/phy/phy-twl4030-usb.c
@@ -796,6 +796,10 @@ static int twl4030_usb_probe(struct platform_device *pdev)
 	struct device_node	*np = pdev->dev.of_node;
 	struct phy_provider	*phy_provider;
 
+#ifdef DEBUG
+printk("twl4030_usb_probe\n");
+#endif
+
 	twl = devm_kzalloc(&pdev->dev, sizeof(*twl), GFP_KERNEL);
 	if (!twl)
 		return -ENOMEM;
@@ -813,6 +817,10 @@ static int twl4030_usb_probe(struct platform_device *pdev)
 	otg = devm_kzalloc(&pdev->dev, sizeof(*otg), GFP_KERNEL);
 	if (!otg)
 		return -ENOMEM;
+
+#ifdef DEBUG
+printk("twl4030_usb_probe: otg = %p\n", otg);
+#endif
 
 	twl->dev		= &pdev->dev;
 	twl->irq		= platform_get_irq(pdev, 0);
@@ -834,6 +842,10 @@ static int twl4030_usb_probe(struct platform_device *pdev)
 		return PTR_ERR(phy);
 	}
 
+#ifdef DEBUG
+printk("twl4030_usb_probe: phy = %p\n", phy);
+#endif
+
 	phy_set_drvdata(phy, twl);
 
 	phy_provider = devm_of_phy_provider_register(twl->dev,
@@ -851,6 +863,11 @@ static int twl4030_usb_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "ldo init failed\n");
 		return err;
 	}
+
+#ifdef DEBUG
+printk("twl4030_usb_probe: usb_add_phy_dev\n");
+#endif
+
 	usb_add_phy_dev(&twl->phy);
 
 	platform_set_drvdata(pdev, twl);
@@ -897,6 +914,10 @@ static int twl4030_usb_probe(struct platform_device *pdev)
 
 	pm_runtime_mark_last_busy(&pdev->dev);
 	pm_runtime_put_autosuspend(twl->dev);
+
+#ifdef DEBUG
+printk("twl4030_usb_probe: done\n");
+#endif
 
 	dev_info(&pdev->dev, "Initialized TWL4030 USB module\n");
 	return 0;
@@ -963,6 +984,9 @@ static struct platform_driver twl4030_usb_driver = {
 
 static int __init twl4030_usb_init(void)
 {
+#ifdef DEBUG
+	printk("twl4030_usb_init\n");
+#endif
 	return platform_driver_register(&twl4030_usb_driver);
 }
 subsys_initcall(twl4030_usb_init);
