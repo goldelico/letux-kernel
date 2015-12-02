7c0f1af421b9f4c64fe9216a6657a326c6f915cd
Lukas Märdian <lukas@goldelico.com>
2015-12-02 19:33:11 +0100

    Android: omapdrm: HACK for correct resolution
    Before it was only 480x320 and the UI was squashed into the upper half of the display.

--- drivers/gpu/drm/omapdrm/omap_fbdev.c
+++ drivers/gpu/drm/omapdrm/omap_fbdev.c
@@ -116,7 +116,12 @@ static int omap_fbdev_create(struct drm_fb_helper *helper,
 			sizes->surface_depth);
 
 	mode_cmd.width = sizes->surface_width;
-	mode_cmd.height = sizes->surface_height;
+	//mode_cmd.height = sizes->surface_height;
+	/* Ugly HACK to fix framebuffer resolution on Android/Replicant,
+	   it was only 480x320 for some reason.
+	   TODO: find a proper fix for this problem.
+	 */
+	mode_cmd.height = sizes->surface_height*2;
 
 	mode_cmd.pitches[0] =
 			DIV_ROUND_UP(mode_cmd.width * sizes->surface_bpp, 8);
