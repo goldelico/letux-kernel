085306d1c4620195f466b29e00d3f206fcb1371b
H. Nikolaus Schaller <hns@goldelico.com>
2016-04-11 21:51:29 +0200

    power: bq2429x: cleanup of header file
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- include/linux/power/bq2429x_charger.h
+++ include/linux/power/bq2429x_charger.h
@@ -18,12 +18,6 @@
 #define FAULT_STATS_REGISTER		0x09
 #define VENDOR_STATS_REGISTER		0x0A
 
-/* power-on configuration register value */
-#define REGISTER_RESET_ENABLE	 1
-#define REGISTER_RESET_DISABLE	 0
-#define REGISTER_RESET_OFFSET	 7
-#define REGISTER_RESET_MASK	 1
-
 /* input source control register value */
 #define EN_HIZ_ENABLE	 1
 #define EN_HIZ_DISABLE	 0
@@ -41,6 +35,31 @@
 #define IINLIM_OFFSET		0
 #define IINLIM_MASK		7
 
+/* power-on configuration register value */
+#define WATCHDOG_RESET	0x40
+
+#define REGISTER_RESET_ENABLE	 1
+#define REGISTER_RESET_DISABLE	 0
+#define REGISTER_RESET_OFFSET	 7
+#define REGISTER_RESET_MASK	 1
+
+/* Charge Mode Config */
+#define CHARGE_MODE_CONFIG_CHARGE_DISABLE	0x00
+#define CHARGE_MODE_CONFIG_CHARGE_BATTERY	0x01
+#define CHARGE_MODE_CONFIG_OTG_OUTPUT		0x02
+#define CHARGE_MODE_CONFIG_OFFSET		4
+#define CHARGE_MODE_CONFIG_MASK		0x03
+
+#define SYS_MIN_OFFSET		1
+#define SYS_MIN_MASK		0x7
+
+/* OTG Mode Current Config */
+#define OTG_MODE_CURRENT_CONFIG_500MA		0x00
+#define OTG_MODE_CURRENT_CONFIG_1300MA	0x01
+#define OTG_MODE_CURRENT_CONFIG_OFFSET	0
+#define OTG_MODE_CURRENT_CONFIG_MASK		0x01
+
+/* charge current limit register value */
 #define CHARGE_CURRENT_64MA		0x01
 #define CHARGE_CURRENT_128MA		0x02
 #define CHARGE_CURRENT_256MA		0x04
@@ -51,6 +70,18 @@
 #define CHARGE_CURRENT_OFFSET		2
 #define CHARGE_CURRENT_MASK		0x3f
 
+/* Pre-Charge/Termination Current Control Register value */
+/* Pre-Charge Current Limit */
+#define PRE_CHARGE_CURRENT_LIMIT_128MA		0x00
+#define PRE_CHARGE_CURRENT_LIMIT_256MA		0x01
+#define PRE_CHARGE_CURRENT_LIMIT_OFFSET		4
+#define PRE_CHARGE_CURRENT_LIMIT_MASK		0x0f
+/* Termination Current Limit */
+#define TERMINATION_CURRENT_LIMIT_128MA		0x00
+#define TERMINATION_CURRENT_LIMIT_256MA		0x01
+#define TERMINATION_CURRENT_LIMIT_OFFSET		0
+#define TERMINATION_CURRENT_LIMIT_MASK		0x0f
+
 /* Charge Termination/Timer control register value */
 #define WATCHDOG_DISABLE		0
 #define WATCHDOG_40S		1
@@ -58,6 +89,10 @@
 #define WATCHDOG_160S		3
 #define WATCHDOG_OFFSET		4
 #define WATCHDOG_MASK		3
+
+/* boost voltage/thermal regulation register */
+#define BOOSTV_OFFSET	4
+#define BOOSTV_MASK	0xf
 
 /* misc operation control register value */
 #define DPDM_ENABLE	 1
@@ -81,49 +116,22 @@
 #define CHRG_MASK		3
 
 /* vendor status register value */
-#define CHIP_BQ24190		0
+/* #define CHIP_BQ24190		0
 #define CHIP_BQ24191		1
 #define CHIP_BQ24192		2
 #define CHIP_BQ24192I		3
 #define CHIP_BQ24190_DEBUG		4
-#define CHIP_BQ24192_DEBUG		5
-#define CHIP_BQ24296		10
-#define CHIP_OFFSET		3
+#define CHIP_BQ24192_DEBUG		5 */
+#define CHIP_BQ24296		1
+#define CHIP_BQ24297		3
+#define CHIP_OFFSET		5
 #define CHIP_MASK		7
-
-/* Pre-Charge/Termination Current Control Register value */
-/* Pre-Charge Current Limit */
-#define PRE_CHARGE_CURRENT_LIMIT_128MA		0x00
-#define PRE_CHARGE_CURRENT_LIMIT_256MA		0x01
-#define PRE_CHARGE_CURRENT_LIMIT_OFFSET		4
-#define PRE_CHARGE_CURRENT_LIMIT_MASK		0x0f
-/* Termination Current Limit */
-#define TERMINATION_CURRENT_LIMIT_128MA		0x00
-#define TERMINATION_CURRENT_LIMIT_256MA		0x01
-#define TERMINATION_CURRENT_LIMIT_OFFSET		0
-#define TERMINATION_CURRENT_LIMIT_MASK		0x0f
-/* Charge Mode Config */
-#define CHARGE_MODE_CONFIG_CHARGE_DISABLE		0x00
-#define CHARGE_MODE_CONFIG_CHARGE_BATTERY		0x01
-#define CHARGE_MODE_CONFIG_OTG_OUTPUT		0x02
-#define CHARGE_MODE_CONFIG_OFFSET		4
-#define CHARGE_MODE_CONFIG_MASK		0x03
-/* OTG Mode Current Config */
-#define OTG_MODE_CURRENT_CONFIG_500MA		0x00
-#define OTG_MODE_CURRENT_CONFIG_1300MA		0x01
-#define OTG_MODE_CURRENT_CONFIG_OFFSET		0
-#define OTG_MODE_CURRENT_CONFIG_MASK		0x01
 
 #define BQ24296_CHG_COMPELET       0x03
 #define BQ24296_NO_CHG             0x00
 
 #define BQ24296_DC_CHG             0x02
 #define BQ24296_USB_CHG            0x01
-
-enum {
-	AC_NOT_INSERT = 0,
-	AC_INSERT = 1,
-};
 
 struct bq24296_platform_data {		
 	unsigned int otg_usb_pin;
@@ -141,7 +149,6 @@ struct bq24296_board {
 	unsigned int chg_current[3];
 };
 
- int bq24296_charge_otg_en(int chg_en,int otg_en);
 #endif
 
 
