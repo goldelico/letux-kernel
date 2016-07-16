e92e7c9e2910c9d7983d347ae8f4b7cb00f59797
Matthijs van Duin <matthijsvanduin@gmail.com>
2016-07-16 14:52:28 +0200

    drm: omapdrm: gem: improve choice of memory type for tiled memory
    On omap5/dra7 tiled memory is currently strongly-ordered, which is just
    painfully and needlessly slow.  Using device memory suffices and is much
    faster.  Even normal uncacheable memory is safe if the application
    avoids reading from the framebuffer, so I've added a flag to enable
    userspace to indicate it really wants the memory type requested.
    
    The option to use strongly-ordered memory has been retained as an extra
    memory type in case anyone needs it for some reason.
    
    Signed-off-by: Matthijs van Duin <matthijsvanduin@gmail.com>

--- drivers/gpu/drm/omapdrm/omap_gem.c
+++ drivers/gpu/drm/omapdrm/omap_gem.c
@@ -73,8 +73,9 @@ struct omap_gem_object {
 	refcount_t pin_cnt;
 
 	/**
-	 * If the buffer has been imported from a dmabuf the OMAP_DB_DMABUF flag
-	 * is set and the sgt field is valid.
+	 * buffer represented as sg table. It is valid if
+	 * - the buffer is imported from dmabuf (OMAP_BO_MEM_DMABUF flag set)
+	 * - the buffer is mapped through dmabuf (also increases dma_addr_cnt)
 	 */
 	struct sg_table *sgt;
 
@@ -537,11 +538,20 @@ static int omap_gem_object_mmap(struct drm_gem_object *obj, struct vm_area_struc
 
 	vm_flags_set(vma, VM_DONTEXPAND | VM_DONTDUMP | VM_IO | VM_MIXEDMAP);
 
-	if (omap_obj->flags & OMAP_BO_WC) {
-		vma->vm_page_prot = pgprot_writecombine(vma_get_page_prot(vma));
-	} else if (omap_obj->flags & OMAP_BO_UNCACHED) {
-		vma->vm_page_prot = pgprot_noncached(vma_get_page_prot(vma));
-	} else {
+// CHECKME: this is always overwritten?
+	vma->vm_page_prot = vm_get_page_prot(vma->vm_flags);
+
+	switch (omap_obj->flags & OMAP_BO_CACHE_MASK) {
+	case OMAP_BO_WC:
+		vma->vm_page_prot = pgprot_writecombine(vma->vm_page_prot);
+		break;
+	case OMAP_BO_UNCACHED:
+		vma->vm_page_prot = pgprot_device(vma->vm_page_prot);
+		break;
+	case OMAP_BO_SYNC:
+		vma->vm_page_prot = pgprot_stronglyordered(vma->vm_page_prot);
+		break;
+	default:
 		/*
 		 * We do have some private objects, at least for scanout buffers
 		 * on hardware without DMM/TILER.  But these are allocated write-
@@ -1301,11 +1311,13 @@ struct drm_gem_object *omap_gem_new(struct drm_device *dev,
 		flags |= OMAP_BO_MEM_SHMEM;
 
 		/*
-		 * Currently don't allow cached buffers. There is some caching
-		 * stuff that needs to be handled better.
+		 * Unless caller knows what he's doing replace cacheable and
+		 * normal uncacheable memory types by default type for cpu.
 		 */
-		flags &= ~(OMAP_BO_CACHED|OMAP_BO_WC|OMAP_BO_UNCACHED);
-		flags |= tiler_get_cpu_cache_flags();
+		if (!(flags & (OMAP_BO_UNCACHED | OMAP_BO_FORCE))) {
+			flags &= ~OMAP_BO_CACHE_MASK;
+			flags |= tiler_get_cpu_cache_flags();
+		}
 	} else if ((flags & OMAP_BO_SCANOUT) && !priv->has_dmm) {
 		/*
 		 * If we don't have DMM, we must allocate scanout buffers
