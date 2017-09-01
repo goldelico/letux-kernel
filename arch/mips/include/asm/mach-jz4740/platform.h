bef5f5fd14b3ce99ce6ba0d22737e97b45eef0bb
Paul Boddie <paul@boddie.org.uk>
2017-09-01 17:31:20 +0000

    MIPS:Ingenic: introduce JZ4730 SoC
    Signed-off-by: Paul Boddie <paul@boddie.org.uk>
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- arch/mips/include/asm/mach-jz4740/platform.h
+++ arch/mips/include/asm/mach-jz4740/platform.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
  *  Copyright (C) 2009-2010, Lars-Peter Clausen <lars@metafoo.de>
+ *  Copyright (C) 2017 Paul Boddie <paul@boddie.org.uk>
  *  JZ4740 platform device definitions
  */
 
@@ -21,6 +22,14 @@ extern struct platform_device jz4740_pcm_device;
 extern struct platform_device jz4740_codec_device;
 extern struct platform_device jz4740_adc_device;
 extern struct platform_device jz4740_pwm_device;
+extern struct platform_device jz4740_wdt_device;
 extern struct platform_device jz4740_dma_device;
+
+#ifdef CONFIG_MACH_JZ4730
+extern struct platform_device jz4730_pwm_device;
+#else
+extern struct platform_device jz4740_pwm_device;
+extern struct platform_device jz4740_adc_device;
+#endif
 
 #endif
