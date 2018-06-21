fbe8b41db984b4611b8f358ffbf43aac174ef505
Josua Mayer <josua.mayer97@gmail.com>
2018-06-21 23:45:30 +0200

    greybus: disable building gb_loopback_test for android
    This caused a weird, and hard to debug build error!
    
    Signed-off-by: Josua Mayer <josua.mayer97@gmail.com

--- drivers/staging/greybus/tools/Android.mk
+++ drivers/staging/greybus/tools/Android.mk
@@ -1,10 +1,1 @@
 LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:= loopback_test.c
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := gb_loopback_test
-
-include $(BUILD_EXECUTABLE)
-
