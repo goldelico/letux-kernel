2f3f00ab0b2c8bb127acb1ac8d57eee49e9a0dca
Marek Belisko <marek.belisko@open-nandra.com>
2018-06-23 22:17:43 +0200

    omap: abe: Fixes after adding patches
    Signed-off-by: Marek Belisko <marek.belisko@open-nandra.com>

--- sound/soc/ti/omap-abe-twl6040.c
+++ sound/soc/ti/omap-abe-twl6040.c
@@ -116,7 +116,7 @@ static int omap_abe_mcpdm_hw_params(struct snd_pcm_substream *substream,
 	clk_id = twl6040_get_clk_id(codec_dai->component);
 	if (clk_id == TWL6040_SYSCLK_SEL_HPPLL)
 		freq = card_data->mclk_freq;
-	else if (clk_id == TWL6040_SYSCLK_SEL_LPPLL)
+	else if (clk_id == TWL6040_SYSCLK_SEL_LPPLL) {
 		freq = 32768;
 	} else {
 		dev_err(card->dev, "invalid clock\n");
