9f43700cf3eb37d634d66b29fadaaf80629f387b
H. Nikolaus Schaller <hns@goldelico.com>
2018-07-04 10:37:40 +0200

    soc: soc-dapm: fix to remove snd_soc_dapm_kcontrol_platform since we have no snd_soc_platform any more
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -553,16 +553,6 @@ struct snd_soc_dapm_context *snd_soc_dapm_kcontrol_dapm(
 }
 EXPORT_SYMBOL_GPL(snd_soc_dapm_kcontrol_dapm);
 
-/**
- * snd_soc_dapm_kcontrol_platform() - Returns the platform associated to a kcontrol
- * @kcontrol: The kcontrol
- */
-struct snd_soc_platform *snd_soc_dapm_kcontrol_platform(struct snd_kcontrol *kcontrol)
-{
-	return snd_soc_component_to_platform(dapm_kcontrol_get_wlist(kcontrol)->widgets[0]->dapm->component);
-}
-EXPORT_SYMBOL_GPL(snd_soc_dapm_kcontrol_platform);
-
 static void dapm_reset(struct snd_soc_card *card)
 {
 	struct snd_soc_dapm_widget *w;
