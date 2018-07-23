mvduin/linux

XXX omapdrm: allocate fbdev in TILER
For now I'm just forcing its actual width to the container width (8192
pixels).  It's a giant waste of RAM, but it makes the buffer physically
contiguous which ensures easy compatibility with existing fbdev code and
hassle-free mapping into kernel memory.

drivers/gpu/drm/omapdrm/omap_gem.c
@@ -1021,16 +1021,24 @@ int omap_gem_put_pages(struct drm_gem_object *obj)
 void *omap_gem_vaddr(struct drm_gem_object *obj)
 {
 	struct omap_gem_object *omap_obj = to_omap_bo(obj);
+	void *va = omap_obj->vaddr;
 	WARN_ON(!mutex_is_locked(&obj->dev->struct_mutex));
-	if (!omap_obj->vaddr) {
+	if (va)
+		return va;
+	if (omap_obj->flags & OMAP_BO_TILED) {
+		// FIXME to avoid contiguous mapping?
+		va = ioremap(omap_obj->paddr, obj->size);
+	} else {
 		struct page **pages;
 		int ret = get_pages(obj, &pages);
 		if (ret)
 			return ERR_PTR(ret);
-		omap_obj->vaddr = vmap(pages, obj->size >> PAGE_SHIFT,
+		va = vmap(pages, obj->size >> PAGE_SHIFT,
 				VM_MAP, pgprot_writecombine(PAGE_KERNEL));
 	}
-	return omap_obj->vaddr;
+	if (!IS_ERR(va))
+		omap_obj->vaddr = va;
+	return va;
 }
 #endif
 
@@ -1354,6 +1362,8 @@ void omap_gem_free_object(struct drm_gem_object *obj)
 	if (omap_obj->flags & OMAP_BO_MEM_DMA_API) {
 		dma_free_wc(dev->dev, obj->size, omap_obj->vaddr,
 			    omap_obj->paddr);
+	} else if (omap_obj->flags & OMAP_BO_TILED) {
+		iounmap(omap_obj->vaddr);
 	} else if (omap_obj->vaddr) {
 		vunmap(omap_obj->vaddr);
 	} else if (obj->import_attach) {

goldelico/letux-kernel

drivers/gpu/drm/omapdrm/omap_gem.c
@@ -1019,16 +1019,24 @@ int omap_gem_put_pages(struct drm_gem_object *obj)
 void *omap_gem_vaddr(struct drm_gem_object *obj)
 {
 	struct omap_gem_object *omap_obj = to_omap_bo(obj);
+	void *va = omap_obj->vaddr;
 	WARN_ON(!mutex_is_locked(&obj->dev->struct_mutex));
-	if (!omap_obj->vaddr) {
+	if (va)
+		return va;
+	if (omap_obj->flags & OMAP_BO_TILED) {
+		// FIXME to avoid contiguous mapping?
+		va = ioremap(omap_obj->dma_addr, obj->size);
+	} else {
 		struct page **pages;
 		int ret = get_pages(obj, &pages);
 		if (ret)
 			return ERR_PTR(ret);
-		omap_obj->vaddr = vmap(pages, obj->size >> PAGE_SHIFT,
+		va = vmap(pages, obj->size >> PAGE_SHIFT,
 				VM_MAP, pgprot_writecombine(PAGE_KERNEL));
 	}
-	return omap_obj->vaddr;
+	if (!IS_ERR(va))
+		omap_obj->vaddr = va;
+	return va;
 }
 #endif
 
@@ -1150,6 +1158,8 @@ void omap_gem_free_object(struct drm_gem_object *obj)
 	if (omap_obj->flags & OMAP_BO_MEM_DMA_API) {
 		dma_free_wc(dev->dev, obj->size, omap_obj->vaddr,
 			    omap_obj->dma_addr);
+	} else if (omap_obj->flags & OMAP_BO_TILED) {
+		iounmap(omap_obj->vaddr);
 	} else if (omap_obj->vaddr) {
 		vunmap(omap_obj->vaddr);
 	} else if (obj->import_attach) {