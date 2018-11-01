0158c61121bb8ab360a00feccf817de50d576eb3
Vasily Khoruzhick <anarsoul@gmail.com>
2018-10-31 20:07:41 -0700

    Bluetooth: hci_h5: Add support for binding RTL8723BS with device tree
    RTL8723BS is often used in ARM boards, so add ability to bind it
    using device tree.
    
    Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

--- drivers/bluetooth/hci_h5.c
+++ drivers/bluetooth/hci_h5.c
@@ -822,6 +822,8 @@ static int h5_serdev_probe(struct serdev_device *serdev)
 			return -ENODEV;
 
 		h5->vnd = (const struct h5_vnd *)data;
+		of_property_read_string(serdev->dev.of_node,
+					"firmware-postfix", &h5->id);
 	}
 
 
@@ -1029,6 +1031,8 @@ static const struct dev_pm_ops h5_serdev_pm_ops = {
 
 static const struct of_device_id rtl_bluetooth_of_match[] = {
 #ifdef CONFIG_BT_HCIUART_RTL
+	{ .compatible = "realtek,rtl8822bs-bt",
+	  .data = (const void *)&rtl_vnd },
 	{ .compatible = "realtek,rtl8822cs-bt",
 	  .data = (const void *)&rtl_vnd },
 	{ .compatible = "realtek,rtl8723bs-bt",
