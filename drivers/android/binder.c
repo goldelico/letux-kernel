a3bc106d51d043646ff3b96e3d1d4578214ea6a1
H. Nikolaus Schaller <hns@goldelico.com>
2019-07-11 09:42:45 +0200

    drivers: android: Revert "ANDROID: binder: remove 32-bit binder interface."
    This reverts commit 1190b4e38f97023154e6b3bef61b251aa5f970d0.
    
    The commit did remove 32 bit API for the Android Binder which we
    still need for the <= v4.4 Replicant binaries. So let's reintroduce
    it.
    
    We need to set CONFIG_ANDROID_BINDER_IPC_32BIT=y.
    
    At some point in the future, the underlaying code will disappear
    from upstream, but then we hopefully have newer Replicant binary
    releases for all devices.
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -68,6 +68,10 @@
 #include <linux/sizes.h>
 #include <linux/ktime.h>
 
+#ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
+#define BINDER_IPC_32BIT 1
+#endif
+
 #include <uapi/linux/android/binder.h>
 
 #include <linux/cacheflush.h>
