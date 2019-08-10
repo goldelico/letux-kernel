5ef035f6293657e10d2f9a9127a4e1513a990ef1
H. Nikolaus Schaller <hns@goldelico.com>
2019-08-10 09:32:54 +0200

    Makefile: improve to handle CONFIG_SGX_DRM
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/staging/pvr/1.14.3699939/eurasia_km/services4/srvkm/env/linux/module.c
+++ drivers/staging/pvr/1.14.3699939/eurasia_km/services4/srvkm/env/linux/module.c
@@ -47,8 +47,10 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #endif
 #endif
 
+#if defined(CONFIG_OMAP2PLUS)
 #if (AM_VERSION != 5)
 #include <linux/platform_data/sgx-omap.h>
+#endif
 #endif
 
 #if defined(SUPPORT_DRI_DRM) && !defined(SUPPORT_DRI_DRM_PLUGIN)
@@ -83,12 +85,14 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #if defined(PVR_LDM_DEVICE_TREE) && !defined(NO_HARDWARE)
 #define PVR_USE_DEVICE_TREE
+#include <linux/mod_devicetable.h>
 #endif
 
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/fs.h>
+#include <linux/reset.h>
 
 #if defined(SUPPORT_DRI_DRM)
 #include <drm/drmP.h>
@@ -318,6 +322,11 @@ static LDM_DRV powervr_driver = {
 
 LDM_DEV *gpsPVRLDMDev;
 
+#ifdef CONFIG_RESET_CONTROLLER
+struct reset_control *rstc;
+bool already_deasserted = false;
+#endif
+
 #if defined(MODULE) && defined(PVR_LDM_PLATFORM_MODULE) && \
 	!defined(PVR_USE_PRE_REGISTERED_PLATFORM_DEV)
 static void PVRSRVDeviceRelease(struct device unref__ *pDevice)
@@ -357,21 +366,60 @@ static int __devinit PVRSRVDriverProbe(LDM_DEV *pDevice, const struct pci_device
 #endif
 {
 	SYS_DATA *psSysData;
-#if (AM_VERSION != 5)
 	int ret;
+#ifndef CONFIG_RESET_CONTROLLER
 	struct device *dev = &pDevice->dev;
 	struct gfx_sgx_platform_data *pdata = dev->platform_data;
 #endif
 
 	PVR_TRACE(("PVRSRVDriverProbe(pDevice=%p)", pDevice));
-#if (AM_VERSION != 5)
+#ifdef CONFIG_RESET_CONTROLLER
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,16,0))
+	rstc = reset_control_get_optional_exclusive(&pDevice->dev, NULL);
+#else
+	rstc = reset_control_get(&pDevice->dev, NULL);
+#endif
+
+	if (IS_ERR(rstc))
+	{
+		dev_err(&pDevice->dev, "%s: error: reset_control_get\n", __func__);
+		return PTR_ERR(rstc);
+	}
+
+	if(rstc) {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(99,99,99))
+		ret = reset_control_clear_reset(rstc);
+
+		if (ret < 0)
+		{
+			dev_err(&pDevice->dev, "%s: error: reset_control_clear_reset\n", __func__);
+			return ret;
+		}
+#endif
+
+		ret = reset_control_deassert(rstc);
+
+		if (ret == -EEXIST)
+		{
+		already_deasserted = true;
+		}
+		else if (ret < 0)
+		{
+			dev_err(&pDevice->dev, "%s: error: reset_control_deassert\n", __func__);
+			return ret;
+		}
+	}
+#else /* CONFIG_RESET_CONTROLLER */
 	if (pdata && pdata->deassert_reset) {
 		ret = pdata->deassert_reset(pDevice, pdata->reset_name);
 		if (ret) {
 			dev_err(dev, "Unable to reset SGX!\n");
 		}
+	} else {
+		dev_err(dev, "SGX Platform data missing deassert_reset!\n");
+		return -ENODEV;
 	}
-#endif
+#endif  /* CONFIG_RESET_CONTROLLER */
 
 #if 0   /* INTEGRATION_POINT */
 	/* Some systems require device-specific system initialisation.
@@ -397,6 +445,21 @@ static int __devinit PVRSRVDriverProbe(LDM_DEV *pDevice, const struct pci_device
 			return -ENODEV;
 		}
 	}
+
+#ifdef CONFIG_RESET_CONTROLLER
+        if (!already_deasserted && rstc)
+        {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(99,99,99))
+                ret = reset_control_is_reset(rstc);
+                if (ret <= 0)
+                {
+                        PVR_DPF((PVR_DBG_MESSAGE, "reset control reset"));
+                }
+#endif
+        }
+        reset_control_put(rstc);
+#endif /* CONFIG_RESET_CONTROLLER */
+
 	return 0;
 }
 
@@ -787,6 +850,10 @@ static int PVRSRVOpen(struct inode unref__ * pInode, struct file *pFile)
 #endif
 
 	LinuxLockMutexNested(&gPVRSRVLock, PVRSRV_LOCK_CLASS_BRIDGE);
+
+#if !defined(SUPPORT_DRI_DRM)
+	pFile->f_mode |= FMODE_UNSIGNED_OFFSET;
+#endif
 
 	ui32PID = OSGetCurrentProcessIDKM();
 
