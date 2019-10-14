f9e64393d24b037c1e03185004139756f64d3d0f
Paul Boddie <paul@boddie.org.uk>
2019-10-14 22:18:30 +0200

    Fixed clock definitions; removed functions.
    The JZ4730_CLK_DMA and JZ4730_CLK_MMC clock definitions were using
    the wrong register for clock gating and have been fixed.
    The jz4730_clock functions appear to be superfluous and have been
    removed.
    
    Signed-off-by: Paul Boddie <paul@boddie.org.uk>

--- drivers/clk/ingenic/jz4730-cgu.c
+++ drivers/clk/ingenic/jz4730-cgu.c
@@ -4,7 +4,7 @@
  * Copyright (c) 2015 Imagination Technologies
  * Author: Paul Burton <paul.burton@imgtec.com>
  *
- * Copyright (c) 2017 Paul Boddie <paul@boddie.org.uk>
+ * Copyright (c) 2017, 2019 Paul Boddie <paul@boddie.org.uk>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -189,13 +189,13 @@ static const struct ingenic_cgu_clk_info jz4730_cgu_clocks[] = {
 	[JZ4730_CLK_DMA] = {
 		"dma", CGU_CLK_GATE,
 		.parents = { JZ4730_CLK_PCLK, -1, -1, -1 },
-		.gate = { CGU_REG_CFCR, 5 },
+		.gate = { CGU_REG_MSCR, 5 },
 	},
 
 	[JZ4730_CLK_MMC] = {
 		"mmc", CGU_CLK_GATE,
 		.parents = { JZ4730_CLK_MSC16M, JZ4730_CLK_MSC24M, -1, -1 },
-		.gate = { CGU_REG_CFCR, 24 },
+		.gate = { CGU_REG_MSCR, 13 },
 	},
 
 	[JZ4730_CLK_UART0] = {
@@ -239,77 +239,3 @@ static void __init jz4730_cgu_init(struct device_node *np)
 		pr_err("%s: failed to register CGU Clocks\n", __func__);
 }
 CLK_OF_DECLARE(jz4730_cgu, "ingenic,jz4730-cgu", jz4730_cgu_init);
-
-#if 0	// unused and does no longer compile
-void jz4730_clock_set_wait_mode(enum jz4740_wait_mode mode)
-{
-	uint32_t lcr = readl(cgu->base + CGU_REG_LPCR);
-
-	switch (mode) {
-	case JZ4740_WAIT_MODE_IDLE:
-		lcr &= ~LPCR_SLEEP;
-		break;
-
-	case JZ4740_WAIT_MODE_SLEEP:
-		lcr |= LPCR_SLEEP;
-		break;
-	}
-
-	writel(lcr, cgu->base + CGU_REG_LPCR);
-}
-#endif
-
-void jz4730_clock_udc_disable_auto_suspend(void)
-{
-	uint32_t clkgr = readl(cgu->base + CGU_REG_MSCR);
-
-	clkgr &= ~MSCR_UDC;
-	writel(clkgr, cgu->base + CGU_REG_MSCR);
-}
-EXPORT_SYMBOL_GPL(jz4730_clock_udc_disable_auto_suspend);
-
-void jz4730_clock_udc_enable_auto_suspend(void)
-{
-	uint32_t clkgr = readl(cgu->base + CGU_REG_MSCR);
-
-	clkgr |= MSCR_UDC;
-	writel(clkgr, cgu->base + CGU_REG_MSCR);
-}
-EXPORT_SYMBOL_GPL(jz4730_clock_udc_enable_auto_suspend);
-
-#define JZ_CLOCK_GATE_UART0	BIT(0)
-#define JZ_CLOCK_GATE_OST	BIT(3)
-#define JZ_CLOCK_GATE_DMAC	BIT(5)
-
-void jz4730_clock_suspend(void)
-{
-	uint32_t clkgr, cppcr;
-
-	clkgr = readl(cgu->base + CGU_REG_MSCR);
-	clkgr |= JZ_CLOCK_GATE_OST | JZ_CLOCK_GATE_DMAC | JZ_CLOCK_GATE_UART0;
-	writel(clkgr, cgu->base + CGU_REG_MSCR);
-
-	cppcr = readl(cgu->base + CGU_REG_PLCR1);
-	cppcr &= ~BIT(jz4730_cgu_clocks[JZ4730_CLK_PLL].pll.enable_bit);
-	writel(cppcr, cgu->base + CGU_REG_PLCR1);
-}
-
-void jz4730_clock_resume(void)
-{
-	uint32_t clkgr, cppcr, stable;
-
-	cppcr = readl(cgu->base + CGU_REG_PLCR1);
-	cppcr |= BIT(jz4730_cgu_clocks[JZ4730_CLK_PLL].pll.enable_bit);
-	writel(cppcr, cgu->base + CGU_REG_PLCR1);
-
-	stable = BIT(jz4730_cgu_clocks[JZ4730_CLK_PLL].pll.stable_bit);
-	do {
-		cppcr = readl(cgu->base + CGU_REG_PLCR1);
-	} while (!(cppcr & stable));
-
-	clkgr = readl(cgu->base + CGU_REG_MSCR);
-	clkgr &= ~JZ_CLOCK_GATE_OST;
-	clkgr &= ~JZ_CLOCK_GATE_DMAC;
-	clkgr &= ~JZ_CLOCK_GATE_UART0;
-	writel(clkgr, cgu->base + CGU_REG_MSCR);
-}
