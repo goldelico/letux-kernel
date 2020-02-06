1bd0d89c521d749e49d43557845ab740c54c344e
Samuel Holland <samuel@sholland.org>
2020-02-05 23:40:08 -0600

    ASoC: sun8i-codec: Avoid distortion on AIF2
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- sound/soc/sunxi/sun8i-codec.c
+++ sound/soc/sunxi/sun8i-codec.c
@@ -380,6 +380,13 @@ static int sun8i_codec_hw_params(struct snd_pcm_substream *substream,
 	u8 bclk_div;
 	u32 value;
 
+	/*
+	 * There should be at least two slots in each frame, or else the codec
+	 * cuts off the first bit of each sample, and often de-syncs.
+	 */
+	if (channels == 1)
+		slot_width *= 2;
+
 	bclk_div = sun8i_codec_get_bclk_div(scodec, params_rate(params),
 					    channels, slot_width);
 	regmap_update_bits(scodec->regmap, reg,
