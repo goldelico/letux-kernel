fb8b04f6d4627a1b029815934102e08fbe29acf5
H. Nikolaus Schaller <hns@goldelico.com>
2020-03-14 07:43:31 +0100

    printk for osfunc.c
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/gpu/drm/pvrsgx/1.14.3759903/eurasia_km/services4/srvkm/env/linux/osfunc.c
+++ drivers/gpu/drm/pvrsgx/1.14.3759903/eurasia_km/services4/srvkm/env/linux/osfunc.c
@@ -4194,6 +4194,8 @@ IMG_BOOL CheckExecuteCacheOp(IMG_HANDLE hOSMemHandle,
 	IMG_VOID *pvPhysRangeStart = pvVirtRangeStart;
 #endif
 
+printk("%s\n", __func__);
+
 	PVR_ASSERT(psLinuxMemArea != IMG_NULL);
 
 	LinuxLockMutexNested(&g_sMMapMutex, PVRSRV_LOCK_CLASS_MMAP);
@@ -4218,10 +4220,14 @@ IMG_BOOL CheckExecuteCacheOp(IMG_HANDLE hOSMemHandle,
 	/* Recursion surely isn't possible? */
 	PVR_ASSERT(psLinuxMemArea->eAreaType != LINUX_MEM_AREA_SUB_ALLOC);
 
+printk("%s 1\n", __func__);
+
 	switch(psLinuxMemArea->eAreaType)
 	{
 		case LINUX_MEM_AREA_VMALLOC:
 		{
+printk("%s 2\n", __func__);
+
 			if(is_vmalloc_addr(pvVirtRangeStart))
 			{
 				pvMinVAddr = psLinuxMemArea->uData.sVmalloc.pvVmallocAddress + uiAreaOffset;
@@ -4260,6 +4266,8 @@ IMG_BOOL CheckExecuteCacheOp(IMG_HANDLE hOSMemHandle,
 
 		case LINUX_MEM_AREA_EXTERNAL_KV:
 		{
+printk("%s 3\n", __func__);
+
 			/* We'll only see bPhysContig for frame buffers, and we shouldn't
 			 * be flushing those (they're write combined or uncached).
 			 */
@@ -4288,6 +4296,8 @@ IMG_BOOL CheckExecuteCacheOp(IMG_HANDLE hOSMemHandle,
 
 		case LINUX_MEM_AREA_ALLOC_PAGES:
 		{
+printk("%s 4\n", __func__);
+
 			pvMinVAddr = FindMMapBaseVAddr(psMMapOffsetStructList,
 			                               pvVirtRangeStart, uiLength);
 			if(!pvMinVAddr)
@@ -4297,21 +4307,34 @@ IMG_BOOL CheckExecuteCacheOp(IMG_HANDLE hOSMemHandle,
 		}
 
 		default:
+printk("%s 5\n", __func__);
+
 			PVR_DBG_BREAK;
 			goto err_blocked;
 	}
 
+printk("%s 6\n", __func__);
+
 #if defined(USE_PHYSICAL_CACHE_OP)
+
+printk("%s 7\n", __func__);
+
 	switch(psLinuxMemArea->eAreaType)
 	{
 		case LINUX_MEM_AREA_VMALLOC:
 		{
+
+printk("%s 8\n", __func__);
+
 			pfnMemAreaToPhys = VMallocAreaToPhys;
 			break;
 		}
 
 		case LINUX_MEM_AREA_EXTERNAL_KV:
 		{
+
+printk("%s 9\n", __func__);
+
 			uPageNumOffset = ((uiAreaOffset & PAGE_MASK) + (pvPhysRangeStart - pvMinVAddr)) >> PAGE_SHIFT;
 			pfnMemAreaToPhys = ExternalKVAreaToPhys;
 			break;
@@ -4319,6 +4342,9 @@ IMG_BOOL CheckExecuteCacheOp(IMG_HANDLE hOSMemHandle,
 
 		case LINUX_MEM_AREA_ALLOC_PAGES:
 		{
+
+printk("%s 10\n", __func__);
+
 			uPageNumOffset = ((uiAreaOffset & PAGE_MASK) + (pvPhysRangeStart - pvMinVAddr)) >> PAGE_SHIFT;
 
 			if (psLinuxMemArea->hBMHandle)
@@ -4330,11 +4356,17 @@ IMG_BOOL CheckExecuteCacheOp(IMG_HANDLE hOSMemHandle,
 		}
 
 		default:
+
+printk("%s 11\n", __func__);
+
 			PVR_DBG_BREAK;
 	}
 #endif
 
 #if defined(USE_VIRTUAL_CACHE_OP)
+
+printk("%s 12\n", __func__);
+
 	DoVirtualCacheOp(hOSMemHandle,
 	                 ui32ByteOffset,
 	                 pvVirtRangeStart,
@@ -4344,7 +4376,12 @@ IMG_BOOL CheckExecuteCacheOp(IMG_HANDLE hOSMemHandle,
 
 	LinuxUnLockMutex(&g_sMMapMutex);
 
+printk("%s 13\n", __func__);
+
 #if defined(USE_PHYSICAL_CACHE_OP)
+
+printk("%s 14\n", __func__);
+
 	PVR_ASSERT(pfnMemAreaToPhys != IMG_NULL);
 
 	DoPhysicalCacheOp(psLinuxMemArea,
@@ -4355,14 +4392,22 @@ IMG_BOOL CheckExecuteCacheOp(IMG_HANDLE hOSMemHandle,
 	                  pfnPhysicalCacheOp);
 #endif
 
+printk("%s 15\n", __func__);
+
 	return IMG_TRUE;
 
 err_blocked:
+
+printk("%s 16\n", __func__);
+
 	PVR_DPF((PVR_DBG_WARNING, "%s: Blocked cache op on virtual range "
 							  "%p-%p (type %d)", __func__,
 			 pvVirtRangeStart, pvVirtRangeStart + uiLength,
 			 psLinuxMemArea->eAreaType));
 	LinuxUnLockMutex(&g_sMMapMutex);
+
+printk("%s 17\n", __func__);
+
 	return IMG_FALSE;
 }
 
