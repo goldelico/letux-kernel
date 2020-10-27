5490bd5ec85fa1075d4abe89b992e53248eb3c9d
H. Nikolaus Schaller <hns@goldelico.com>
2020-10-27 13:37:49 +0100

    MIPS: add jz4730 to generic/board-ingenic
    Add the SoC identification.
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- arch/mips/generic/board-ingenic.c
+++ arch/mips/generic/board-ingenic.c
@@ -37,6 +37,8 @@ static __init char *ingenic_get_system_type(unsigned long machtype)
 		return "JZ4775";
 	case MACH_INGENIC_JZ4770:
 		return "JZ4770";
+	case MACH_INGENIC_JZ4730:
+		return "JZ4730";
 	case MACH_INGENIC_JZ4725B:
 		return "JZ4725B";
 	default:
@@ -63,6 +65,7 @@ static __init const void *ingenic_fixup_fdt(const void *fdt, const void *match_d
 static const struct of_device_id ingenic_of_match[] __initconst = {
 	{ .compatible = "ingenic,jz4740", .data = (void *)MACH_INGENIC_JZ4740 },
 	{ .compatible = "ingenic,jz4725b", .data = (void *)MACH_INGENIC_JZ4725B },
+	{ .compatible = "ingenic,jz4730", .data = (void *)MACH_INGENIC_JZ4730 },
 	{ .compatible = "ingenic,jz4770", .data = (void *)MACH_INGENIC_JZ4770 },
 	{ .compatible = "ingenic,jz4775", .data = (void *)MACH_INGENIC_JZ4775 },
 	{ .compatible = "ingenic,jz4780", .data = (void *)MACH_INGENIC_JZ4780 },
