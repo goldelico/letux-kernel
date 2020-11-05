24f73feaf3482d0cbbd0b1a5f93165dd343c56eb
H. Nikolaus Schaller <hns@goldelico.com>
2020-11-05 18:22:55 +0100

    Android: omapdrm: force RGB565
    this is a better fix in addition to the previous one.
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/gpu/drm/omapdrm/omap_fbdev.c
+++ drivers/gpu/drm/omapdrm/omap_fbdev.c
@@ -138,7 +138,7 @@ static int omap_fbdev_create(struct drm_fb_helper *helper,
 	/* Force RGB565 since this is what is needed for Android/Replicant
 	 */
 	sizes->surface_bpp = 16;
-	sizes->surface_depth = 0;
+	sizes->surface_depth = 16;
 
 	DBG("create fbdev: %dx%d@%d (%dx%d)", sizes->surface_width,
 			sizes->surface_height, sizes->surface_bpp,
