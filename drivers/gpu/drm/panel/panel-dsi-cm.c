8b5b9484f975cba6cbb7427cad104550a42b6400
Tomi Valkeinen <tomi.valkeinen@ti.com>
2020-11-13 12:52:45 +0200

    drm/panel: panel-dsi-cm: drop unneeded includes
    Drop unneeded includes.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/panel/panel-dsi-cm.c
+++ drivers/gpu/drm/panel/panel-dsi-cm.c
@@ -9,12 +9,7 @@
 #include <linux/backlight.h>
 #include <linux/delay.h>
 #include <linux/gpio/consumer.h>
-#include <linux/interrupt.h>
 #include <linux/jiffies.h>
-#include <linux/module.h>
-#include <linux/sched/signal.h>
-#include <linux/slab.h>
-#include <linux/of_device.h>
 #include <linux/regulator/consumer.h>
 
 #include <drm/drm_connector.h>
