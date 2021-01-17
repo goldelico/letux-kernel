d2c4824d6245ce1e46192ac46bc0df89df1a4576
H. Nikolaus Schaller <hns@goldelico.com>
2021-01-17 20:23:52 +0100

    Revert "MIPS: mm: Add Ingenic XBurst SoCs specific cache driver."
    This reverts commit bb02f18302376ac8af9d0dcb03a310dc2d2c016c.

--- arch/mips/mm/cache.c
+++ arch/mips/mm/cache.c
@@ -218,7 +218,6 @@ void cpu_cache_init(void)
 
 		r8k_cache_init();
 	}
-
 	if (cpu_has_tx39_cache) {
 		extern void __weak tx39_cache_init(void);
 
@@ -229,12 +228,6 @@ void cpu_cache_init(void)
 		extern void __weak octeon_cache_init(void);
 
 		octeon_cache_init();
-	}
-
-	if (cpu_has_ingenic_cache) {
-		extern void __weak ingenic_cache_init(void);
-
-		ingenic_cache_init();
 	}
 
 	setup_protection_map();
