9a45512819dff23e1235645c3ca368c3c530ef13
H. Nikolaus Schaller <hns@goldelico.com>
2021-03-22 12:42:37 +0100

    epdc: remove never implemented MODULE_SUPPORTED_DEVICE like commit 6417f03132a69
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/video/fbdev/mxc/mxc_epdc_fb.c
+++ drivers/video/fbdev/mxc/mxc_epdc_fb.c
@@ -6058,4 +6058,3 @@ module_exit(mxc_epdc_fb_exit);
 MODULE_AUTHOR("Freescale Semiconductor, Inc.");
 MODULE_DESCRIPTION("MXC EPDC framebuffer driver");
 MODULE_LICENSE("GPL");
-MODULE_SUPPORTED_DEVICE("fb");
