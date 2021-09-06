a33f3f84d356ff2af18db87a5799e47200bcd45f
H. Nikolaus Schaller <hns@goldelico.com>
2021-09-06 19:24:08 +0200

    ingenic-drm-drv: if we provide MEDIA_BUS_FMT_RGB888_1X24 by dw-hdmi we do not need to hack ingenic-drm-drv.c
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/gpu/drm/ingenic/ingenic-drm-drv.c
+++ drivers/gpu/drm/ingenic/ingenic-drm-drv.c
@@ -760,7 +760,7 @@ static void ingenic_drm_encoder_atomic_mode_set(struct drm_encoder *encoder,
 				cfg |= JZ_LCD_CFG_MODE_TV_OUT_P;
 		} else {
 
-bridge->bus_cfg.format = MEDIA_BUS_FMT_RGB888_1X24;
+printk("%s %d: use format %08x\n", __func__, __LINE__, bridge->bus_cfg.format);
 
 			switch (bridge->bus_cfg.format) {
 			case MEDIA_BUS_FMT_RGB565_1X16:
