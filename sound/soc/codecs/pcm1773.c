ddce90f463e3947669dce5ab4e917f10ef35d49b
H. Nikolaus Schaller <hns@goldelico.com>
2022-08-27 10:57:14 +0200

    ASoC: pcm1773-codec: replace non_legacy_dai_naming = 1 with legacy_dai_naming = 0
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- sound/soc/codecs/pcm1773.c
+++ sound/soc/codecs/pcm1773.c
@@ -141,7 +141,6 @@ static struct snd_soc_component_driver soc_component_dev_pcm1773 = {
 	.num_dapm_widgets = ARRAY_SIZE(pcm1773_dapm_widgets),
 	.dapm_routes = pcm1773_dapm_routes,
 	.num_dapm_routes = ARRAY_SIZE(pcm1773_dapm_routes),
-	.non_legacy_dai_naming = 1,
 };
 
 static int pcm1773_codec_probe(struct platform_device *pdev)
