4d114b6bcad8de0183ce5480aefec40315a53bb4
H. Nikolaus Schaller <hns@goldelico.com>
2023-01-31 09:42:43 +0100

    sound: core: pcm: export snd_interval_step() for use by AESS
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- sound/core/pcm_lib.c
+++ sound/core/pcm_lib.c
@@ -1123,7 +1123,7 @@ int snd_interval_ranges(struct snd_interval *i, unsigned int count,
 }
 EXPORT_SYMBOL(snd_interval_ranges);
 
-static int snd_interval_step(struct snd_interval *i, unsigned int step)
+int snd_interval_step(struct snd_interval *i, unsigned int step)
 {
 	unsigned int n;
 	int changed = 0;
@@ -1145,6 +1145,7 @@ static int snd_interval_step(struct snd_interval *i, unsigned int step)
 	}
 	return changed;
 }
+EXPORT_SYMBOL(snd_interval_step);
 
 /* Info constraints helpers */
 
