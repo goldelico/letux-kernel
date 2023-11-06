de7a5a75bac37cf33ed83cbc95cdcc4e346b260a
H. Nikolaus Schaller <hns@goldelico.com>
2023-11-06 21:43:14 +0100

    ll_printk init/main.c print "start kernel"

--- init/main.c
+++ init/main.c
@@ -898,6 +898,8 @@ void start_kernel(void)
 	char *command_line;
 	char *after_dashes;
 
+ll_printstr("start kernel\n");
+
 	set_task_stack_end_magic(&init_task);
 	smp_setup_processor_id();
 	debug_objects_early_init();
