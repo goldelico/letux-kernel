3f5cb24f398d9aa6395ed8d6d99fbb4d2b495d05
H. Nikolaus Schaller <hns@goldelico.com>
2024-01-01 12:45:10 +0100

    ASoC: omap-mcpdm: fix double probe issue
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- sound/soc/ti/omap-mcpdm.c
+++ sound/soc/ti/omap-mcpdm.c
@@ -577,7 +577,7 @@ static int omap_mcpdm_remove(struct snd_soc_dai *dai)
 	return 0;
 }
 
-static const struct snd_soc_dai_ops omap_mcpdm_dai_ops = {
+static const struct snd_soc_dai_ops omap_mcpdm_legacy_dai_ops = {
 	.probe		= omap_mcpdm_probe,
 	.remove		= omap_mcpdm_remove,
 	.startup	= omap_mcpdm_dai_startup,
@@ -587,6 +587,17 @@ static const struct snd_soc_dai_ops omap_mcpdm_dai_ops = {
 	.probe_order	= SND_SOC_COMP_ORDER_LATE,
 	.remove_order	= SND_SOC_COMP_ORDER_EARLY,
 };
+
+#if IS_ENABLED(CONFIG_SND_SOC_OMAP_AESS)
+static const struct snd_soc_dai_ops omap_mcpdm_abe_dai_ops = {
+	.startup	= omap_mcpdm_dai_startup,
+	.shutdown	= omap_mcpdm_dai_shutdown,
+	.hw_params	= omap_mcpdm_dai_hw_params,
+	.prepare	= omap_mcpdm_prepare,
+	.probe_order	= SND_SOC_COMP_ORDER_LATE,
+	.remove_order	= SND_SOC_COMP_ORDER_EARLY,
+};
+#endif
 
 #ifdef CONFIG_PM_SLEEP
 static int omap_mcpdm_suspend(struct snd_soc_component *component)
@@ -650,7 +661,7 @@ static struct snd_soc_dai_driver omap_mcpdm_dai[] = {
 		.formats = OMAP_MCPDM_FORMATS,
 		.sig_bits = 24,
 	},
-	.ops = &omap_mcpdm_dai_ops,
+	.ops = &omap_mcpdm_legacy_dai_ops,
 },
 #if IS_ENABLED(CONFIG_SND_SOC_OMAP_AESS)
 {
@@ -668,7 +679,7 @@ static struct snd_soc_dai_driver omap_mcpdm_dai[] = {
 		.rates = OMAP_MCPDM_RATES,
 		.formats = OMAP_MCPDM_FORMATS,
 	},
-	.ops = &omap_mcpdm_dai_ops,
+	.ops = &omap_mcpdm_abe_dai_ops,
 },
 #endif
 };
