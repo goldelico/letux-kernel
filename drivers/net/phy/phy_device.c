85eaa3b226862428cc0562ca2739166cf547e8c7
H. Nikolaus Schaller <hns@goldelico.com>
2024-07-29 12:00:21 +0200

    Partial Revert: ODROID-COMMON: phy/realtek: add Wake-on-Lan to Realtek PHY
    there is no dev->wol_enabled since v6.11-rc1
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -1882,10 +1882,8 @@ void phy_detach(struct phy_device *phydev)
 		phydev->is_genphy_driven = 0;
 	}
 
-	if (!dev->wol_enabled) {
-		/* Assert the reset signal */
-		phy_device_reset(phydev, 1);
-	}
+	/* Assert the reset signal */
+	phy_device_reset(phydev, 1);
 
 	/*
 	 * The phydev might go away on the put_device() below, so avoid
