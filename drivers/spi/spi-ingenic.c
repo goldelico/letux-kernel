f251d9b611e796f6dfd5dca34355b64b6d1038b1
H. Nikolaus Schaller <hns@goldelico.com>
2025-01-29 22:09:16 +0100

    spi: ingenic: fix and improve clock setup
    The parent clock should be as fast as reasonable. Hence we try a
    frequency with maximal SSIGR divider value. But we have to limit
    it to what the internal hardware can support. This limit seems to
    be 300 MHz.
    
    Then, we derive the SSIGR divider with the highest permissible
    divider. Note, that we also have to round up the divisor and
    not round down.
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/spi/spi-ingenic.c
+++ drivers/spi/spi-ingenic.c
@@ -101,11 +101,21 @@ static void spi_ingenic_prepare_transfer(struct ingenic_spi *priv,
 					 struct spi_device *spi,
 					 struct spi_transfer *xfer)
 {
-	unsigned long clk_hz = clk_get_rate(priv->clk);
+	unsigned long clk_hz;
+	u64 wanted_clk_hz;
 	u32 cdiv, speed_hz = xfer->speed_hz ?: spi->max_speed_hz,
 	    bits_per_word = xfer->bits_per_word ?: spi->bits_per_word;
 
-	cdiv = clk_hz / (speed_hz * 2);
+	/* try to raise as high as possible while keeping cdiv in unclamped range */
+	wanted_clk_hz = 256 * (u64) speed_hz * 2;
+	clk_hz = MIN(wanted_clk_hz, 300000000);	/* upper limit */
+
+	clk_set_rate(priv->clk, clk_hz);
+
+	/* read back frequency that is really divided */
+	clk_hz = clk_get_rate(priv->clk);
+
+	cdiv = (clk_hz + speed_hz * 2 - 1) / (speed_hz * 2);
 	cdiv = clamp(cdiv, 1u, 0x100u) - 1;
 
 	regmap_write(priv->map, REG_SSIGR, cdiv);
