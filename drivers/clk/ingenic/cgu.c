e9c566663a56f961ed3241c5e8b263e12b480815
H. Nikolaus Schaller <hns@goldelico.com>
2025-06-05 19:43:35 +0200

    ingenic: cgu: x2000: fix for new parameter
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/clk/ingenic/cgu.c
+++ drivers/clk/ingenic/cgu.c
@@ -159,7 +159,7 @@ ingenic_pll_calc(const struct ingenic_cgu_clk_info *clk_info,
 	unsigned int m, n, od;
 
 	if (pll_info->calc_m_n_od)
-		(*pll_info->calc_m_n_od)(pll_info, rate, parent_rate, &m, &n, &od);
+		(*pll_info->calc_m_n_od)(pll_info, rate, parent_rate, &m, &n, &od, NULL);
 	else
 		ingenic_pll_calc_m_n_od(pll_info, rate, parent_rate, &m, &n, &od);
 
