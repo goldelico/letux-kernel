9711971f8b44f359bccb1fcb1af39540f5b4c79b
H. Nikolaus Schaller <hns@goldelico.com>
2025-08-29 09:37:03 +0200

    aess: dbg: do not reject empty dma buffer but handle gracefully
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- sound/soc/ti/aess/omap-aess-dbg.c
+++ sound/soc/ti/aess/omap-aess-dbg.c
@@ -318,12 +318,14 @@ static int aess_open_data(struct inode *inode, struct file *file)
 
 	debug->buffer_bytes = debug->elem_bytes * 4 * debug->buffer_msecs;
 
-	debug->buffer = dma_alloc_wc(aess->dev, debug->buffer_bytes,
+	if (debug->buffer_bytes > 0) {
+		debug->buffer = dma_alloc_wc(aess->dev, debug->buffer_bytes,
 					&debug->buffer_addr, GFP_KERNEL);
-	if (debug->buffer == NULL) {
-		dev_err(aess->dev, "can't alloc %d bytes for trace DMA buffer\n",
-			debug->buffer_bytes);
-		return -ENOMEM;
+		if (debug->buffer == NULL) {
+			dev_err(aess->dev, "can't alloc %d bytes for trace DMA buffer\n",
+				debug->buffer_bytes);
+			return -ENOMEM;
+		}
 	}
 
 	file->private_data = inode->i_private;
