575d7328d090479008272aaa0ac32a7d158498f0
H. Nikolaus Schaller <hns@goldelico.com>
2025-08-30 20:53:55 +0200

    aess: pcm: cleanup code
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- sound/soc/ti/aess/omap-aess-pcm.c
+++ sound/soc/ti/aess/omap-aess-pcm.c
@@ -486,17 +486,6 @@ printk("%s %d: aess=%px fw=%px\n", __func__, __LINE__, aess, fw);
 
 printk("%s %d\n", __func__, __LINE__);
 
-// FIXME: this is defined by omap-abe-twl6040.c and can't be called here!!!
-// should be done there - after loading firmware - or assuming firmware has been loaded
-//
-//	ret = omap_abe_add_legacy_dai_links(card);
-//	if (ret < 0)
-//		return ret;
-
-//	ret = omap_abe_add_aess_dai_links(card);
-//	if (ret < 0)
-//		return ret;
-
 	ret = snd_soc_tplg_component_load(component, &soc_tplg_ops,
 					  aess->fw);
 	if (ret < 0) {
