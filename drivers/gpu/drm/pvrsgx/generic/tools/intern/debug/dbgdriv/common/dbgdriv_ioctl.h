44f7943febf404ad396517501dcb0d1ce3617334
H. Nikolaus Schaller <hns@goldelico.com>
2025-09-14 15:53:33 +0200

    gpu: pvrsrvkm: SoC integration reconstructed from 1.7.862890
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/gpu/drm/pvrsgx/generic/tools/intern/debug/dbgdriv/common/dbgdriv_ioctl.h
+++ drivers/gpu/drm/pvrsgx/generic/tools/intern/debug/dbgdriv/common/dbgdriv_ioctl.h
@@ -24,35 +24,12 @@
  *
  ******************************************************************************/
 
-#ifndef _HOSTFUNC_
-#define _HOSTFUNC_
+#ifndef _IOCTL_
+#define _IOCTL_
 
-#define HOST_PAGESIZE			(4096)
-#define DBG_MEMORY_INITIALIZER	(0xe2)
+#define MAX_DBGVXD_W32_API 25
 
-IMG_UINT32 HostReadRegistryDWORDFromString(IMG_CHAR *pcKey, IMG_CHAR *pcValueName, IMG_UINT32 *pui32Data);
-
-IMG_VOID * HostPageablePageAlloc(IMG_UINT32 ui32Pages);
-IMG_VOID HostPageablePageFree(IMG_VOID * pvBase);
-IMG_VOID * HostNonPageablePageAlloc(IMG_UINT32 ui32Pages);
-IMG_VOID HostNonPageablePageFree(IMG_VOID * pvBase);
-
-IMG_VOID * HostMapKrnBufIntoUser(IMG_VOID * pvKrnAddr, IMG_UINT32 ui32Size, IMG_VOID * *ppvMdl);
-IMG_VOID HostUnMapKrnBufFromUser(IMG_VOID * pvUserAddr, IMG_VOID * pvMdl, IMG_VOID * pvProcess);
-
-IMG_VOID HostCreateRegDeclStreams(IMG_VOID);
-
-IMG_VOID * HostCreateMutex(IMG_VOID);
-IMG_VOID HostAquireMutex(IMG_VOID * pvMutex);
-IMG_VOID HostReleaseMutex(IMG_VOID * pvMutex);
-IMG_VOID HostDestroyMutex(IMG_VOID * pvMutex);
-
-#if defined(SUPPORT_DBGDRV_EVENT_OBJECTS)
-IMG_INT32 HostCreateEventObjects(IMG_VOID);
-IMG_VOID HostWaitForEvent(DBG_EVENT eEvent);
-IMG_VOID HostSignalEvent(DBG_EVENT eEvent);
-IMG_VOID HostDestroyEventObjects(IMG_VOID);
-#endif	
+extern IMG_UINT32 (*g_DBGDrivProc[MAX_DBGVXD_W32_API])(IMG_VOID *, IMG_VOID *);
 
 #endif
 
