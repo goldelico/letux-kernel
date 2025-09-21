c4baa9974c42a10ac0d9501fe8e48c459768426d
H. Nikolaus Schaller <hns@goldelico.com>
2025-09-21 08:33:36 +0200

    abe: twl6040: call aess functions through opp pointers
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- sound/soc/ti/omap-abe-twl6040.c
+++ sound/soc/ti/omap-abe-twl6040.c
@@ -34,6 +34,10 @@
 
 #if IS_ENABLED(CONFIG_SND_SOC_OMAP_AESS)
 #include "aess/omap-aess.h"
+
+#define aess(AESS) platform_get_drvdata(AESS)
+#define aess_ops(AESS) ((struct omap_aess_ops *) platform_get_drvdata(AESS))
+
 #endif
 #include "../codecs/twl6040.h"
 #include "omap-dmic.h"
@@ -530,7 +534,7 @@ printk("%s %d\n", __func__, __LINE__);
 
 	priv->twl6040_power_mode = ucontrol->value.integer.value[0];
 #if IS_ENABLED(CONFIG_SND_SOC_OMAP_AESS)
-	omap_aess_pm_set_mode(platform_get_drvdata(omap_aess_dev), priv->twl6040_power_mode);
+	aess_ops(omap_aess_dev)->pm_set_mode(aess(omap_aess_dev), priv->twl6040_power_mode);
 #endif
 
 	return 1;
@@ -636,7 +640,7 @@ printk("%s %d\n", __func__, __LINE__);
 	gain = twl6040_get_dl1_gain(component) * 100;
 
 #if IS_ENABLED(CONFIG_SND_SOC_OMAP_AESS)
-	omap_aess_set_dl1_gains(platform_get_drvdata(omap_aess_dev), gain, gain);
+	aess_ops(omap_aess_dev)->set_dl1_gains(aess(omap_aess_dev), gain, gain);
 #endif
 
 	return 0;
@@ -658,7 +662,7 @@ printk("%s %d\n", __func__, __LINE__);
 	right_offset = TWL6040_HSF_TRIM_RIGHT(hfotrim);
 	left_offset = TWL6040_HSF_TRIM_LEFT(hfotrim);
 
-	omap_aess_dc_set_hf_offset(platform_get_drvdata(omap_aess_dev), left_offset, right_offset);
+	aess_ops(omap_aess_dev)->dc_set_hf_offset(aess(omap_aess_dev), left_offset, right_offset);
 
 	return 0;
 }
@@ -715,6 +719,7 @@ printk("%s %d\n", __func__, __LINE__);
 #if IS_ENABLED(CONFIG_SND_SOC_OMAP_AESS)
 /* FIXME: should this be called twl6040_dl1_init? */
 // NOTE: This does overall backend initialization
+
 static int omap_abe_twl6040_aess_init(struct snd_soc_pcm_runtime *rtd)
 {
 	struct snd_soc_component *component = snd_soc_rtd_to_codec(rtd, 0)->component;
@@ -762,7 +767,7 @@ printk("%s %d:ret=%d\n", __func__, __LINE__, ret);
 
 		step_mV = twl6040_get_hs_step_size(component);
 
-		omap_aess_dc_set_hs_offset(platform_get_drvdata(omap_aess_dev), left_offset,
+		aess_ops(omap_aess_dev)->dc_set_hs_offset(aess(omap_aess_dev), left_offset,
 					   right_offset, step_mV);
 
 		/* ABE power control */
@@ -1080,7 +1085,7 @@ printk("%s %d: omap_aess_dev =%px\n", __func__, __LINE__, omap_aess_dev);
 	if(!aess->driver)
 		return -EPROBE_DEFER;	// not yet bound to driver
 
-printk("%s %d: %px %s platform_get_drvdata=%px\n", __func__, __LINE__, omap_aess_dev, omap_aess_dev?omap_aess_dev->name:NULL, platform_get_drvdata(omap_aess_dev));
+printk("%s %d: %px %s platform_get_drvdata=%px\n", __func__, __LINE__, omap_aess_dev, omap_aess_dev?omap_aess_dev->name:NULL, aess(omap_aess_dev));
 
 #endif
 
