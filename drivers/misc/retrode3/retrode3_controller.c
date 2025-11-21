64e83f478ebeea54ac7b535b55d776b6f0445afd
H. Nikolaus Schaller <hns@goldelico.com>
2025-11-21 23:38:23 +0100

    misc: retrode3: prepare to handle 4 independent controllers
    2x SEGA + 2xSNES
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/misc/retrode3/retrode3_controller.c
+++ drivers/misc/retrode3/retrode3_controller.c
@@ -3,7 +3,7 @@
 /*
  * driver for Retrode 3 game controller driver
  *
- *  Copyright (C) 2022-23, H. Nikolaus Schaller
+ *  Copyright (C) 2022-25, H. Nikolaus Schaller
  *
  * FIXME: make this an independent driver module
  */
@@ -12,10 +12,10 @@
 #include <linux/delay.h>
 
 /*
- * game controller driver
+ * game controller driver for SEGA and SNES controllers
  */
 
-#define POLL_RATE	msecs_to_jiffies(20)	/* in ms */
+#define POLL_RATE	msecs_to_jiffies(1000/60)	/* 60 Hz should be 16.67 for SNES */
 
 static inline void retrode3_set_select(struct retrode3_slot *slot, int state)
 {
@@ -30,7 +30,7 @@ static void retrode3_polling_work(struct work_struct *work)
 
 	int cycle;
 	int w;
-	u64 word[2] = { 0, 0 };	// left and right controllers
+	u64 word[4] = { 0, 0, 0, 0 };	// left and right controllers
 
 	select_slot(slot->bus, slot);
 
@@ -47,14 +47,16 @@ static void retrode3_polling_work(struct work_struct *work)
 	}
 // printk("%s: %llx %llx\n", __func__, word[0], word[1]);
 
-	select_slot(slot->bus, NULL);
-
-
-// see: https://nfggames.com/forum2/index.php?topic=2266.0
-/* see https://sites.ualberta.ca/~delliott/cmpe490/appnotes/2016w/g6_genesis_controllers/genesis_controller.pdf */
-
-/* there is a 6 button variant where TH cycles through more banks: https://huguesjohnson.com/programming/genesis/6button/ */
-/* NOTE: he uses a mirrored pin numbering scheme */
+	/* SEGA controller
+	 * see: https://nfggames.com/forum2/index.php?topic=2266.0
+	 * see https://sites.ualberta.ca/~delliott/cmpe490/appnotes/2016w/g6_genesis_controllers/genesis_controller.pdf
+	 *
+	 * there is a 6 button variant where TH cycles through more banks: https://huguesjohnson.com/programming/genesis/6button/
+	 * NOTE: he uses a mirrored pin numbering scheme
+	 *
+	 * A22: 		Clock
+	 * D0-D6/D8-D13:	Data
+	 */
 
 	for (i=0; i < 2; i++) {
 		struct retrode3_controller *c = &slot->controllers[i];
@@ -77,10 +79,10 @@ static void retrode3_polling_work(struct work_struct *work)
 		if (c->state_valid) { // skip first analysis after boot
 #define SEND_CHANGE(BIT, KEY) if (changes & BIT_ULL(BIT)) input_report_key(c->input, KEY, !(state & BIT_ULL(BIT)));
 			/* NOTE: in fast key press actions more than a single key status may have changed */
-			SEND_CHANGE(59, KEY_U);
-			SEND_CHANGE(58, KEY_D);
-			SEND_CHANGE(48, KEY_R);
-			SEND_CHANGE(49, KEY_L);
+			SEND_CHANGE(59, KEY_UP);
+			SEND_CHANGE(58, KEY_DOWN);
+			SEND_CHANGE(48, KEY_RIGHT);
+			SEND_CHANGE(49, KEY_LEFT);
 			SEND_CHANGE(60, KEY_A);
 			SEND_CHANGE(52, KEY_B);
 			SEND_CHANGE(53, KEY_C);
@@ -97,6 +99,81 @@ static void retrode3_polling_work(struct work_struct *work)
 		c->last_state = state;
 		c->state_valid = true;
 	}
+
+	/* SNES controller
+	 * based on description at https://gamefaqs.gamespot.com/snes/916396-super-nintendo/faqs/5395
+	 *
+	 * A21:			Latch
+	 * A22:			Clock
+	 * D7/D15:		Data (button pressed = 0)
+	 * 			unplugged makes all data read 0
+	 */
+
+	retrode3_set_select(slot, 1);	// select slot and set clock high
+	set_address(slot->bus, (slot->bus->current_addr | BIT(21)));	// latch pulse
+	// add usleep_range(12, 20); or rely on that setting a gpio takes "enough" time?
+	set_address(slot->bus, (slot->bus->current_addr & ~BIT(21)));	// end latch pulse
+
+	// it may be possible to merge this with the above loop for sega
+	// but there are differences: 16 clocks vs. 8 cycles
+	// sampling is on one edge only - but that would simply multiply the bit numbers by 2
+	// but makes it more difficult (&0x5555) to detect all buttons are low
+	for (cycle = 0; cycle < 16; cycle ++) { // 16 clocks
+		retrode3_set_select(slot, 0);
+		w = read_word(slot->bus);	// D7 and D15 are relevant
+		if(w < 0) { // invalid read
+			select_slot(slot->bus, NULL);
+			return;
+		}
+		for (i = 2; i < 3; i++)
+			word[i] = (word[i] << 1) | ((w >> slot->controllers[i].data_offset) & 0x1);
+		retrode3_set_select(slot, 1);
+	}
+
+// printk("%s: %llx %llx\n", __func__, word[2], word[3]);
+
+	for (i=2; i < 3; i++) {
+		struct retrode3_controller *c = &slot->controllers[i];
+		u64 state = word[i];
+		u64 changes = state ^ c->last_state;
+
+if (changes) printk("%s: controller %d changes %16llx state %16llx\n", __func__, i, changes, state);
+
+		if (!c->state_valid || state == 0) { // first event after boot or controller has been (un)plugged
+			char *envp[4];
+
+			envp[0] = kasprintf(GFP_KERNEL, "SLOT=%s", dev_name(&slot->dev));
+			envp[1] = kasprintf(GFP_KERNEL, "CHANNEL=%d", i);
+			envp[2] = kasprintf(GFP_KERNEL, "STATE=%s", (state == 0)?"disconnected":"connected");
+			envp[3] = NULL;
+// printk("%s: %s %s %s\n", __func__, envp[0], envp[1], envp[2]);
+			// check with: udevadm monitor --environment
+			kobject_uevent_env(&slot->dev.kobj, KOBJ_CHANGE, envp);
+		}
+
+		if (c->state_valid) { // skip first analysis after boot
+			/* NOTE: in fast key press actions more than a single key status may have changed */
+			SEND_CHANGE(15, KEY_B);	// B
+			SEND_CHANGE(14, KEY_Y);	// Y
+			SEND_CHANGE(13, KEY_C);	// Select
+			SEND_CHANGE(12, KEY_S);	// Start
+			SEND_CHANGE(11, KEY_UP);	// U on joypad
+			SEND_CHANGE(10, KEY_DOWN);	// D on joypad
+			SEND_CHANGE(9, KEY_LEFT);	// L on joypad
+			SEND_CHANGE(8, KEY_RIGHT);	// R on joypad
+			SEND_CHANGE(7, KEY_A);	// A
+			SEND_CHANGE(6, KEY_X);	// X
+			SEND_CHANGE(5, KEY_L);	// L shoulder
+			SEND_CHANGE(4, KEY_R);	// R shoulder
+			// 4 more bits
+			input_sync(c->input);
+		}
+
+		c->last_state = state;
+		c->state_valid = true;
+	}
+
+	select_slot(slot->bus, NULL);
 
 	schedule_delayed_work(&slot->work, POLL_RATE);	// start next check
 }
@@ -123,7 +200,7 @@ int retrode3_probe_controller(struct retrode3_slot *slot, struct device_node*chi
 	gpiod_set_value(slot->ce, 0);	// turn inactive
 
 	id = 0;
-	while ((controller = of_get_next_child(child, controller))) {
+	while (id < 4 && (controller = of_get_next_child(child, controller))) {
 		struct input_dev *input_dev;
 
 		dev_info(&slot->dev, "add game controller %d\n", id);
@@ -139,19 +216,34 @@ int retrode3_probe_controller(struct retrode3_slot *slot, struct device_node*chi
 
 		input_dev->id.bustype = BUS_GAMEPORT;
 
-		input_set_capability(input_dev, EV_KEY, KEY_A);
-		input_set_capability(input_dev, EV_KEY, KEY_B);
-		input_set_capability(input_dev, EV_KEY, KEY_C);
-		input_set_capability(input_dev, EV_KEY, KEY_U);
-		input_set_capability(input_dev, EV_KEY, KEY_D);
-		input_set_capability(input_dev, EV_KEY, KEY_L);
-		input_set_capability(input_dev, EV_KEY, KEY_R);
-		input_set_capability(input_dev, EV_KEY, KEY_S);
+		if (id < 2) { // sega
+			input_set_capability(input_dev, EV_KEY, KEY_A);
+			input_set_capability(input_dev, EV_KEY, KEY_B);
+			input_set_capability(input_dev, EV_KEY, KEY_C);
+			input_set_capability(input_dev, EV_KEY, KEY_UP);
+			input_set_capability(input_dev, EV_KEY, KEY_DOWN);
+			input_set_capability(input_dev, EV_KEY, KEY_LEFT);
+			input_set_capability(input_dev, EV_KEY, KEY_RIGHT);
+			input_set_capability(input_dev, EV_KEY, KEY_S);
 // FIXME: add only after detecting a 6 button controller?
-		input_set_capability(input_dev, EV_KEY, KEY_X);
-		input_set_capability(input_dev, EV_KEY, KEY_Y);
-		input_set_capability(input_dev, EV_KEY, KEY_Z);
-		input_set_capability(input_dev, EV_KEY, KEY_M);
+			input_set_capability(input_dev, EV_KEY, KEY_X);
+			input_set_capability(input_dev, EV_KEY, KEY_Y);
+			input_set_capability(input_dev, EV_KEY, KEY_Z);
+			input_set_capability(input_dev, EV_KEY, KEY_M);
+		} else { // snes
+			input_set_capability(input_dev, EV_KEY, KEY_A);
+			input_set_capability(input_dev, EV_KEY, KEY_B);
+			input_set_capability(input_dev, EV_KEY, KEY_X);
+			input_set_capability(input_dev, EV_KEY, KEY_Y);
+			input_set_capability(input_dev, EV_KEY, KEY_S);
+			input_set_capability(input_dev, EV_KEY, KEY_C);
+			input_set_capability(input_dev, EV_KEY, KEY_UP);
+			input_set_capability(input_dev, EV_KEY, KEY_DOWN);
+			input_set_capability(input_dev, EV_KEY, KEY_LEFT);
+			input_set_capability(input_dev, EV_KEY, KEY_RIGHT);
+			input_set_capability(input_dev, EV_KEY, KEY_L);
+			input_set_capability(input_dev, EV_KEY, KEY_R);
+		}
 
 		ret = input_register_device(input_dev);
 		if (ret) {
