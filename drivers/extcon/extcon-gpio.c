f50d7cbc461e7a1ee9ce4b803fef25db1638ad97
H. Nikolaus Schaller <hns@goldelico.com>
2026-04-29 10:11:37 +0200

    extcon: gpio: replace of_gpio.h by gpio/consumer.h for v7.1-rc1
    Fixes: b6420bd5aa0c3
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/extcon/extcon-gpio.c
+++ drivers/extcon/extcon-gpio.c
@@ -18,7 +18,7 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_platform.h>
-#include <linux/of_gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/of_irq.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
