0c33b7fef72e1f58f56982c5d3c6b3772eea9589
H. Nikolaus Schaller <hns@goldelico.com>
2026-08-20 10:03:53 +0200

    rtl8189es: replace strncpy() by strscpy() or memcpy()
    - only compile tested
    - may be wrong in detail (different handling of trailing \0)
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/staging/rtl8189es/core/rtw_wlan_util.c
+++ drivers/staging/rtl8189es/core/rtw_wlan_util.c
@@ -4429,7 +4429,7 @@ bool rtw_wowlan_parser_pattern_cmd(u8 *input, char *pattern,
 		} else {
 			u8 hex;
 
-			strncpy(member, input, len);
+			strscpy(member, input, len);
 			if (!rtw_check_pattern_valid(member, sizeof(member))) {
 				RTW_INFO("%s:[ERROR] pattern is invalid!!\n",
 					 __func__);
