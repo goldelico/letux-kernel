256702784f3945c75ff6d4919316812b3e48d21a
H. Nikolaus Schaller <hns@goldelico.com>
2026-09-18 19:49:42 +0200

    pwm: jz4730: prepare for upcoming replacement of linux/gpio.h by linux/gpio/consumer.h
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/pwm/pwm-jz4730.c
+++ drivers/pwm/pwm-jz4730.c
@@ -12,7 +12,7 @@
 
 #include <linux/clk.h>
 #include <linux/err.h>
-#include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/of_device.h>
