18e95c8bf2bec264173894e7a5526efddcbe2523
H. Nikolaus Schaller <hns@goldelico.com>
2026-09-18 19:49:38 +0200

    regulator: TPS6518x: prepare for upcoming replacement of linux/gpio.h by linux/gpio/consumer.h
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/regulator/tps6518x-regulator.c
+++ drivers/regulator/tps6518x-regulator.c
@@ -29,7 +29,6 @@
 #include <linux/regulator/driver.h>
 #include <linux/regulator/of_regulator.h>
 #include <linux/mfd/tps6518x.h>
-#include <linux/gpio.h>
 #include <linux/gpio/consumer.h>
 
 //#define TPS65185_V3P3_ENABLE		1
